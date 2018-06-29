e38f013a590fc3f549409c62efab11f230ffd8ae
CodeToFLAG <hxyqbsmile@gmail.com>
2018-06-29 21:06:50 +0800

    Update 139. Word Break.java

--- 6.29.2018/139. Word Break.java
+++ 6.29.2018/139. Word Break.java
@@ -1,14 +1,14 @@
 class Solution {
     public boolean wordBreak(String s, List<String> wordDict) {
-        boolean[] b = new boolean[s.length() + 1];
-        b[0] = true;
+        boolean[] res = new boolean[s.length() + 1];
+        res[0] = true;
         for (int i = 1; i <= s.length(); i++) {
             for (int j = 0; j < i; j++) {
-                if (b[j] == true && wordDict.contains(s.substring(j, i))) {
-                    b[i] = true;
+                if (res[j] && wordDict.contains(s.substring(j, i))) {
+                    res[i] = true;
                 }
             }
         }
-        return b[s.length()];
+        return res[s.length()];
     }
 }
