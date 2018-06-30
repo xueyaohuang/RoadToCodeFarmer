b9c639ffb88c89e180baafb1a9e26be37b71e7e9
CodeToFLAG <hxyqbsmile@gmail.com>
2018-06-30 16:46:18 +0800

    Create 217. Contains Duplicate.java

---
+++ 6.30.2018/217. Contains Duplicate.java
@@ -0,0 +1,34 @@
+// 1.
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        if (nums == null || nums.length < 2) {
+            return false;
+        }
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (nums[i] == nums[i + 1]) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
+
+// 2.
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        if (nums == null || nums.length < 2) {
+            return false;
+        }
+        Set<Integer> set = new HashSet<>();
+        for (int num : nums) {
+            if (set.contains(num)) {
+                return true;
+            }
+            else {
+                set.add(num);
+            }
+        }
+        return false;
+    }
+}
