507b40cbae34c0ce4a5f0844773293da12d6b1cf
CodeToFLAG <hxyqbsmile@gmail.com>
2018-06-30 14:41:38 +0800

    Create 202. Happy Number.java

---
+++ 6.30.2018/202. Happy Number.java
@@ -0,0 +1,42 @@
+class Solution {
+    public boolean isHappy(int n) {
+        Set<Integer> set = new HashSet<>();
+        while (set.add(n)) {
+            if (sumSquNum(n) == 1) {
+                return true;
+            }
+            n = sumSquNum(n);           
+        }
+        return false;
+    }
+    private int sumSquNum(int n) {
+        int sum = 0;
+        while (n > 0) {
+            sum += (n % 10) * (n % 10);
+            n /= 10;
+        }
+        return sum;
+    }
+}
+
+class Solution {
+    public boolean isHappy(int n) {
+        Set<Integer> set = new HashSet<>();
+        set.add(n);
+        while (n != 1) {
+            n = getSquare(n);
+            if (!set.add(n)) {
+                return false;
+            }
+        }
+        return true;
+    }
+    private int getSquare(int n) {
+        int res = 0;
+        while (n != 0) {
+            res += Math.pow(n % 10, 2);
+            n /= 10;
+        }
+        return res;
+    }
+}
