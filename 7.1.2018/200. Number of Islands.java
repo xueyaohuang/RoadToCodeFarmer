578c32ef07433ec54acf5b380eb8fe444ffeff46
CodeToFLAG <hxyqbsmile@gmail.com>
2018-07-01 11:24:57 +0800

    Create 200. Number of Islands.java

---
+++ 7.1.2018/200. Number of Islands.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int numIslands(char[][] grid) {
+        if (grid == null || grid.length == 0) {
+            return 0;
+        }
+        int count = 0;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j] == '1') {
+                    count++;
+                    removeIsland(grid, i, j);
+                }
+            }
+        }
+        return count;
+    }
+    private void removeIsland(char[][] grid, int i, int j) {
+        if (i < 0 || i >= grid.length || j < 0 || j >= grid[0].length || grid[i][j] == '0') {
+            return;
+        }
+        grid[i][j] = '0';
+        removeIsland(grid, i + 1, j);
+        removeIsland(grid, i - 1, j);
+        removeIsland(grid, i, j + 1);
+        removeIsland(grid, i, j - 1);
+        return;
+    }
+}
