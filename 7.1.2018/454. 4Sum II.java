cd29fc9f1f18bcf2a92fbb46388f9fae1d0d7e8a
CodeToFLAG <hxyqbsmile@gmail.com>
2018-07-01 11:09:14 +0800

    Create 454. 4Sum II.java

---
+++ 7.1.2018/454. 4Sum II.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int fourSumCount(int[] A, int[] B, int[] C, int[] D) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < A.length; i++) {
+            for (int j = 0; j < B.length; j++) {
+                map.put(A[i] + B[j], map.getOrDefault(A[i] + B[j], 0) + 1);
+            }
+        }
+        int res = 0;
+        for (int i = 0; i < C.length; i++) {
+            for (int j = 0; j <D.length; j++) {
+                res += map.getOrDefault(-1 * (C[i] + D[j]), 0);
+            }
+        }
+        return res;
+    }
+}
