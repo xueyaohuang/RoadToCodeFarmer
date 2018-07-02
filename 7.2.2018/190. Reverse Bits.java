8fbffd47c665767360cbc797a1dc6c6fb9ddcb1a
CodeToFLAG <hxyqbsmile@gmail.com>
2018-07-02 13:00:37 +0800

    Update 190. Reverse Bits.java

--- 7.2.2018/190. Reverse Bits.java
+++ 7.2.2018/190. Reverse Bits.java
@@ -14,21 +14,21 @@ public class Solution {
 int reverseBits(int n) {
   return Integer.reverse(n);
 }
-
 public class Solution {
     // you need treat n as an unsigned value
-    Integer[] hash = new Integer[256];
+    HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
     public int reverseBits(int n) {
         int res = 0;
         for(int i = 0; i < 4; i++){
             int tmp = n & 0xFF;
-            if(hash[tmp] != null){
-                res = (res << 8) + hash[tmp]; 
+            if(map.containsKey(tmp)){
+                res = (res << 8) + map.get(tmp); 
             } else{
                 res = (res << 8) + reverse8Bits(tmp);
             }
             n >>= 8;
         }
+        
         return res;
     }
     
@@ -39,7 +39,7 @@ public class Solution {
             res = (res << 1) + (tmp & 1);
             tmp >>= 1;
         }
-        hash[n] = res;
+        map.put(n, res);
         return res;
     }
 }
