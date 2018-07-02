d48fc1089548b53f38469a4f27e28d3b9b80dae4
CodeToFLAG <hxyqbsmile@gmail.com>
2018-07-02 14:49:05 +0800

    Create 242. Valid Anagram.java

---
+++ 7.2.2018/242. Valid Anagram.java
@@ -0,0 +1,37 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if ((s == null && t != null) || (s != null && t == null) || s.length() != t.length()) {
+            return false;
+        }
+        char[] sc = s.toCharArray();
+        char[] tc = t.toCharArray();
+        Arrays.sort(sc);
+        Arrays.sort(tc);
+        for (int i = 0; i < sc.length; i++) {
+            if (sc[i] != tc[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if ((s == null && t != null) || (s != null && t == null) || s.length() != t.length()) {
+            return false;
+        }
+        int[] freq = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            freq[s.charAt(i) - 'a']++;
+            freq[t.charAt(i) - 'a']--;
+        }
+        for (int i = 0; i < 26; i++) {
+            if (freq[i] != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
