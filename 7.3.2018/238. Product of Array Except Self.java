efcdd2280e8e3f29488b9217597307719ad57b46
CodeToFLAG <hxyqbsmile@gmail.com>
2018-07-03 13:52:13 +0800

    Create 238. Product of Array Except Self.java

---
+++ 7.3.2018/238. Product of Array Except Self.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        int[] prod = new int[nums.length];
+        if (nums == null || nums.length == 0) {
+            return prod;
+        }
+        prod[0] = 1;
+        for (int i = 1; i < nums.length; i++) {
+            prod[i] = prod[i - 1] * nums[i - 1];
+        }
+        int temp = nums[nums.length - 1];
+        for (int i = nums.length - 2; i >= 0; i--) {
+            prod[i] *= temp;
+            temp *= nums[i];
+        }
+        return prod;
+    }
+}
