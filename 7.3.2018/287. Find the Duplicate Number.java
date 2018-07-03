7c73e4c760c148e13c9c78b45b7ff35f3ee22df4
CodeToFLAG <hxyqbsmile@gmail.com>
2018-07-03 22:23:01 +0800

    Update 287. Find the Duplicate Number.java

--- 7.3.2018/287. Find the Duplicate Number.java
+++ 7.3.2018/287. Find the Duplicate Number.java
@@ -1,17 +1,21 @@
 class Solution {
     public int findDuplicate(int[] nums) {
-        int slow = nums[0];
-        int fast = nums[nums[0]];
-        while (slow != fast) {
-            slow = nums[slow];
+        int fast = 0;
+        int slow = 0;
+        int len = nums.length;
+        while (fast < len && nums[fast] < len) {
             fast = nums[nums[fast]];
-        }
-        fast = 0;
-        while (slow != fast) {
             slow = nums[slow];
-            fast = nums[fast];
+            if (slow == fast) {
+                fast = 0;
+                while (fast != slow) {
+                    fast = nums[fast];
+                    slow = nums[slow];
+                }
+                return slow;
+            }
         }
-        return slow;
+        return -1;
     }
 }
 
