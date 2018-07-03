f0a2d593b4e8750a426a3593a721bb04277e59eb
CodeToFLAG <hxyqbsmile@gmail.com>
2018-07-03 21:38:40 +0800

    Create 142. Linked List Cycle II.java

---
+++ 7.3.2018/142. Linked List Cycle II.java
@@ -0,0 +1,18 @@
+public class Solution {
+    public ListNode detectCycle(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+            if (slow == fast) {
+                while (head != slow) {
+                    head = head.next;
+                    slow = slow.next;
+                }
+                return slow;
+            }
+        }
+        return null;
+    }
+}
