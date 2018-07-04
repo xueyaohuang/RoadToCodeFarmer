f76214e412439fb1a45b6ad0a62432c89c46b50f
CodeToFLAG <hxyqbsmile@gmail.com>
2018-07-04 21:03:48 +0800

    Create 350. Intersection of Two Arrays II.java

---
+++ 7.4.2018/350. Intersection of Two Arrays II.java
@@ -0,0 +1,45 @@
+class Solution {
+    public int[] intersect(int[] nums1, int[] nums2) {
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+        int i = 0;
+        int j = 0;
+        int k = 0;
+        int[] res = new int[Math.min(nums1.length, nums2.length)];
+        while (i < nums1.length && j < nums2.length) {
+            if (nums1[i] == nums2[j]) {
+                res[k++] = nums1[i++];
+                j++;
+            }
+            else if (nums1[i] < nums2[j]) {
+                i++;
+            }
+            else {
+                j++;
+            }
+        }
+        return Arrays.copyOfRange(res, 0, k);
+    }
+}
+
+class Solution {
+    public int[] intersect(int[] nums1, int[] nums2) {
+        Map<Integer, Integer> map = new HashMap<>();
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < nums1.length; i++) {
+            map.put(nums1[i], map.getOrDefault(nums1[i], 0) + 1);
+        }
+        for (int i = 0; i < nums2.length; i++) {
+            if (map.containsKey(nums2[i]) && map.get(nums2[i]) > 0) {
+                list.add(nums2[i]);
+                map.put(nums2[i], map.get(nums2[i]) - 1);
+            }
+        }
+        int[] res = new int[list.size()];
+        int j = 0;
+        for (int i : list) {
+            res[j++] = i;
+        }
+        return res;
+    } 
+}
