97d675f8458fddfb6675a984c9ff2e54f71fe65f
CodeToFLAG <hxyqbsmile@gmail.com>
2018-07-04 21:07:19 +0800

    Update 350. Intersection of Two Arrays II.java

--- 7.4.2018/350. Intersection of Two Arrays II.java
+++ 7.4.2018/350. Intersection of Two Arrays II.java
@@ -43,3 +43,13 @@ class Solution {
         return res;
     } 
 }
+
+/*
+What if elements of nums2 are stored on disk, and the memory is
+limited such that you cannot load all elements into the memory at
+once?
+
+If only nums2 cannot fit in memory, put all elements of nums1 into a HashMap, read chunks of array that fit into the memory, and record the intersections.
+
+If both nums1 and nums2 are so huge that neither fit into the memory, sort them individually (external sort), then read 2 elements from each array at a time in memory, record intersections.
+*/
