fdce645b4fb083d93e27fff3c993d830d8e9dbd3
CodeToFLAG <hxyqbsmile@gmail.com>
2018-07-07 02:11:57 +0800

    Create 205. Isomorphic Strings.java

---
+++ 7.6.2018/205. Isomorphic Strings.java
@@ -0,0 +1,39 @@
+class Solution {
+    public boolean isIsomorphic(String s, String t) {
+        Map<Character, Character> map = new HashMap<>();
+        for (int i = 0; i < s.length(); i++) {
+            char cs = s.charAt(i);
+            char ct = t.charAt(i);
+            if (map.containsKey(cs)) {
+                if (map.get(cs) != ct) {
+                    return false;
+                }
+            }
+            else if (! map.containsValue(ct)) {
+                map.put(cs, ct);
+            }
+            else {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
+class Solution {
+    public boolean isIsomorphic(String s, String t) {
+        Map<Character, Integer> maps = new HashMap<>();
+        Map<Character, Integer> mapt = new HashMap<>();
+        for (Integer i = 0; i < s.length(); i++) {
+            if (maps.put(s.charAt(i), i) != mapt.put(t.charAt(i), i)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
+/*for (int i = 0; i < s.length(); i++)会报错，因为if (maps.put(s.charAt(i), i) != mapt.put(t.charAt(i), i))
+是比较reference（address）,而不是content comparison。
+如果用int i，由于Map<Character, Integer> 放进去value的类型是Integer，用int construct Integer时，maps和mapt会生成
+2个不同的Integer object。但是如果同Integer i, maps和mapt在put时，放入的是同一个integer object。*/
