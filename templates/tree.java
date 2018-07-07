5f3497c4c83581dd885b0662ab016659c68c0d64
CodeToFLAG <hxyqbsmile@gmail.com>
2018-07-07 16:49:34 +0800

    Update tree.java

--- templates/tree.java
+++ templates/tree.java
@@ -88,3 +88,47 @@ class Solution {
         return res;
     }
 }
+
+// reverse inorder traversal: right->root->left. 
+// EX. Leetcode 538	Convert BST to Greater Tree  
+class Solution {
+    
+    private int sum = 0; // sum has to be a global variable.
+    
+    public TreeNode convertBST(TreeNode root) {
+        if (root == null) {
+            return root;
+        }
+        convertBST(root.right);
+        int temp = root.val;
+        root.val += sum;
+        sum += temp;
+        convertBST(root.left);
+        return root;
+    }
+}
+
+class Solution {
+    public TreeNode convertBST(TreeNode root) {
+        if (root == null) {
+            return root;
+        }
+        int sum = 0;
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode p = root;
+        while (!stack.isEmpty() || p != null) {
+            if (p != null) {
+                stack.push(p);
+                p = p.right;
+            }
+            else {
+                TreeNode node = stack.pop();
+                int temp = node.val;
+                node.val += sum;
+                sum += temp;
+                p = node.left;
+            }
+        }
+        return root;
+    }
+}
