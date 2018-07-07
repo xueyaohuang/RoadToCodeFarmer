6e804574a9567a95886e85ee113bb123797836e2
CodeToFLAG <hxyqbsmile@gmail.com>
2018-07-07 18:33:34 +0800

    Update 207. Course Schedule.java

--- 7.6.2018/207. Course Schedule.java
+++ 7.6.2018/207. Course Schedule.java
@@ -1,72 +1,66 @@
-public class Solution {
+// DFS detect cycle solution.
+class Solution {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
-
-        boolean[] visited = new boolean[numCourses];  
-        List<Integer>[] adj = new List[numCourses];    
-        for(int i = 0; i < numCourses; i++)
+        List<Integer>[] adj = new ArrayList[numCourses];
+        boolean[] visited = new boolean[numCourses];
+        for (int i = 0; i < numCourses; i++) {
             adj[i] = new ArrayList<Integer>();
-        for(int i = 0; i < prerequisites.length; i++)  
-        {
-            int curCourse = prerequisites[i][0];        
-            int preCourse = prerequisites[i][1];        
-            adj[preCourse].add(curCourse);
         }
-        for(int i = 0; i < numCourses; i++)
-        {
-            if(!dfs(adj, visited, i))     
+        for (int i = 0; i < prerequisites.length; i++) {
+            adj[prerequisites[i][1]].add(prerequisites[i][0]);
+        }
+        for (int i = 0; i < numCourses; i++) {
+            if (hasCycle(adj, i, visited)) {
                 return false;
+            }
         }
         return true;
     }
-
-    private boolean dfs(List<Integer>[] adj, boolean[] visited, int course){
-
-        if(visited[course])         // have circle
-            return false;
-        visited[course] = true;
-        for (int i = 0; i < adj[course].size(); i++)
-        {
-            if(!dfs(adj, visited, adj[course].get(i)))
-                return false;
-            adj[course].remove(i);  // delete edge
+    private boolean hasCycle(List<Integer>[] adj, int i, boolean[] visited) {
+        if (visited[i]) {
+            return true;
         }
-        visited[course] = false;    
-        return true;
+        visited[i] = true;
+        for (int j = 0; j < adj[i].size(); j++) {
+            if (hasCycle(adj, adj[i].get(j), visited)) {
+                return true;
+            }
+            adj[i].remove(j); // this line of code is not a must. But it can boost the speed significantly.
+        }
+        visited[i] = false;
+        return false;
     }
 }
 
-public class Solution {
+// BFS
+class Solution {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
-
-        List<Integer>[] adj = new List[numCourses];    
-        for(int i = 0; i < numCourses; i++)
+        List<Integer>[] adj = new ArrayList[numCourses];
+        int[] inDegree = new int[numCourses];
+        Queue<Integer> finished = new LinkedList<>();
+        int canFinish = 0;
+        for (int i = 0; i < numCourses; i++) {
             adj[i] = new ArrayList<Integer>();
-        int[] indegree = new int[numCourses];          
-        Queue<Integer> readyCourses = new LinkedList(); 
-        int finishCount = 0;                        
-        for (int i = 0; i < prerequisites.length; i++)  
-        {
-            int curCourse = prerequisites[i][0];        
-            int preCourse = prerequisites[i][1];        
-            adj[preCourse].add(curCourse);
-            indegree[curCourse]++;
         }
-        for (int i = 0; i < numCourses; i++) 
-        {
-            if (indegree[i] == 0) 
-                readyCourses.offer(i);           
+        for (int i = 0; i < prerequisites.length; i++) {
+            adj[prerequisites[i][1]].add(prerequisites[i][0]);
+            inDegree[prerequisites[i][0]]++;
         }
-        while (!readyCourses.isEmpty()) 
-        {
-            int course = readyCourses.poll();        // finish
-            finishCount++;
-            for (int nextCourse : adj[course]) 
-            {
-                indegree[nextCourse]--;
-                if (indegree[nextCourse] == 0)    
-                    readyCourses.offer(nextCourse);  // ready
+        for (int i = 0; i < numCourses; i++) {
+            if (inDegree[i] == 0) {
+                finished.offer(i);
             }
         }
-        return finishCount == numCourses;
+        while (!finished.isEmpty()) {
+            int curCourse = finished.poll();
+            canFinish++;
+            for (int i = 0; i < adj[curCourse].size(); i++) {
+                inDegree[adj[curCourse].get(i)]--;
+                if (inDegree[adj[curCourse].get(i)] == 0) {
+                    finished.offer(adj[curCourse].get(i));
+                }
+            }
+        }
+        return canFinish == numCourses;
     }
 }
