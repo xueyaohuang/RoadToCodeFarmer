6a76e6270da669c06d5715da767f9f6a3a2d4cd4
CodeToFLAG <hxyqbsmile@gmail.com>
2018-07-07 20:03:43 +0800

    Update 210. Course Schedule II.java

--- 7.7.2018/210. Course Schedule II.java
+++ 7.7.2018/210. Course Schedule II.java
@@ -1,5 +1,47 @@
 // DFS
-
+class Solution {
+    public int[] findOrder(int numCourses, int[][] prerequisites) {
+        List<Integer>[] adj = new ArrayList[numCourses];
+        int[] visited = new int[numCourses];
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i < numCourses; i++) {
+            adj[i] = new ArrayList<Integer>();
+        }
+        for (int i = 0; i < prerequisites.length; i++) {
+            adj[prerequisites[i][1]].add(prerequisites[i][0]);
+        }
+        for (int i = 0; i < numCourses; i++) {
+            if (hasCycle(adj, i, visited, stack)) {
+                return new int[0];
+            }
+        }
+        int i = 0;
+        int[] res = new int[numCourses];
+        while (!stack.isEmpty()) {
+            res[i++] = stack.pop();
+        }
+        return res;
+    }
+    private boolean hasCycle(List<Integer>[] adj, int i, int[] visited, Stack<Integer> stack) {
+        // visit[i] == 1 means is visiting
+        if (visited[i] == 1) {
+            return true;
+        }    
+        // visit[i] == 2 means has been visited
+        if (visited[i] == 2) {
+            return false;
+        }      
+        visited[i] = 1;
+        for (int j = 0; j < adj[i].size(); j++) {
+            if (hasCycle(adj, adj[i].get(j), visited, stack)) {
+                return true;
+            }
+        }
+        visited[i] = 2;
+        stack.push(i);
+        return false;
+    }
+}
 
 // BFS
 class Solution {
