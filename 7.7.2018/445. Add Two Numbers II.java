f09f320937b8fd585a13e4e57e81b16ef40020a8
CodeToFLAG <hxyqbsmile@gmail.com>
2018-07-07 20:38:53 +0800

    Update 445. Add Two Numbers II.java

--- 7.7.2018/445. Add Two Numbers II.java
+++ 7.7.2018/445. Add Two Numbers II.java
@@ -45,3 +45,34 @@ class Solution {
         return prev;
     }
 }
+
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        Stack<Integer> s1 = new Stack<>();
+        Stack<Integer> s2 = new Stack<>();
+        ListNode list = new ListNode(0);
+        int sum = 0;
+        while (l1 != null) {
+            s1.push(l1.val);
+            l1 = l1.next;
+        }
+        while (l2 != null) {
+            s2.push(l2.val);
+            l2 = l2.next;
+        }
+        while (!s1.isEmpty() || !s2.isEmpty()) {
+            if (!s1.isEmpty()) {
+                sum += s1.pop();
+            }
+            if (!s2.isEmpty()) {
+                sum += s2.pop();
+            }
+            list.val = sum % 10;
+            ListNode head = new ListNode(sum / 10);
+            head.next = list;
+            list = head;
+            sum /= 10;
+        }
+        return list.val == 0 ? list.next : list;
+    }
+}
