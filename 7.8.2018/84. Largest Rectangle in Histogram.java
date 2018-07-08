200e1b44a9b5df65010dc19fda966f1b65840789
CodeToFLAG <hxyqbsmile@gmail.com>
2018-07-08 15:19:56 +0800

    Update 84. Largest Rectangle in Histogram.java

--- 7.8.2018/84. Largest Rectangle in Histogram.java
+++ 7.8.2018/84. Largest Rectangle in Histogram.java
@@ -21,3 +21,35 @@ class Solution {
 }
 // stack stores the indexes not heights[i].
 // Do not increase i in the for loop, increas i only when push index into stack.
+
+class Solution {
+    public int largestRectangleArea(int[] heights) {
+        if (heights == null || heights.length == 0) {
+            return 0;
+        }
+        int len = heights.length;
+        int[] left = new int[len]; //记录当前i，到左边，第一个比自己低的位置
+        int[] right = new int[len];//记录当前i，到右边，第一个比自己低的位置
+        left[0] = -1;
+        right[len - 1] = len;
+        for (int i = 1; i < len; i++) {
+            int pre = i - 1;
+            while (pre >= 0 && heights[pre] >= heights[i]) {
+                pre = left[pre]; // important
+            }
+            left[i] = pre;
+        }
+        for (int i = len - 2; i >= 0; i--) {
+            int pos = i + 1;
+            while (pos < len && heights[pos] >= heights[i]) {
+                pos = right[pos];
+            }
+            right[i] = pos;
+        }
+        int area = 0;
+        for (int i = 0; i < len; i++) {
+            area = Math.max(area, heights[i] * (right[i] - left[i] - 1));
+        }
+        return area;
+    }
+}
