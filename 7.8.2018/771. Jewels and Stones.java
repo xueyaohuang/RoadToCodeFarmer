fd696f11d4031f66c3ab931badec48bb622ea6ec
CodeToFLAG <hxyqbsmile@gmail.com>
2018-07-08 17:28:19 +0800

    Create 771. Jewels and Stones.java

---
+++ 7.8.2018/771. Jewels and Stones.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int numJewelsInStones(String J, String S) {
+        int count = 0;
+        for (char c : S.toCharArray()) {
+            if (J.indexOf(c) != -1) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
