78689e654f0ce72111c5c7d64293aea240a4cec3
CodeToFLAG <hxyqbsmile@gmail.com>
2018-07-09 18:03:12 +0800

    Update 582. Kill Process.java

--- 7.9.2018/582. Kill Process.java
+++ 7.9.2018/582. Kill Process.java
@@ -1,1 +1,21 @@
-.
+class Solution {
+    public List<Integer> killProcess(List<Integer> pid, List<Integer> ppid, int kill) {
+        Map<Integer, List<Integer>> adj = new HashMap<>();
+        List<Integer> res = new ArrayList<>();
+        for (int i = 0; i < pid.size(); i++) {
+            adj.putIfAbsent(ppid.get(i), new ArrayList<>());
+            adj.get(ppid.get(i)).add(pid.get(i));
+        }
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(kill);
+        res.add(kill);
+        while (!queue.isEmpty()) {
+        	int parent = queue.poll();
+        	if (adj.containsKey(parent)) {
+        		queue.addAll(adj.get(parent));
+        		res.addAll(adj.get(parent));
+        	}
+        }
+        return res;
+    }
+}
