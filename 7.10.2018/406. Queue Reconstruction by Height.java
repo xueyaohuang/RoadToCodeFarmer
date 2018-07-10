0c6b955fa181d37447ca0fdd180686ce7c5678c8
CodeToFLAG <hxyqbsmile@gmail.com>
2018-07-11 00:18:16 +0800

    Update 406. Queue Reconstruction by Height.java

--- 7.10.2018/406. Queue Reconstruction by Height.java
+++ 7.10.2018/406. Queue Reconstruction by Height.java
@@ -1,10 +1,10 @@
-public class Solution {
+class Solution {
     public int[][] reconstructQueue(int[][] people) {
         Arrays.sort(people, (a, b) -> a[0] == b[0] ? a[1] - b[1] : b[0] - a[0]);
-        List<int[]> list = new LinkedList<>();
+        LinkedList<int[]> res = new LinkedList<>();
         for (int[] p : people) {
-            list.add(p[1], p);
+            res.add(p[1], p);
         }
-        return list.toArray(new int[list.size()][]);
+        return res.toArray(people); // res.toArray(new int[people.length][2]);
     }
 }
