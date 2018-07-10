e8e2d55d258bf1ed8ad17a5cc2026e09dd5caccf
CodeToFLAG <hxyqbsmile@gmail.com>
2018-07-10 14:15:26 +0800

    Update 315. Count of Smaller Numbers After Self.java

--- 7.19.2018/315. Count of Smaller Numbers After Self.java
+++ 7.19.2018/315. Count of Smaller Numbers After Self.java
@@ -21,3 +21,55 @@ class Solution {
     	return count;
     }
 }
+
+
+class Solution {
+    public List<Integer> countSmaller(int[] nums) {
+        if (nums.length == 0) {
+            return Collections.emptyList();
+        }
+        LinkedList<Integer> list = new LinkedList<>();
+        TreeNode head = new TreeNode(nums[nums.length - 1]);
+        list.add(0);
+        for (int i = nums.length - 2; i >= 0; i--) {
+            int count = insert(head, nums[i]);
+            list.addFirst(count);
+        }
+        return list;
+    }
+    
+    private int insert(TreeNode root, int num) {
+        int count = 0;
+        while (true) {
+            if (num <= root.val) {
+                root.count++;
+                if (root.left == null) {
+                    root.left = new TreeNode(num);
+                    break;
+                } else {
+                    root = root.left;
+                }
+            } else {
+                count += root.count;
+                if (root.right == null) {
+                    root.right = new TreeNode(num);
+                    break;
+                } else {
+                    root = root.right;
+                }
+            }
+        }    
+        return count;
+    }
+    
+    private static class TreeNode {
+        int val;
+        int count;
+        TreeNode left;
+        TreeNode right;
+        public TreeNode(int val) {
+            this.val = val;
+            this.count = 1;
+        }
+    }
+}
