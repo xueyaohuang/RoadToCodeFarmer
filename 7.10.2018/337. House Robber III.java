8449d1399674a0c50dac32dbbc380a33d79a3e92
CodeToFLAG <hxyqbsmile@gmail.com>
2018-07-10 22:49:26 +0800

    Update 337. House Robber III.java

--- 7.10.2018/337. House Robber III.java
+++ 7.10.2018/337. House Robber III.java
@@ -1,1 +1,23 @@
-.
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public int rob(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int val = 0;
+        if (root.left != null) {
+            val += rob(root.left.left) + rob(root.left.right);
+        }
+        if (root.right != null) {
+            val += rob(root.right.left) + rob(root.right.right);
+        }
+        return Math.max(val + root.val, rob(root.left) + rob(root.right));
+    }
+}
