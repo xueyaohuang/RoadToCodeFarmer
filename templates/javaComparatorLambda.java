40f00c1757084071ef6c36502da5a101eb8e6f31
CodeToFLAG <hxyqbsmile@gmail.com>
2018-07-11 13:30:13 +0800

    Update javaComparatorLambda.java

--- templates/javaComparatorLambda.java
+++ templates/javaComparatorLambda.java
@@ -40,3 +40,20 @@ class Solution {
         return res;
     }
 }
+
+// 252. Meeting Rooms
+
+class Solution {
+    public boolean canAttendMeetings(Interval[] intervals) {
+        if (intervals == null || intervals.length == 0) {
+            return true;
+        }
+        Arrays.sort(intervals, (a, b) -> a.start - b.start);
+        for (int i = 1; i < intervals.length; i++) {
+            if (intervals[i].start < intervals[i - 1].end) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
