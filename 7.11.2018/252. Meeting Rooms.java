b8982b0f3b20ede46d28aa27dd9b57a7bab49d90
CodeToFLAG <hxyqbsmile@gmail.com>
2018-07-11 13:24:55 +0800

    Create 252. Meeting Rooms.java

---
+++ 7.11.2018/252. Meeting Rooms.java
@@ -0,0 +1,32 @@
+/**
+ * Definition for an interval.
+ * public class Interval {
+ *     int start;
+ *     int end;
+ *     Interval() { start = 0; end = 0; }
+ *     Interval(int s, int e) { start = s; end = e; }
+ * }
+ */
+class Solution {
+    public boolean canAttendMeetings(Interval[] intervals) {
+        if (intervals == null || intervals.length == 0) {
+            return true;
+        }
+        int n = intervals.length;
+        int[] startTime = new int[n];
+        int[] endTime = new int[n];
+        int i = 0;
+        for (Interval itv : intervals) {
+            startTime[i] = itv.start;
+            endTime[i++] = itv.end;
+        }
+        Arrays.sort(startTime);
+        Arrays.sort(endTime);
+        for (int j = 1; j < n; j++) {
+            if (startTime[j] < endTime[j - 1]) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
