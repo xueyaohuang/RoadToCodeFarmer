866b9636448617438deaa29baee1de029f2dc0bb
CodeToFLAG <hxyqbsmile@gmail.com>
2018-07-11 13:30:31 +0800

    Update 252. Meeting Rooms.java

--- 7.11.2018/252. Meeting Rooms.java
+++ 7.11.2018/252. Meeting Rooms.java
@@ -30,3 +30,18 @@ class Solution {
         return true;
     }
 }
+
+class Solution {
+    public boolean canAttendMeetings(Interval[] intervals) {
+        if (intervals == null || intervals.length == 0) {
+            return true;
+        }
+        Arrays.sort(intervals, (a, b) -> a.start - b.start);
+        for (int i = 1; i < intervals.length; i++) {
+            if (intervals[i].start < intervals[i - 1].end) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
