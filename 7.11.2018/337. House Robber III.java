5a7211e0a045c01bcf2e7660f2d54549675cb21e
CodeToFLAG <hxyqbsmile@gmail.com>
2018-07-11 09:59:37 +0800

    Update 337. House Robber III.java

--- 7.11.2018/337. House Robber III.java
+++ 7.11.2018/337. House Robber III.java
@@ -21,3 +21,21 @@ class Solution {
         return val;
     }
 }
+
+class Solution {
+    public int rob(TreeNode root) {
+        int[] res = robHelper(root);
+        return Math.max(res[0], res[1]);
+    }
+    private int[] robHelper(TreeNode root) {
+        if (root == null) {
+            return new int[2];
+        }
+        int[] res = new int[2];
+        int[] left = robHelper(root.left);
+        int[] right = robHelper(root.right);
+        res[0] = Math.max(left[0], left[1]) + Math.max(right[0], right[1]);
+        res[1] = root.val + left[0] +right[0];
+        return res;
+    }
+}
