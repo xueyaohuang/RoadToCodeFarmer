095945f69f01ea6caa8eabeae51afe7e70d4c104
CodeToFLAG <hxyqbsmile@gmail.com>
2018-07-12 00:03:54 +0800

    Update 339. Nested List Weight Sum.java

--- 7.11.2018/339. Nested List Weight Sum.java
+++ 7.11.2018/339. Nested List Weight Sum.java
@@ -1,1 +1,17 @@
-339. Nested List Weight Sum
+class Solution {
+    public int depthSum(List<NestedInteger> nestedList) {
+        return depthSumHelper(nestedList, 1);
+    }
+    private int depthSumHelper(List<NestedInteger> nestedList, int depth) {
+        int res = 0;
+        for (NestedInteger nl : nestedList) {
+            if (nl.isInteger()) {
+                res += nl.getInteger() * depth;
+            }
+            else {
+                res += depthSumHelper(nl.getList(), depth + 1);
+            }
+        }
+        return res;
+    }
+}
