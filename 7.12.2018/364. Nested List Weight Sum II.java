f159387469418318ea46b42159a93c9c4250157c
CodeToFLAG <hxyqbsmile@gmail.com>
2018-07-12 22:55:42 +0800

    Update 364. Nested List Weight Sum II.java

--- 7.12.2018/364. Nested List Weight Sum II.java
+++ 7.12.2018/364. Nested List Weight Sum II.java
@@ -1,1 +1,20 @@
-.
+class Solution {
+    public int depthSumInverse(List<NestedInteger> nestedList) {
+        int weighted = 0;
+        int unweighted = 0;
+        while (!nestedList.isEmpty()) {
+            List<NestedInteger> nextLevel = new ArrayList<>();
+            for (NestedInteger ni : nestedList) {
+                if (ni.isInteger()) {
+                    unweighted += ni.getInteger();
+                }
+                else {
+                    nextLevel.addAll(ni.getList());
+                }
+            }
+            weighted += unweighted;
+            nestedList = nextLevel;
+        }
+        return weighted;
+    }
+}
