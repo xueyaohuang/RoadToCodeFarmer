9d867adb0b4a15ad048843f31a21498ce3a4af3b
CodeToFLAG <hxyqbsmile@gmail.com>
2018-07-13 18:53:55 +0800

    Update 447. Number of Boomerangs.java

--- 7.13.2018/447. Number of Boomerangs.java
+++ 7.13.2018/447. Number of Boomerangs.java
@@ -1,28 +1,27 @@
-public int numberOfBoomerangs(int[][] points) {
-    int res = 0;
-
-    Map<Integer, Integer> map = new HashMap<>();
-    for(int i=0; i<points.length; i++) {
-        for(int j=0; j<points.length; j++) {
-            if(i == j)
-                continue;
-            
-            int d = getDistance(points[i], points[j]);                
-            map.put(d, map.getOrDefault(d, 0) + 1);
+class Solution {
+    public int numberOfBoomerangs(int[][] points) {
+        if (points == null || points.length == 0) {
+            return 0;
         }
-        
-        for(int val : map.values()) {
-            res += val * (val-1);
-        }            
-        map.clear();
+        int count = 0;
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < points.length; i++) {
+            map.clear();
+            for (int j = 0; j < points.length; j++) {
+                if (i != j) {
+                    int distance = distanceSqare(points[i], points[j]);
+                    map.put(distance, map.getOrDefault(distance, 0) + 1);
+                }
+            }
+            for (int num : map.values()) {
+                count += num * (num - 1);
+            }
+        }
+        return count;
     }
-    
-    return res;
-}
-
-private int getDistance(int[] a, int[] b) {
-    int dx = a[0] - b[0];
-    int dy = a[1] - b[1];
-    
-    return dx*dx + dy*dy;
+    private int distanceSqare(int[] A, int[] B) {
+        int xx = A[0] - B[0];
+        int yy = A[1] - B[1];
+        return xx * xx + yy * yy;
+    }
 }
