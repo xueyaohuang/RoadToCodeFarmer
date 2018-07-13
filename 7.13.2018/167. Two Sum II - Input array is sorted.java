5aa5830bb653d0ebbe33a5b86d11f106e1b60ef8
CodeToFLAG <hxyqbsmile@gmail.com>
2018-07-13 19:04:45 +0800

    Update 167. Two Sum II - Input array is sorted.java

--- 7.13.2018/167. Two Sum II - Input array is sorted.java
+++ 7.13.2018/167. Two Sum II - Input array is sorted.java
@@ -1,1 +1,18 @@
-.
+class Solution {
+    public int[] twoSum(int[] numbers, int target) {
+        int idx1 = 0;
+        int idx2 = numbers.length - 1;
+        while (idx1 < idx2) {
+            if (numbers[idx1] + numbers[idx2] < target) {
+                idx1++;
+            }
+            else if (numbers[idx1] + numbers[idx2] > target) {
+                idx2--;
+            }
+            else {
+                break;
+            }
+        }
+        return new int[]{idx1 + 1, idx2 + 1};
+    }
+}
