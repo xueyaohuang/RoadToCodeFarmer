fc8018f67720e21aec1b661bf4c320c2c1f36607
CodeToFLAG <hxyqbsmile@gmail.com>
2018-07-13 09:39:29 +0800

    Update 245. Shortest Word Distance III.java

--- 7.13.2018/245. Shortest Word Distance III.java
+++ 7.13.2018/245. Shortest Word Distance III.java
@@ -1,1 +1,22 @@
-.
+class Solution {
+    public int shortestWordDistance(String[] words, String word1, String word2) {
+        int idx1 = -1;
+        int idx2 = -1;
+        int distance = Integer.MAX_VALUE;
+        for (int i = 0; i < words.length; i++) {
+            if (word1.equals(words[i])) {
+                idx1 = i;
+            }
+            if (idx1 != idx2 && idx1 != -1 && idx2 != -1) {
+                distance = Math.min(distance, Math.abs(idx1 - idx2));
+            }
+            if (word2.equals(words[i])) {
+                idx2 = i;
+            }
+            if (idx1 != idx2 && idx1 != -1 && idx2 != -1) {
+                distance = Math.min(distance, Math.abs(idx1 - idx2));
+            }
+        }
+        return distance;
+    }
+}
