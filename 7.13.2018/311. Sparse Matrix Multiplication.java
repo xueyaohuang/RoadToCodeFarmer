4627a9439718e8471f7a8b419ab60b07e85b8e7c
CodeToFLAG <hxyqbsmile@gmail.com>
2018-07-13 09:48:55 +0800

    Update 311. Sparse Matrix Multiplication.java

--- 7.13.2018/311. Sparse Matrix Multiplication.java
+++ 7.13.2018/311. Sparse Matrix Multiplication.java
@@ -1,1 +1,18 @@
-.
+class Solution {
+    public int[][] multiply(int[][] A, int[][] B) {
+        int row = A.length;
+        int col = B[0].length;
+        int n = A[0].length;
+        int[][] res = new int[row][col];
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                for (int k = 0; k < n; k++) {
+                    if (A[i][k] != 0 && B[k][j] != 0) {
+                        res[i][j] += A[i][k] * B[k][j];
+                    }
+                }
+            }
+        }
+        return res;
+    }
+}
