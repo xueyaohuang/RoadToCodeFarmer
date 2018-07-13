6322c804e7aef95dc6a3a6743e0f5d47ac89c2d7
CodeToFLAG <hxyqbsmile@gmail.com>
2018-07-14 00:03:40 +0800

    Update 464. Can I Win.java

--- 7.13.2018/464. Can I Win.java
+++ 7.13.2018/464. Can I Win.java
@@ -1,116 +1,37 @@
 class Solution {
-    Map<Integer, Boolean> map;
-    boolean[] used;
+    
     public boolean canIWin(int maxChoosableInteger, int desiredTotal) {
-        int sum = (1 + maxChoosableInteger) * maxChoosableInteger / 2;
+        if (desiredTotal <= maxChoosableInteger) {
+            return true;
+        }
+        //Note: n should be <= 32 as int is 32-bit in Java; else it will 1 << 33+ equals 0.
+        int n = maxChoosableInteger;
+        int sum = n * (n + 1) / 2;
         if (sum < desiredTotal) {
             return false;
         }
-        if (desiredTotal <= 0) {
-            return true;
-        }
-        map = new HashMap<>();
-        used = new boolean[1 + maxChoosableInteger];
-        return helper(desiredTotal);
+        Boolean[] dp = new Boolean[1 << n];
+        return canIWin(0, n, desiredTotal, dp);
     }
-    
-    private boolean helper(int t) {
-        if (t <= 0) {
-            return false;
-        } 
-        int key = transform(used);
-        if (!map.containsKey(key)) {
-            for (int i = 1; i < used.length; i++) {
-                if (!used[i]) {
-                    used[i] = true;
-                    if (!helper(t - i)) {
-                        map.put(key, true);
-                        used[i] = false;
-                        return true;
-                    }
-                    used[i] = false;
+
+    private boolean canIWin(int state, int n, int remain, Boolean[] dp) {
+        if (remain <= 0) {
+            //dp[state] = false;
+            return false;   //Base case:
+        }
+
+        if (dp[state] == null) {
+            dp[state] = false;
+            int mask = 1;
+            for (int i = n; i > 0; i--) { //Key Point: take from the tail
+                int future = state | mask;
+                if (future != state && !canIWin(future, n, remain - i, dp)) {
+                    dp[state] = true;   //update current status = true
+                    break;
                 }
-            }
-            map.put(key, false);
-        }
-        return map.get(key);
-    }
-    
-    private int transform(boolean[] used) {
-        int num = 0;
-        for (boolean b : used){
-            num <<= 1;
-            if (b) {
-                num |= 1;
+                mask <<= 1;
             }
         }
-        return num;
-    }
-    
-    
-}
-
-
-class Solution {
-     class Node{
-        Node next = null;
-        int val;
-        Node(int val){
-            this.val = val;
-        }
-        void insert(Node n){
-            n.next = next;
-            next = n;
-        }
-        void remove(){
-            if (next != null){
-                next = next.next;
-            }
-        }
-    }
-    byte[] map;
-    public boolean canIWin(int max, int tgt) {
-        int total = max * (max + 1) / 2;
-        if (total < tgt){
-            return false;
-        }
-        if (total == tgt){
-            return max % 2 == 1;
-        }
-        if (max >= tgt){
-            return true;
-        }
-        Node head = new Node(0);
-        Node cur = head;
-        for (int i = max; i >= 1; i--){
-            cur.next = new Node(i);
-            cur = cur.next;
-        }
-        map = new byte[1 << max];
-        return dfs(head, tgt, 0);
-    }
-    
-    boolean dfs(Node head, int tgt, int state){
-        if (map[state] != 0){
-            return map[state] > 0;
-        }
-        if (tgt <= 0){
-            map[state] = -1;
-            return false;
-        }
-        Node cur = head;
-        while (cur.next != null){
-            Node next = cur.next;
-            cur.remove();
-            if (!dfs(head, tgt - next.val, state | (1 << (next.val - 1)))){
-                cur.insert(next);
-                map[state] = 1;
-                return true;
-            }
-            cur.insert(next);
-            cur = next;
-        }
-        map[state] = -1;
-        return false;
+        return dp[state];
     }
 }
