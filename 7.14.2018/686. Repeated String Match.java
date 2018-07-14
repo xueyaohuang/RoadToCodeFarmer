ed72f4946b1524fd6d23d6400091fd9aeaad91b4
CodeToFLAG <hxyqbsmile@gmail.com>
2018-07-14 14:35:33 +0800

    Update 686. Repeated String Match.java

--- 7.14.2018/686. Repeated String Match.java
+++ 7.14.2018/686. Repeated String Match.java
@@ -13,3 +13,54 @@ class Solution {
         return -1;
     }
 }
+
+class Solution {
+    public int repeatedStringMatch(String A, String B) {
+        int count = 1;
+        StringBuilder sb = new StringBuilder(A);
+        while (sb.length() < B.length()) {
+            sb.append(A);
+            count++;
+        }
+        if (sb.toString().contains(B)) {
+            return count;
+        }
+        if (sb.append(A).toString().contains(B)) {
+            return count + 1;
+        }
+        return -1;      
+    }
+}
+
+class Solution {
+    public int repeatedStringMatch(String A, String B) {
+        if (A == null || B == null) {
+            return -1;
+        }
+        int lenA = A.length();
+        int lenB = B.length();
+        String doubleA = A + A;
+        int idx = doubleA.indexOf(B.substring(0, Math.min(lenA, lenB)));
+        if (idx == -1) {
+            return -1;
+        }
+        int count = 1;
+        int idxB = 0;
+        while (idxB < lenB) {
+            int countA = 0;
+            int idxA = idx;
+            while (countA < lenA && idxB< lenB) {
+                if (A.charAt(idxA) != B.charAt(idxB)) {
+                    return -1;
+                }
+                countA++;
+                idxB++;
+                idxA = (idxA + 1) % lenA;
+                if (idxA == 0 && idxB < lenB) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+}
