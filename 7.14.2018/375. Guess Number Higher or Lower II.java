e2e2f5b287a6372c9f99d2190229c84062c194c1
CodeToFLAG <hxyqbsmile@gmail.com>
2018-07-14 13:19:21 +0800

    Update 375. Guess Number Higher or Lower II.java

--- 7.14.2018/375. Guess Number Higher or Lower II.java
+++ 7.14.2018/375. Guess Number Higher or Lower II.java
@@ -1,1 +1,21 @@
-.
+class Solution {
+    public int getMoneyAmount(int n) {
+        int[][] dp = new int[n + 1][n + 1];
+        for (int j = 2; j <= n; j++) {
+            for (int i = j - 1; i >= 1; i--) {
+                if (i == j - 1) {
+                    dp[i][j] = i;
+                }
+                else {
+                    int max = Integer.MAX_VALUE;
+                    for (int k = i + 1; k < j; k++) {
+                        int min = k + Math.max(dp[i][k - 1], dp[k + 1][j]);
+                        max = Math.min(max, min);
+                    }
+                    dp[i][j] = max;
+                }              
+            }     
+        }
+        return dp[1][n];
+    }
+}
