f7184e5d85737a0624e543eff78dc514424b97ca
CodeToFLAG <hxyqbsmile@gmail.com>
2018-07-14 17:10:10 +0800

    Update 254. Factor Combinations.java

--- 7.14.2018/254. Factor Combinations.java
+++ 7.14.2018/254. Factor Combinations.java
@@ -23,3 +23,28 @@ class Solution {
         }
     }
 }
+
+public class Solution {
+public List<List<Integer>> getFactors(int n) {
+    List<List<Integer>> res = new ArrayList<>();
+    backTrack(res, new ArrayList<Integer>(), 2, n);
+    return res;
+}
+
+public void backTrack(List<List<Integer>> res, List<Integer> cur, int start, int n) {
+    int upper = (int)Math.sqrt(n);
+    for(int i = start; i <= upper; i++) {
+        int factor = -1;
+        if(n % i == 0) {
+            factor = n/i;
+        }
+        if(factor != -1 && factor >= i) {
+            cur.add(i);
+            cur.add(factor);
+            res.add(new ArrayList<Integer>(cur));
+            cur.remove(cur.size()-1);
+            backTrack(res, cur, i, factor);
+            cur.remove(cur.size()-1);
+        }
+    }
+}
