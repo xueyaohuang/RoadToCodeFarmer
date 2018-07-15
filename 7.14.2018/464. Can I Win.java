1f4204f8f98e3b9080c6bb7a1748e4011db9a1d9
CodeToFLAG <hxyqbsmile@gmail.com>
2018-07-15 21:25:48 +0800

    Update 464. Can I Win.java

--- 7.14.2018/464. Can I Win.java
+++ 7.14.2018/464. Can I Win.java
@@ -1,33 +1,41 @@
+/*
+After solving several "Game Playing" questions in leetcode, I find them to be pretty similar. Most of them can be solved
+using the top-down DP approach, which "brute-forcely" simulates every possible state of the game. The key part for the
+top-down dp strategy is that we need to avoid repeatedly solving sub-problems. Instead, we should use some strategy to
+"remember" the outcome of sub-problems. Then when we see them again, we instantly know their result. 
+*/
+
 class Solution {
     private byte[] memo;
     public boolean canIWin(int maxChoosableInteger, int desiredTotal) {
-        int M = maxChoosableInteger, T = desiredTotal;
-        int sum = M * (M + 1) / 2;
-        if (sum < T) {
-            return false;
-        }
-        if (T <= 0) {
+        if (desiredTotal <= 0) {
             return true;
         }
-        memo = new byte[1 << M];
-        return canWin(M, T, 0);
-    }
-    
-    private boolean canWin(int M, int T, int state) {
-        if (T <= 0) {
+        int sum = maxChoosableInteger * (maxChoosableInteger + 1) / 2;
+        if (sum < desiredTotal) {
             return false;
         }
-        if (memo[state] != 0) {
+        memo = new byte[1 << maxChoosableInteger];
+        return canFirstWin(0, maxChoosableInteger, desiredTotal);
+    }
+    private boolean canFirstWin(int state, int max, int total) {
+        if (total <= 0) { // in the last step, the player already reach desiredTotal
+            return false;
+        }
+        if (memo[state] != 0) { // already has the result
             return memo[state] == 1;
         }
-        for (int i = 0; i < M; i++) {
-            if ((state & (1 << i)) > 0) continue;
-            if (!canWin(M, T - (i + 1), state | (1 << i))) {
-                memo[state] = 1;
-                return true;
+        for (int i = max - 1; i >= 0; i--) { // try every choosable integer
+            if ((state & (1 << i)) == 0) { // if not used
+                if (!canFirstWin(state | (1 << i), max, total - i - 1)) {
+                    memo[state] = 1;
+                    return true;
+                }
             }
         }
         memo[state] = -1;
         return false;
     }
 }
+// state is a maxChoosableInteger bit integer, representing whether a number in range 1 to n is used or not.
+//(state & (1 << i)) == 0 is true if the number is not used, state | (1 << i) will set the number to be used
