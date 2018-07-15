3b236bd2d11066ce5d14db0d108a4662e22829ce
CodeToFLAG <hxyqbsmile@gmail.com>
2018-07-15 22:43:56 +0800

    Update 359. Logger Rate Limiter.java

--- 7.14.2018/359. Logger Rate Limiter.java
+++ 7.14.2018/359. Logger Rate Limiter.java
@@ -1,1 +1,34 @@
-.
+class Logger {
+
+    Map<String, Integer> map;
+    /** Initialize your data structure here. */
+    public Logger() {
+        map = new HashMap<String, Integer>();
+    }
+    
+    /** Returns true if the message should be printed in the given timestamp, otherwise returns false.
+        If this method returns false, the message will not be printed.
+        The timestamp is in seconds granularity. */
+    public boolean shouldPrintMessage(int timestamp, String message) {
+        if (!map.containsKey(message)) {
+            map.put(message, timestamp);
+            return true;
+        }
+        else {
+            int lastPrint = map.get(message);
+            if (timestamp - lastPrint < 10) {
+                return false;
+            }
+            else {
+                map.put(message, timestamp);
+                return true;
+            }
+        }
+    }
+}
+
+/**
+ * Your Logger object will be instantiated and called as such:
+ * Logger obj = new Logger();
+ * boolean param_1 = obj.shouldPrintMessage(timestamp,message);
+ */
