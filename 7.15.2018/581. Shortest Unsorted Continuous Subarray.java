fb2ce84e18bc3e171db0fea2fb83a29744c292fc
CodeToFLAG <hxyqbsmile@gmail.com>
2018-07-16 02:26:55 +0800

    Update 581. Shortest Unsorted Continuous Subarray.java

--- 7.15.2018/581. Shortest Unsorted Continuous Subarray.java
+++ 7.15.2018/581. Shortest Unsorted Continuous Subarray.java
@@ -5,10 +5,10 @@ class Solution {
         }
         int start = 0;
         int end = nums.length - 1;
-        int[] copy = new int[nums.length];
-        for (int i = 0; i < nums.length; i++) {
-            copy[i] = nums[i];
-        }
+        int[] copy = Arrays.copyOf(nums, nums.length);
+        // for (int i = 0; i < nums.length; i++) {
+        //     copy[i] = nums[i];
+        // }
         Arrays.sort(copy);
         for (int i = 0; i < nums.length; i++) {
             if (nums[i] == copy[i]) {
@@ -25,5 +25,42 @@ class Solution {
             }
         }
         return Math.max(0, end - start + 1);
+    }
+}
+
+class Solution {
+    public int findUnsortedSubarray(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        int len = nums.length;
+        int l = 0;
+        int r = len - 1;
+        // 先找出开头和结尾递增的两段
+        while (l < len - 1 && nums[l] <= nums[l + 1]) {
+            l++;
+        }
+        while (r > 0 && nums[r] >= nums[r - 1]) {
+            r--;
+        }
+        // 找出中间一段的最大和最小值
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+        for (int i = l; i <= r; i++) {
+            if (nums[i] < min) {
+                min = nums[i];
+            }
+            if (nums[i] > max) {
+                max = nums[i];
+            }
+        }
+        // 保证l要小于min,r大于max
+        while (l >= 0 && nums[l] > min) {
+            l--;
+        }
+        while (r < len && nums[r] < max) {
+            r++;
+        }
+        return r > l ? r - l - 1 : 0;
     }
 }
