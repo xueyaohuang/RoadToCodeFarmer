bc438ab4bfeef6bbad7e6784e8dfc7597c4961f4
CodeToFLAG <hxyqbsmile@gmail.com>
2018-07-16 20:09:14 +0800

    Update 347. Top K Frequent Elements.java

--- 7.16.2018/347. Top K Frequent Elements.java
+++ 7.16.2018/347. Top K Frequent Elements.java
@@ -21,3 +21,22 @@ class Solution {
         return res;
     }
 }
+
+class Solution {
+    public List<Integer> topKFrequent(int[] nums, int k) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int n : nums) {
+            map.put(n, map.getOrDefault(n, 0) + 1);
+        }
+        PriorityQueue<Map.Entry<Integer, Integer>> queue = new PriorityQueue<>((a, b) -> b.getValue() - a.getValue());
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            queue.add(entry);
+        }
+        List<Integer> res = new ArrayList<>();
+        while (res.size() < k) {
+            Map.Entry<Integer, Integer> entry = queue.poll();
+            res.add(entry.getKey());
+        }
+        return res;
+    }
+}
