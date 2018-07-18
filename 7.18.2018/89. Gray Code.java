5c7962412081b0bb4c8d148b6654cb5c036df5a3
CodeToFLAG <hxyqbsmile@gmail.com>
2018-07-19 00:17:07 +0800

    Create 89. Gray Code.java

---
+++ 7.18.2018/89. Gray Code.java
@@ -0,0 +1,23 @@
+class Solution {
+    public List<Integer> grayCode(int n) {
+        List<Integer> res = new ArrayList<>();
+        for (int i = 0; i < (1 << n); i++) {
+            res.add(i ^ (i >> 1));
+        }
+        return res;
+    }
+}
+
+class Solution {
+    public List<Integer> grayCode(int n) {
+        List<Integer> res = new ArrayList<>();
+        res.add(0);
+        for (int i = 0; i < n; i++) {
+            int size = res.size();
+            for (int j = size - 1; j >= 0; j--) {
+                res.add(res.get(j) | (1 << i));
+            }
+        }
+        return res;
+    }
+}
