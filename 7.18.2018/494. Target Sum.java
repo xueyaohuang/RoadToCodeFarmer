5455f3168d2bb1b6c60e1846328042182c640599
CodeToFLAG <hxyqbsmile@gmail.com>
2018-07-18 23:18:08 +0800

    Update 494. Target Sum.java

--- 7.18.2018/494. Target Sum.java
+++ 7.18.2018/494. Target Sum.java
@@ -1,1 +1,25 @@
 
+class Solution {
+    public int findTargetSumWays(int[] nums, int S) {
+        int sum = 0;
+        for (int i : nums) {
+            sum += i;
+        }
+        if (sum < S || -sum > S || (sum + S) % 2 == 1) {
+            return 0;
+        }
+        int target = (sum + S) / 2;
+        return subSetSum(nums, target);
+    }
+    
+    private int subSetSum(int[] nums, int target) {
+        int[] dp = new int[target + 1];
+        dp[0] = 1;
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = target; j >= nums[i]; j--) {
+                dp[j] += dp[j - nums[i]];
+            }
+        }
+        return dp[target];
+    }
+}
