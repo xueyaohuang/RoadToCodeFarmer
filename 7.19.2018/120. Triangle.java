06052c942a4f625ac821c7af27095c7c69c89c19
CodeToFLAG <hxyqbsmile@gmail.com>
2018-07-19 12:01:34 +0800

    Update 120. Triangle.java

--- 7.19.2018/120. Triangle.java
+++ 7.19.2018/120. Triangle.java
@@ -19,3 +19,16 @@ class Solution {
         return res;
     }
 }
+
+class Solution {
+    public int minimumTotal(List<List<Integer>> triangle) {
+        int len = triangle.size();
+        int[] res = new int[len + 1];
+        for (int i = len - 1; i >= 0; i--) {
+            for (int j = 0; j < triangle.get(i).size(); j++) {
+                res[j] = Math.min(res[j], res[j + 1]) + triangle.get(i).get(j);
+            }
+        }
+        return res[0];
+    }
+}
