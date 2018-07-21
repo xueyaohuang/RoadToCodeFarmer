e2fd4148714f54f3bc6b0668a1449deaa0d80758
CodeToFLAG <hxyqbsmile@gmail.com>
2018-07-21 18:31:48 +0800

    Update 221. Maximal Square.java

--- 7.21.2018/221. Maximal Square.java
+++ 7.21.2018/221. Maximal Square.java
@@ -1,1 +1,20 @@
-.
+class Solution {
+    public int maximalSquare(char[][] matrix) {
+        if (matrix == null || matrix.length == 0) {
+            return 0;
+        }
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int side = 0;
+        int[][] dp = new int[m + 1][n + 1];
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (matrix[i - 1][ j - 1] == '1') {
+                    dp[i][j] = 1 + Math.min(dp[i - 1][j - 1], Math.min(dp[i - 1][j], dp[i][j - 1]));
+                    side = Math.max(side, dp[i][j]);
+                }   
+            }
+        }
+        return side * side;
+    }
+}
