9e12e5a26e6a52d88d7e9397f03e490048659561
CodeToFLAG <hxyqbsmile@gmail.com>
2018-07-21 18:16:12 +0800

    Update 312. Burst Balloons.java

--- 7.21.2018/312. Burst Balloons.java
+++ 7.21.2018/312. Burst Balloons.java
@@ -1,25 +1,55 @@
 class Solution {
-    public int maxCoins(int[] nn) {
-    	int n = nn.length;
-    	int[] nums = new int[n+2];
-    	for(int i=0; i<n; i++) nums[i+1] = nn[i];
-    	nums[0] = nums[n+1] = 1;
-
-        int[][] dp = new int[n+2][n+2];
-        
-        for(int j=2; j < dp.length; j++) {
-        	for(int i=0; i+j<dp.length; i++) {
-        		find(nums, dp, i, i+j);
-        	}
+    public int maxCoins(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
         }
-
-        return dp[0][n+1];
+        int n = nums.length + 2;
+        int[] balloons = new int[n];
+        int[][] dp = new int[n][n];
+        balloons[0] = 1;
+        balloons[n - 1] = 1;
+        for (int i = 1; i < n - 1; i++) {
+            balloons[i] = nums[i - 1];   
+        }
+        for (int j = 2; j < n; j++) {
+            for (int i = 0; i + j < n; i++) {
+                int temp = balloons[i] * balloons[i + j];
+                for (int k = i + 1; k < i + j; k++) {
+                    dp[i][i + j] = Math.max(dp[i][i + j], dp[i][k] + dp[k][i + j] + temp * balloons[k]);
+                    
+                }
+            }
+        }
+        return dp[0][n - 1];
     }
-    
-    private void find(int[] nums, int[][] dp, final int start, final int end) {
-        int r = nums[start]*nums[end];
-        for(int k=start+1; k<end; k++) {
-            dp[start][end] = Math.max(dp[start][end], dp[start][k] + dp[k][end] + r*nums[k]);
+}
+
+// use helper function is faster
+class Solution {
+    public int maxCoins(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        int n = nums.length + 2;
+        int[] balloons = new int[n];
+        int[][] dp = new int[n][n];
+        balloons[0] = 1;
+        balloons[n - 1] = 1;
+        for (int i = 1; i < n - 1; i++) {
+            balloons[i] = nums[i - 1];   
+        }
+        for (int j = 2; j < n; j++) { // 区间长度
+            for (int i = 0; i + j < n; i++) { // 区间开始位置
+                optimize(balloons, dp, i, i + j);
+            }
+        }
+        return dp[0][n - 1];
+    }
+    private void optimize(int[] balloons, int[][] dp, int start, int end) {
+        int temp = balloons[start] * balloons[end];
+        for (int k = start + 1; k < end; k++) { //最后burst的位置（注意，不是最开始戳破的位置）
+            dp[start][end] = Math.max(dp[start][end], dp[start][k] + dp[k][end] + temp * balloons[k]);
+
         }
     }
 }
