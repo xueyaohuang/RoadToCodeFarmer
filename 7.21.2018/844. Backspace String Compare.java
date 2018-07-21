d80f0f97e54e0588a9df9ead7b2dd42c74470af1
CodeToFLAG <hxyqbsmile@gmail.com>
2018-07-22 00:36:09 +0800

    Update 844. Backspace String Compare.java

--- 7.21.2018/844. Backspace String Compare.java
+++ 7.21.2018/844. Backspace String Compare.java
@@ -33,3 +33,33 @@ class Solution {
         return true;
     }
 }
+
+class Solution {
+    public boolean backspaceCompare(String S, String T) {
+        if (S == null || T == null) {
+            return false;
+        }
+        String s = modifyString(S);
+        String t = modifyString(T);
+        return s.equals(t);
+    }
+    private String modifyString(String str) {
+        int count = 0;
+        StringBuilder sb = new StringBuilder();
+        for (int i = str.length() - 1; i >= 0; i--) { // 倒序遍历，可以处理一开始就是#的情况
+            char c = str.charAt(i);
+            if (c == '#') {
+                count++;
+            }
+            else {
+                if (count > 0) {
+                    count--;
+                }
+                else {
+                    sb.append(c);
+                }
+            }
+        }
+        return sb.toString();
+    }
+}
