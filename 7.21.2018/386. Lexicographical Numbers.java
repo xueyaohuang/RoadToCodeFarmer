81b089138d52b409ed91e4c7bedbf2ef0e2cf402
CodeToFLAG <hxyqbsmile@gmail.com>
2018-07-21 12:26:14 +0800

    Update 386. Lexicographical Numbers.java

--- 7.21.2018/386. Lexicographical Numbers.java
+++ 7.21.2018/386. Lexicographical Numbers.java
@@ -23,7 +23,7 @@ class Solution {
 
 class Solution {
     public List<Integer> lexicalOrder(int n) {
-        Integer[] res = new Integer[n]; // 用Integer array很快，不知道为啥
+        Integer[] res = new Integer[n]; // 与上个解法一样，只是换用Integer array，Integer array很快，不知道为啥
         int cur = 1;
         for (int i = 0; i < n; i++) {
             res[i] = cur;
@@ -41,5 +41,23 @@ class Solution {
             }
         }
         return Arrays.asList(res);
+    }
+}
+
+class Solution {
+    public List<Integer> lexicalOrder(int n) {
+        List<Integer> res = new ArrayList<>();
+        lexicalOrderHelper(1, n, res);
+        return res;
+    }
+    private void lexicalOrderHelper(int cur, int n, List<Integer> res) { // preorder traversal
+        if (cur > n) {
+            return;
+        }
+        res.add(cur); // add root
+        lexicalOrderHelper(cur * 10, n, res); // add left
+        if (cur + 1 <= cur /10 * 10 + 9) {
+            lexicalOrderHelper(cur + 1, n, res); // add right
+        }
     }
 }
