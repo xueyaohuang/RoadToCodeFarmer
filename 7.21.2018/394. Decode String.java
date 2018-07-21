065f120c7532db7d25701035a9092045fa7f645f
CodeToFLAG <hxyqbsmile@gmail.com>
2018-07-21 18:33:06 +0800

    Update 394. Decode String.java

--- 7.21.2018/394. Decode String.java
+++ 7.21.2018/394. Decode String.java
@@ -1,1 +1,35 @@
-.
+class Solution {
+    public String decodeString(String s) {
+        Stack<Integer> countStack = new Stack<>();
+        Stack<String> strStack = new Stack<>();
+        int idx = 0;
+        String res = "";
+        while (idx < s.length()) {
+            if (Character.isDigit(s.charAt(idx))) {
+                int count = s.charAt(idx++) - '0';
+                while (Character.isDigit(s.charAt(idx))) {
+                    count = count * 10 + s.charAt(idx++) - '0';
+                }
+                countStack.push(count);
+            }
+            else if (s.charAt(idx) == '[') {
+                strStack.push(res);
+                res = "";
+                idx++;
+            }
+            else if (s.charAt(idx) == ']') {
+                StringBuilder temp = new StringBuilder(strStack.pop());
+                int repeat = countStack.pop();
+                for (int i = 0; i < repeat; i++) {
+                    temp.append(res);
+                }
+                res = temp.toString();
+                idx++;
+            }
+            else {
+                res += s.charAt(idx++);
+            }
+        }
+        return res;
+    }
+}
