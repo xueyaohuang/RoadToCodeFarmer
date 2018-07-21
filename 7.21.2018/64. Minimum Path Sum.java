b131aecf114d2cd41be54de238ed5e7934e5e964
CodeToFLAG <hxyqbsmile@gmail.com>
2018-07-21 19:42:29 +0800

    Update 64. Minimum Path Sum.java

--- 7.21.2018/64. Minimum Path Sum.java
+++ 7.21.2018/64. Minimum Path Sum.java
@@ -1,1 +1,43 @@
-.
+class Solution {
+    public int minPathSum(int[][] grid) {
+        if (grid == null || grid.length == 0) {
+            return 0;
+        }
+        int row = grid.length;
+        int col = grid[0].length;
+        for (int i = 1; i < col; i++) {
+            grid[0][i] = grid[0][i] + grid[0][i - 1];
+        }
+        for (int i = 1; i < row; i++) {
+            grid[i][0] = grid[i][0] + grid[i - 1][0];
+        }
+        for (int i = 1; i < row; i++) {
+            for (int j = 1; j < col; j++) {
+                grid[i][j] = grid[i][j] + Math.min(grid[i - 1][j], grid[i][j - 1]);
+            }
+        }
+        return grid[row - 1][col - 1];
+    }
+}
+
+class Solution {
+    public int minPathSum(int[][] grid) {
+        if (grid == null || grid.length == 0) {
+            return 0;
+        }
+        int m = grid.length;
+        int n = grid[0].length;
+        int[] res = new int[n];
+        res[0] = grid[0][0];
+        for (int j = 1; j < n; j++) {
+            res[j] = grid[0][j] + res[j - 1];
+        }
+        for (int i = 1; i < m; i++) {
+            res[0] += grid[i][0];
+            for (int j = 1; j < n; j++) {
+                res[j] = grid[i][j] + Math.min(res[j], res[j - 1]);
+            }
+        }
+        return res[n - 1];
+    }
+}
