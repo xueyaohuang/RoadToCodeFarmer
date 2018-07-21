efaf7fc7c42644c3ad0243926f3e2107eee3ba74
CodeToFLAG <hxyqbsmile@gmail.com>
2018-07-22 00:17:21 +0800

    Create 389. Find the Difference.java

---
+++ 7.21.2018/389. Find the Difference.java
@@ -0,0 +1,17 @@
+class Solution {
+    public char findTheDifference(String s, String t) {
+        int[] freq = new int[26];
+        for (char c : s.toCharArray()) {
+            freq[c - 'a']++;
+        }
+        for (char c : t.toCharArray()) {
+            freq[c - 'a']--;
+        }
+        for (int i = 0; i < 26; i++) {
+            if (freq[i] < 0) {
+                return (char)(i + 'a');
+            }
+        }
+        return '\0';
+    }
+}
