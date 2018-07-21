49f37d4da27a1e8e31e756421da7ae611ce26c6b
CodeToFLAG <hxyqbsmile@gmail.com>
2018-07-21 20:18:03 +0800

    Create 251. Flatten 2D Vector.java

---
+++ 7.21.2018/251. Flatten 2D Vector.java
@@ -0,0 +1,20 @@
+public class Vector2D {
+
+    private Iterator<List<Integer>> i;
+    private Iterator<Integer> j;
+
+    public Vector2D(List<List<Integer>> vec2d) {
+        i = vec2d.iterator();
+    }
+
+    public int next() {
+        hasNext();
+        return j.next();
+    }
+
+    public boolean hasNext() {
+        while ((j == null || !j.hasNext()) && i.hasNext())
+            j = i.next().iterator();
+        return j != null && j.hasNext();
+    }
+}
