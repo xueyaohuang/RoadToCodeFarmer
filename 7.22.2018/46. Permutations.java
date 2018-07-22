c065798813aa6275c4d6c5cc9388de2dc5a5e280
CodeToFLAG <hxyqbsmile@gmail.com>
2018-07-22 20:23:57 +0800

    Update 46. Permutations.java

--- 7.22.2018/46. Permutations.java
+++ 7.22.2018/46. Permutations.java
@@ -1,21 +1,46 @@
 // backtrack
 class Solution {
     public List<List<Integer>> permute(int[] nums) {
-        List<List<Integer>> list = new ArrayList<>();
-        backtrack(nums, list, new ArrayList<Integer>());
-        return list;
+        List<List<Integer>> res = new ArrayList<>();
+        backtrack(nums, res, new ArrayList<Integer>());
+        return res;
     }
     
-    private void backtrack(int[] nums, List<List<Integer>> list, List<Integer> temp) {
+    private void backtrack(int[] nums, List<List<Integer>> res, List<Integer> temp) {
         if (temp.size() == nums.length) {
-            list.add(new ArrayList<Integer>(temp));
+            res.add(new ArrayList<Integer>(temp));
         }
         for (int i = 0; i < nums.length; i++) {
             if (!temp.contains(nums[i])) {
                 temp.add(nums[i]);
-                backtrack(nums, list, temp);
+                backtrack(nums, res, temp);
+                temp.remove(temp.size() - 1); // 以nums[i]开头的都穷举完了，所以删了换下一个开头的。
+            }
+        }
+    }
+}
+
+// DFS 和backtrack一样，只不过用visited代替!temp.contains(nums[i])来检查nums[i]是否用过了
+class Solution {
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        boolean[] visited = new boolean[nums.length];
+        dfs(nums, res, new ArrayList<Integer>(), visited);
+        return res;
+    }
+    
+    private void dfs(int[] nums, List<List<Integer>> res, List<Integer> temp, boolean[] visited) {
+        if (temp.size() == nums.length) {
+            res.add(new ArrayList<Integer>(temp));
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if (!visited[i]) {
+                visited[i] = true;
+                temp.add(nums[i]);
+                dfs(nums, res, temp, visited);
                 temp.remove(temp.size() - 1);
-            } 
+                visited[i] = false;
+            }
         }
     }
 }
