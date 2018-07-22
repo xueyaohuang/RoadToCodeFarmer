f6f5c92d5425a19f540706768ebdab768bbce1ea
CodeToFLAG <hxyqbsmile@gmail.com>
2018-07-22 15:40:58 +0800

    Update 259. 3Sum Smaller.java

--- 7.22.2018/259. 3Sum Smaller.java
+++ 7.22.2018/259. 3Sum Smaller.java
@@ -1,1 +1,24 @@
-.
+class Solution {
+    public int threeSumSmaller(int[] nums, int target) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        int count = 0;
+        Arrays.sort(nums);
+        for (int i = 0; i + 2 < nums.length; i++) {
+            int temp = target - nums[i];
+            int j = i + 1;
+            int k = nums.length - 1;
+            while (j < k) {
+                if (nums[j] + nums[k] < temp) {
+                    count += k - j;
+                    j++;
+                }
+                else {
+                    k--;
+                }
+            }
+        }
+        return count;
+    }
+}
