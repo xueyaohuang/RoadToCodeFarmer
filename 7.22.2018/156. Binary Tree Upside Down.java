8591f86d1598be71f03db2a41bd5c0d7e1114863
CodeToFLAG <hxyqbsmile@gmail.com>
2018-07-22 14:44:49 +0800

    Update 156. Binary Tree Upside Down.java

--- 7.22.2018/156. Binary Tree Upside Down.java
+++ 7.22.2018/156. Binary Tree Upside Down.java
@@ -1,1 +1,40 @@
-.
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public TreeNode upsideDownBinaryTree(TreeNode root) {
+        TreeNode cur = root;
+        TreeNode prev = null;
+        TreeNode next = null;
+        TreeNode temp = null;
+        while (cur != null) {
+            next = cur.left;
+            cur.left = temp;
+            temp = cur.right;
+            cur.right = prev;
+            prev = cur;
+            cur = next;
+        }
+        return prev;
+    }
+}
+
+class Solution {
+    public TreeNode upsideDownBinaryTree(TreeNode root) {
+        if (root == null || root.left == null) {
+            return root;
+        }
+        TreeNode node = upsideDownBinaryTree(root.left);
+        root.left.left = root.right;
+        root.left.right = root;
+        root.left = null;
+        root.right = null;
+        return node;
+    }
+}
