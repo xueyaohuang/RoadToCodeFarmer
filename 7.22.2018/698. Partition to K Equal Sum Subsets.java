4617f05a90df1c0d52bc6f4365b90cc2ad028be6
CodeToFLAG <hxyqbsmile@gmail.com>
2018-07-22 17:09:54 +0800

    Update 698. Partition to K Equal Sum Subsets.java

--- 7.22.2018/698. Partition to K Equal Sum Subsets.java
+++ 7.22.2018/698. Partition to K Equal Sum Subsets.java
@@ -11,23 +11,24 @@ class Solution {
             return false;
         }
         boolean[] visited = new boolean[nums.length];
-        return helper(nums, 0, k, sum / k, visited, 0);
+        return partitionHelper(nums, 0, k, sum / k, visited, 0);
     }
     
-    private boolean helper(int[] nums, int pos, int k, int target, boolean[] visited, int sum) {
+    private boolean partitionHelper(int[] nums, int start, int k, int target, boolean[] visited, int sum) {
         if (k == 1) {
             return true;
         }
-        for (int i = pos; i < nums.length; i++) {
+        for (int i = start; i < nums.length; i++) {
             if (!visited[i]) {
                 visited[i] = true;
                 sum += nums[i];
                 if (sum == target) {
-                    if (helper(nums, 0, k - 1, target, visited, 0)) {
+                    if (partitionHelper(nums, 0, k - 1, target, visited, 0)) {
                         return true;
                     }
-                } else if (sum < target) {
-                    if (helper(nums, i + 1, k, target, visited, sum)) {
+                }
+                else if (sum < target) {
+                    if (partitionHelper(nums, i + 1, k, target, visited, sum)) {
                         return true;
                     }
                 }
