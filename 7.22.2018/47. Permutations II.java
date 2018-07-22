d4ead298a8878f7f7ef7b6e810f272939e31e48f
CodeToFLAG <hxyqbsmile@gmail.com>
2018-07-22 20:39:30 +0800

    Update 47. Permutations II.java

--- 7.22.2018/47. Permutations II.java
+++ 7.22.2018/47. Permutations II.java
@@ -1,1 +1,30 @@
-.
+class Solution {
+    public List<List<Integer>> permuteUnique(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();       
+        List<Integer> list = new ArrayList<>();
+        int[] visited = new int[nums.length];
+        Arrays.sort(nums);
+        dfs(nums, result, list, visited);
+        return result;
+    }
+    
+    void dfs(int[] nums, List<List<Integer>> result, List<Integer> list, int[] visited) {       
+        if(list.size() == nums.length) {
+            result.add(new ArrayList(list));
+            return;
+        }
+        
+        for(int i = 0; i < nums.length; ++i) {
+            if (i > 0 && nums[i - 1] == nums[i] && visited[i - 1] == 1) {
+                continue;
+            }
+            if(visited[i] == 0) {
+                visited[i] = 1;
+                list.add(nums[i]);
+                dfs(nums, result, list, visited);
+                list.remove(list.size() - 1);
+                visited[i] = 0;
+            }
+        }
+    }
+}
