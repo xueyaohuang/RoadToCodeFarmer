7639baf2cc1755ada267df93eb6f098b19ccb1be
CodeToFLAG <hxyqbsmile@gmail.com>
2018-07-23 00:42:10 +0800

    Update 39. Combination Sum.java

--- 7.20.2018/39. Combination Sum.java
+++ 7.20.2018/39. Combination Sum.java
@@ -1,22 +1,19 @@
 class Solution {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        // pos for the index is needed but the visited array is not nesseary B/C duplicates are find
-        List<List<Integer>> res=new ArrayList<>();
-        combinationSum(0, new ArrayList<>(), candidates, target, res);
+        List<List<Integer>> res = new ArrayList<>();
+        backtrack(candidates, res, new ArrayList<Integer>(), target, 0);// Permutations问题没有start/position这个参数，因为顺序matters，combination里面需要start/position，因为顺序改了算重复。不需要visited，因为可以有重复元素。
         return res;
     }
-    private void combinationSum(int pos, List<Integer> cur, int[] candidates, int target, List<List<Integer>> res){
-        // if(target<0) return;
-        if(target==0) {
-            res.add(new ArrayList<>(cur));
+    private void backtrack(int[] candidates, List<List<Integer>> res, List<Integer> temp, int target, int start) {
+        if (target == 0) { // target随加入的candidate改变，退出条件是target == 0，这样就不用检查list的元素和是target。
+            res.add(new ArrayList<Integer>(temp));
             return;
         }
-        
-        for(int i=pos; i<candidates.length; i++){
-            if(target>=candidates[i]){
-                cur.add(candidates[i]);
-                combinationSum(i, cur, candidates, target-candidates[i], res);
-                cur.remove(cur.size()-1);
+        for (int i = start; i < candidates.length; i++) {
+            if (target >= candidates[i]) { // target >= candidates[i]才能加，避免检查target < 0 的情况。
+                temp.add(candidates[i]);
+                backtrack(candidates, res, temp, target - candidates[i], i);
+                temp.remove(temp.size() - 1);
             }
         }
     }
