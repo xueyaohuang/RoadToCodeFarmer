fc1d11284bf4702767a3cf27b0177ec5e86a72bc
CodeToFLAG <hxyqbsmile@gmail.com>
2018-07-23 13:41:56 +0800

    Update 377. Combination Sum IV.java

--- 7.20.2018/377. Combination Sum IV.java
+++ 7.20.2018/377. Combination Sum IV.java
@@ -12,3 +12,11 @@ class Solution {
         return dp[target];
     }
 }
+/*
+The problem with negative numbers is that now the combinations could be potentially of infinite length. Think about nums = [-1, 1] and
+target = 1. We can have all sequences of arbitrary length that follow the patterns -1, 1, -1, 1, ..., -1, 1, 1 and
+1, -1, 1, -1, ..., 1, -1, 1 (there are also others, of course, just to give an example). So we should limit the length of the combination
+sequence, so as to give a bound to the problem.
+*/
+// https://leetcode.com/problems/combination-sum-iv/discuss/85038/JAVA:-follow-up-using-recursion-and-memorization.
+// use hashmap and recursion.
