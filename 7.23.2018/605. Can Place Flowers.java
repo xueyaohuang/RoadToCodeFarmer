2155c936e44dc6a7af611984acbf5eca53472d1a
CodeToFLAG <hxyqbsmile@gmail.com>
2018-07-23 20:23:37 +0800

    Update 605. Can Place Flowers.java

--- 7.23.2018/605. Can Place Flowers.java
+++ 7.23.2018/605. Can Place Flowers.java
@@ -1,1 +1,47 @@
-.
+class Solution {
+    public boolean canPlaceFlowers(int[] flowerbed, int n) {
+        if (n == 0) {
+            return true;
+        }
+        if (flowerbed.length == 1 && n <= 1) {
+            return flowerbed[0] == 0;
+        }
+        int count = 0;
+        for (int i = 0; i < flowerbed.length; i++) {
+            if (i > 0 && i < flowerbed.length - 1 && flowerbed[i] == 0 && flowerbed[i + 1] == 0 && flowerbed[i - 1] == 0) {
+                flowerbed[i] = 1;
+                count++;
+                continue;
+            }
+            if (i == 0 && flowerbed[i] == 0 && flowerbed[i + 1] == 0) {
+                flowerbed[i] = 1;
+                count++;
+                continue;
+            }
+            if (i == flowerbed.length - 1 && flowerbed[i] == 0 && flowerbed[i - 1] == 0) {
+                flowerbed[i] = 1;
+                count++;
+            }
+            if (count == n) {
+                break;
+            }
+        }
+        return count >= n;
+    }
+}
+
+class Solution {
+    public boolean canPlaceFlowers(int[] flowerbed, int n) {
+        int count = 0;
+        for (int i = 0; i < flowerbed.length; i++) {
+            if (flowerbed[i] == 0 && (i == 0 || flowerbed[i - 1] == 0) && (i == flowerbed.length - 1 || flowerbed[i + 1] == 0)) {
+                flowerbed[i] = 1;
+                count++;
+            }
+            if (count == n) {
+                return true;
+            }
+        }
+        return count >= n;
+    }
+}
