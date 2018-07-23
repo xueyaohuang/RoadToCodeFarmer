b7a4b82bf2637eac4d290dc38aad2e035ac802c2
CodeToFLAG <hxyqbsmile@gmail.com>
2018-07-23 19:54:25 +0800

    Update 633. Sum of Square Numbers.java

--- 7.23.2018/633. Sum of Square Numbers.java
+++ 7.23.2018/633. Sum of Square Numbers.java
@@ -1,1 +1,33 @@
-.
+class Solution {
+    public boolean judgeSquareSum(int c) {
+        int i = 0;
+        int j = (int) Math.sqrt(c);
+        while (i <= j) {
+            int sum = i * i + j * j;
+            if (sum == c) {
+                return true;
+            }
+            else if (sum > c) {
+                j--;
+            }
+            else {
+                i++;
+            }
+        }
+        return false;
+    }
+}
+
+class Solution {
+    public boolean judgeSquareSum(int c) {
+        for (int i = 0; i * i <= c / 2; i++) {
+            int rest = c - i * i;
+            int root = (int)Math.sqrt(rest);
+            if (root * root == rest) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
+
