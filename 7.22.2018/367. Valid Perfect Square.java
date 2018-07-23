547345975870853dba33612a6772b5526a073fd4
CodeToFLAG <hxyqbsmile@gmail.com>
2018-07-23 13:54:29 +0800

    Update 367. Valid Perfect Square.java

--- 7.22.2018/367. Valid Perfect Square.java
+++ 7.22.2018/367. Valid Perfect Square.java
@@ -1,1 +1,16 @@
-.
+class Solution {
+    public boolean isPerfectSquare(int num) {
+        int start = 1;
+        int end = num;
+        while (start < end) {
+            int mid = (start + end + 1) / 2;
+            if (mid > num / mid) {
+                end = mid - 1;
+            }
+            else {
+                start = mid;
+            }
+        }
+        return start * start == num;
+    }
+}
