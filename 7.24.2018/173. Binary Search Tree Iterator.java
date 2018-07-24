2d7a93e25806d65e8b1a8c2b9e358c58cb320017
CodeToFLAG <hxyqbsmile@gmail.com>
2018-07-24 10:15:44 +0800

    Update 173. Binary Search Tree Iterator.java

--- 7.24.2018/173. Binary Search Tree Iterator.java
+++ 7.24.2018/173. Binary Search Tree Iterator.java
@@ -40,7 +40,11 @@ public class BSTIterator {
     }
 }
 /*
-This is in fact average O(1) time. The while loop is misleading you to think it is not. Think about the number of times a node has been visited after iterating the whole tree. Each node has been visited twice. In some cases the while loop doesn't execute, so that node at that call is only visited once. Where does the other visit go? It goes to the while loop when you visit another node. That's why it's "average" O(1) time.
+This is in fact average O(1) time. Think about the number of times a node has been visited
+after iterating the whole tree. Each node has been visited twice. In some cases the
+while loop doesn't execute, so that node at that call is only visited once. Where
+does the other visit go? It goes to the while loop when you visit another node.
+That's why it's "average" O(1) time.
 */
 
 /**
