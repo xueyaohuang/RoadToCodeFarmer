d6990be43138463f3b126f8955186627a5531dbf
CodeToFLAG <hxyqbsmile@gmail.com>
2018-07-24 13:45:48 +0800

    Update 293. Flip Game.java

--- 7.24.2018/293. Flip Game.java
+++ 7.24.2018/293. Flip Game.java
@@ -1,1 +1,13 @@
-.
+class Solution {
+    public List<String> generatePossibleNextMoves(String s) {
+        List<String> list = new ArrayList<>();
+        for (int i = 0; i + 1 < s.length(); i++) {
+            if (s.substring(i, i + 2).equals("++")) {
+                StringBuilder sb = new StringBuilder(s);
+                sb.replace(i, i + 2, "--");
+                list.add(sb.toString());
+            }
+        }
+        return list;
+    }
+}
