feb2fc3875ed3710d49c807ee258e2a7518e1344
CodeToFLAG <hxyqbsmile@gmail.com>
2018-07-25 23:55:39 +0800

    Update 270. Closest Binary Search Tree Value.java

--- 7.25.2018/270. Closest Binary Search Tree Value.java
+++ 7.25.2018/270. Closest Binary Search Tree Value.java
@@ -1,1 +1,26 @@
-.
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public int closestValue(TreeNode root, double target) {
+        if (root.val == target) {
+            return root.val;
+        }
+        double diff = Math.abs(root.val - target);
+        if (target < root.val && root.left != null) {
+            int left = closestValue(root.left, target);
+            return diff > Math.abs(target - left) ? left : root.val;
+        }
+        if (target > root.val && root.right != null) {
+            int right = closestValue(root.right, target);
+            return diff > Math.abs(target - right) ? right : root.val;
+        }
+        return root.val;
+    }
+}
