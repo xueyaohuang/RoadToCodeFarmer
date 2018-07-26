f6192ebd646ffd6dcceba1fc39fa5cc04f30f4d9
CodeToFLAG <hxyqbsmile@gmail.com>
2018-07-27 00:00:06 +0800

    Update 482. License Key Formatting.java

--- 7.28.2018/482. License Key Formatting.java
+++ 7.28.2018/482. License Key Formatting.java
@@ -1,1 +1,27 @@
-.
+class Solution {
+    public String licenseKeyFormatting(String S, int K) {
+        int count = 0;
+        StringBuilder sb = new StringBuilder();
+        int len = S.length();
+        for (int i = len - 1; i >= 0; i--) {
+            char c = S.charAt(i);
+            if (c != '-') {
+                if (Character.isLowerCase(c)) {
+                    sb.append(Character.toUpperCase(c));
+                    count++;
+                } else {
+                    sb.append(c);
+                    count++;
+                }
+            }
+            if (count == K) {
+                sb.append('-');
+                count = 0;
+            }
+        }
+        if (sb.length() != 0 && sb.charAt(sb.length() - 1) == '-') {
+            sb.deleteCharAt(sb.length() - 1);
+        }
+        return sb.reverse().toString();
+    }
+}
