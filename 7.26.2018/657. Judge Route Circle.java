d2f7b71b56c0c715705a3fde4a0f741e3bc3f143
CodeToFLAG <hxyqbsmile@gmail.com>
2018-07-26 20:53:25 +0800

    Update 657. Judge Route Circle.java

--- 7.26.2018/657. Judge Route Circle.java
+++ 7.26.2018/657. Judge Route Circle.java
@@ -1,1 +1,21 @@
-.
+class Solution {
+    public boolean judgeCircle(String moves) {
+        if (moves == null || moves.length() % 2 == 1) {
+            return false;
+        }
+        int x = 0;
+        int y = 0;
+        for (char c : moves.toCharArray()) {
+            if (c == 'U') {
+                y++;
+            } else if (c == 'D') {
+                y--;
+            } else if (c == 'R') {
+                x++;
+            } else {
+                x--;
+            } 
+        }
+        return x == 0 && y == 0;
+    }
+}
