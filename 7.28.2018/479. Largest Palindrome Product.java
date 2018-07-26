0a3645baf0edd5c3ce0a090f6c35ac855842b498
CodeToFLAG <hxyqbsmile@gmail.com>
2018-07-27 00:48:09 +0800

    Update 479. Largest Palindrome Product.java

--- 7.28.2018/479. Largest Palindrome Product.java
+++ 7.28.2018/479. Largest Palindrome Product.java
@@ -1,44 +1,48 @@
 class Solution {
     public int largestPalindrome(int n) {
-        if (n == 1) return 9;
-        long max = (long)Math.pow(10, n);
-        long min = max/ 10;
-        long temp = 0;
-        max--;
-        
-        for (long num = max - 1; num >= min; num--){
-            temp = makePal(num);
-            if (isFactor(temp, min, max)){
-                return (int)(temp % 1337);
+        if (n == 1) {
+            return 9;
+        }
+        if (n == 2) {
+            return 987;
+        }
+        long max = (long) Math.pow(10, n) - 1;
+        long min = (long) Math.pow(10, n - 1);
+        for (long i = max; i >= min; i--) {
+            long palProd = makePalProd(i);
+            if (isProduct(palProd, min, max)) {
+                return (int) (palProd % 1337);
             }
         }
         return -1;
     }
     
-    public long makePal(long num){
+    private long makePalProd(long num) {
         long res = num;
-        while(num > 0){
+        while (num != 0) {
             res = res * 10 + num % 10;
             num /= 10;
         }
         return res;
     }
     
-    public boolean isFactor(long product, long min, long max){
-        long mid = (long) Math.sqrt(product);
-        if (mid > max || mid < min) return false;
-        long left = mid;
-        long right = mid;
-        long temp = left * right;
-        while (temp != product){
-            if (temp < product){
-                right++;
-                if (right > max) return false;
-            }else{
+    private boolean isProduct(long target, long min, long max) {
+        long start = (long) Math.sqrt(target);
+        long left = start;
+        long right = start;
+        while (left * right != target) {
+            if (left * right > target) {
                 left--;
-                if (left < min) return false;
+                if (left < min) {
+                    return false;
+                }
             }
-            temp = left * right;
+            if (left * right < target) {
+                right++;
+                if (right > max) {
+                    return false;
+                }
+            }
         }
         return true;
     }
