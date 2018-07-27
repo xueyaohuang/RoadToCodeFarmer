d765c830424cb90cc044a4cf1674be9a8036931a
CodeToFLAG <hxyqbsmile@gmail.com>
2018-07-28 01:43:00 +0800

    Update 737. Sentence Similarity II.java

--- 7.28.2018/737. Sentence Similarity II.java
+++ 7.28.2018/737. Sentence Similarity II.java
@@ -1,33 +1,161 @@
+// 四中解法都值得学习
+
+// 最秀解法
 class Solution {
     public boolean areSentencesSimilarTwo(String[] words1, String[] words2, String[][] pairs) {
-        if (words1.length != words2.length){
+        if (words1.length != words2.length) {
             return false;
         }
         Map<String, String> map = new HashMap<>();
-        for (String[] pair : pairs){
-            String node1 = findBoss(map, pair[0]);
-            String node2 = findBoss(map, pair[1]);
-            if (node1 == null || node2 == null || !node1.equals(node2)){
-                map.put(node1, node2);
+        for (String[] pair : pairs) {
+            String s1 = findRoot(map, pair[0]);
+            String s2 = findRoot(map, pair[1]);
+            if (s1 == null || !s1.equals(s2)) {
+                map.put(s1, s2);
             }
         }
-        
-        for (int i = 0; i < words1.length; i++){
-            String node1 = findBoss(map, words1[i]);
-            String node2 = findBoss(map, words2[i]);
-            if (node1 == null || node2 == null || !node1.equals(node2)){
+        for (int i = 0; i < words1.length; i++) {
+            String s1 = findRoot(map, words1[i]);          
+            String s2 = findRoot(map, words2[i]);
+            if (s1 == null || !s1.equals(s2)) {
                 return false;
             }
         }
         return true;
     }
-    
-    private String findBoss(Map<String, String> map, String word){
-        String boss = map.get(word);
-        while (boss!= null){
-            word = boss;
-            boss = map.get(word);
+    private String findRoot(Map<String, String> map, String s) {
+        String t = map.get(s);
+        while (t != null) {
+            s = t;
+            t = map.get(s);
         }
-        return word;
+        return s;
     }
 }
+
+// union find
+class Solution {
+    
+    class UnionFind {
+        
+        int[] childParent;
+        
+        public UnionFind(int size) {
+            childParent = new int[size];
+            for (int i = 0; i < size; i++) {
+                childParent[i] = i;
+            }
+        }
+        
+        public int findRoot(int child) {
+            while (child != childParent[child]) {
+                child = childParent[child];
+            }
+            return child;
+        }
+        
+        public boolean isConnected(Integer a, Integer b) {
+            if (a == null || b == null) {
+                return false;
+            }
+            int rootA = findRoot(a);
+            int rootB = findRoot(b);
+            return rootA == rootB;
+        }
+        
+        public void connect(int a, int b) {
+            int rootA = findRoot(a);
+            int rootB = findRoot(b);
+            childParent[rootA] = rootB;
+        }      
+    }
+    
+    public boolean areSentencesSimilarTwo(String[] words1, String[] words2, String[][] pairs) {
+        
+        if (words1.length != words2.length) {
+            return false;
+        }
+        
+        int size = 0;
+        Map<String, Integer> map = new HashMap<>();
+        
+        for (String[] pair : pairs) {
+            if (!map.containsKey(pair[0])) {
+                map.put(pair[0], size);
+                size++;
+            }
+            if (!map.containsKey(pair[1])) {
+                map.put(pair[1], size);
+                size++;
+            }
+        }
+        
+        UnionFind uf = new UnionFind(size);
+        
+        for (String[] pair : pairs) {
+            uf.connect(map.get(pair[0]), map.get(pair[1]));
+        }
+        
+        for (int i = 0; i < words1.length; i++) {
+            if (words1[i].equals(words2[i])) {
+                continue;
+            }
+            if (!uf.isConnected(map.get(words1[i]), map.get(words2[i]))) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
+// BFS slowest
+class Solution {
+    public boolean areSentencesSimilarTwo(String[] words1, String[] words2, String[][] pairs) {
+        if (words1.length != words2.length) {
+            return false;
+        }
+        Map<String, Set<String>> map = new HashMap<>();
+        for (String[] strs : pairs) {
+            map.putIfAbsent(strs[0], new HashSet<String>());
+            map.putIfAbsent(strs[1], new HashSet<String>());
+            map.get(strs[0]).add(strs[1]);
+            map.get(strs[1]).add(strs[0]);
+        }
+        for (int i = 0; i < words1.length; i++) {
+            if (words1[i].equals(words2[i])) {
+                continue;
+            } 
+            if (!bfs(words1[i], words2[i], map, new HashSet<>())) {
+                return false;
+            } 
+        }
+        return true;
+    }
+    private boolean bfs(String s, String t, Map<String, Set<String>> map, Set<String> visited) {
+        if (!map.containsKey(s)) {
+            return false;
+        }
+        if (map.get(s).contains(t)) {
+            return true;
+        }
+        Queue<String> queue = new LinkedList<>();
+        queue.add(s);
+        visited.add(s);
+        while (!queue.isEmpty()) {
+            String str = queue.poll();
+            for (String ss : map.get(str)) {
+                if (!visited.contains(ss)) {
+                    visited.add(ss);
+                    if (ss.equals(t)) {
+                        return true;
+                    }
+                    queue.add(ss);
+                }
+            }
+        }
+        return false;
+    }
+}
+
+// DFS
+
