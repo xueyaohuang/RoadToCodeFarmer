ac8c0ccf1907f0074c335721f9b686056423c69b
CodeToFLAG <hxyqbsmile@gmail.com>
2018-07-30 01:13:32 +0800

    Update 27. Remove Element.java

--- 7.29.2018/27. Remove Element.java
+++ 7.29.2018/27. Remove Element.java
@@ -1,1 +1,12 @@
-.
+public class Solution {
+    public int removeElement(int[] nums, int val) {
+        int count = 0;
+        for (int i = 0; i < nums.length; i++){
+            if (nums[i] != val){
+                nums[count] = nums[i];
+                count++;
+            }
+        }
+        return count;       
+    }
+}
