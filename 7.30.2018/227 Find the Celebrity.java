205e9a675604f91a9fa6c95f2b4ca050a75a9312
CodeToFLAG <hxyqbsmile@gmail.com>
2018-07-30 23:59:34 +0800

    Create 227 Find the Celebrity.java

---
+++ 7.30.2018/227 Find the Celebrity.java
@@ -0,0 +1,36 @@
+/* The knows API is defined in the parent class Relation.
+      boolean knows(int a, int b); */
+
+public class Solution extends Relation {
+    public int findCelebrity(int n) {
+        int c = 0;
+        for (int i = 1; i < n; i++) {
+            if (knows(c, i)) {
+                c = i;
+            }
+        }
+        // indices before c and after c cannot be celebrity now.
+        // indices before c knows other people; indices after c
+        // are people c doesn't know.
+        
+        // 第一个for循环后，选出了候选人，并且知道c后面的人，c都不认识。还需要check'：
+        // 1. c后面的人都认识c
+        // 2. c前面的人都认识c
+        // 3. c前面的人，c都不认识。
+        
+        
+        
+        // Now just check whether c is celebrity or not.
+        for (int i = c + 1; i < n; i++) {
+            if (!knows(i, c)) {
+                return -1;
+            }
+        }
+        for (int i = 0; i < c; i++) {
+            if (knows(c, i) || !knows(i, c)) {
+                return -1;
+            }
+        }
+        return c;        
+    }
+}
