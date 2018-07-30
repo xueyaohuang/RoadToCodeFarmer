64fa36a643a9006d20415a34482d4a1e0475f73d
CodeToFLAG <hxyqbsmile@gmail.com>
2018-07-30 23:59:01 +0800

    Create 189. Rotate Array.java

---
+++ 7.30.2018/189. Rotate Array.java
@@ -0,0 +1,37 @@
+class Solution {
+    public void rotate(int[] nums, int k) {
+        if (nums == null || nums.length == 0) {
+            return;
+        }
+        k %= nums.length;
+        reverse(nums, 0, nums.length - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, nums.length - 1);
+    }
+    
+    private void reverse(int[] nums, int start, int end) {
+        while (start < end) {
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++;
+            end--;
+        }
+    }
+}
+
+class Solution {
+    public void rotate(int[] nums, int k) {
+        k %= nums.length;
+        int[] temp = new int[k];
+        for (int i = 0; i < k; i++) {
+            temp[i] = nums[nums.length - k + i];
+        }
+        for (int i = nums.length - 1; i >= nums.length - 1 - k; i--) {
+            nums[i] = nums[i - k];
+        }
+        for (int i = 0; i < k; i++) {
+            nums[i] = temp[i];
+        }
+    }
+}
