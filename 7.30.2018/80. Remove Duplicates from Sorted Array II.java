1aab40ccf2c7f9306e637c9b5b25caa53c6ae2f2
CodeToFLAG <hxyqbsmile@gmail.com>
2018-07-30 21:43:02 +0800

    Create 80. Remove Duplicates from Sorted Array II.java

---
+++ 7.30.2018/80. Remove Duplicates from Sorted Array II.java
@@ -0,0 +1,45 @@
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        if (nums == null) {
+            return 0;
+        }
+        if (nums.length <= 2) {
+            return nums.length;
+        }
+        int j = 2;
+        for (int i = 2; i < nums.length; i++) {
+            // 注意这里是j - 2，不是i - 2，we have to look k steps backward in new updated array.
+            if (nums[i] > nums[j - 2]) { // 把j - 2换成j - k就推广到最多k个重复。
+                nums[j] = nums[i];
+                j++;
+            }
+        }
+        return j;
+    }
+}
+
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        if (nums == null) {
+            return 0;
+        }
+        if (nums.length <= 2) {
+            return nums.length;
+        }
+        int j = 1;
+        int count = 1;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] == nums[i - 1]) {
+                if (count < 2) { //把2换成k就推广到最多k个重复。
+                    nums[j++] = nums[i];
+                    count++;
+                }
+                
+            } else {               
+                nums[j++] = nums[i];                
+                count = 1;
+            }
+        }
+        return j;
+    }
+}
