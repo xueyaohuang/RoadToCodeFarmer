bab264be0fa5f1f3e19f7e13670aa9c5a3de468f
CodeToFLAG <hxyqbsmile@gmail.com>
2018-08-10 01:42:21 +0800

    Create 137. Single Number II.java

---
+++ 137. Single Number II.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        int res = 0;
+        for (int i = 0; i < 32; i++) {
+            int count = 0;
+            for (int j = 0; j < nums.length; j++) {
+                count += (nums[j] >> i) & 1;
+            }
+            count %= 3;
+            res |= (count << i);
+        }
+        return res;
+    }
+}
+/*
+The usual bit manipulation code is bit hard to get and replicate. I like to think about the number in 32 bits and just count how many 1s are there in each bit, and sum %= 3 will clear it once it reaches 3. After running for all the numbers for each bit, if we have a 1, then that 1 belongs to the single number, we can simply move it back to its spot by doing ans |= sum << i;
+
+This has complexity of O(32n), which is essentially O(n) and very easy to think and implement. Plus, you get a general solution for any times of occurrence. Say all the numbers have 5 times, just do sum %= 5.
+*/
