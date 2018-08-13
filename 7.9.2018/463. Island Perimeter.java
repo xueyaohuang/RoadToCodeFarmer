cd6f1ac9d95b049991721e9d38e3aa5622065ffa
CodeToFLAG <hxyqbsmile@gmail.com>
2018-08-14 01:14:24 +0800

    Update 463. Island Perimeter.java

--- 7.9.2018/463. Island Perimeter.java
+++ 7.9.2018/463. Island Perimeter.java
@@ -52,3 +52,45 @@ class Solution {
         return perimrter;
     }
 }
+
+class Solution {
+    private int res;
+    public int islandPerimeter(int[][] grid) {
+        if (grid == null || grid.length == 0) {
+            return 0;
+        }
+        
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j] == 1) {
+                    floodFill(grid, i, j);
+                    
+                }
+            }
+        }
+        return res;
+    }
+    
+    private void floodFill(int[][] grid, int i, int j) {
+        if (i < 0 || i >= grid.length || j < 0 || j >= grid[0].length || grid[i][j] != 1) {
+            return;
+        }
+        grid[i][j] = -1;
+        if (i == 0 || grid[i - 1][j] == 0) {
+            res++;
+        }
+        if (i == grid.length - 1 || grid[i + 1][j] == 0) {
+            res++;
+        }
+        if (j == 0 || grid[i][j - 1] == 0) {
+            res++;
+        }
+        if (j == grid[0].length - 1 || grid[i][j + 1] == 0) {
+            res++;
+        }
+        floodFill(grid, i - 1, j);
+        floodFill(grid, i + 1, j);
+        floodFill(grid, i, j - 1);
+        floodFill(grid, i, j + 1);
+    }
+}
