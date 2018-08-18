b410256607c195df6fe10fc89bd3e94304f71275
CodeToFLAG <hxyqbsmile@gmail.com>
2018-08-18 18:21:12 +0800

    Update 128. Longest Consecutive Sequence.java

--- 128. Longest Consecutive Sequence.java
+++ 128. Longest Consecutive Sequence.java
@@ -1,9 +1,61 @@
+// First turn the input into a set of numbers. 
+// That takes O(n) and then we can ask in O(1) whether we have a certain number.
+
+// Then go through the numbers. If the number x is the start of a streak (i.e., x-1 is not in the set), 
+// then test y = x+1, x+2, x+3, ... and stop at the first number y not in the set. 
+// The length of the streak is then simply y-x and we update our global best with that. 
+// Since we check each streak only once, this is overall O(n)
+
+class Solution {
+    public int longestConsecutive(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        Set<Integer> set = new HashSet<>();
+        int maxLen = 1;
+        for (int num : nums) {
+            set.add(num);
+        }
+        // 每个数都只会被检查一次，所以是O(n)的复杂度
+        for (int num : nums) {
+            // !set.contains(num - 1) 表明 num是当前Consecutive Sequence的起始位置
+            if (!set.contains(num - 1)) {
+                int start = num;
+                while (set.contains(start + 1)) {
+                    start++;
+                }
+                int cur = start - num + 1;
+                maxLen = Math.max(cur, maxLen);
+            }
+        }
+        return maxLen;
+    }
+}
+
+
+// We will use HashMap. The key thing is to keep track of the sequence length and store 
+// that in the boundary points of the sequence. For example, as a result, 
+// for sequence {1, 2, 3, 4, 5}, map.get(1) and map.get(5) should both return 5.
+
+// Whenever a new element n is inserted into the map, do two things:
+
+// 1. See if n - 1 and n + 1 exist in the map, and if so, it means there is an existing
+// sequence next to n. Variables left and right will be the length of those two sequences,
+// while 0 means there is no sequence and n will be the boundary point later. 
+// Store (left + right + 1) as the associated value to key n into the map.
+
+// 2. Use left and right to locate the other end of the sequences to the left and 
+// right of n respectively, and replace the value with the new length.
+
+
 class Solution {
     public int longestConsecutive(int[] nums) {
         if (nums == null || nums.length == 0) {
             return 0;
         }
         int res = 0;
+        // key: Consecutive Sequence 的开头或结尾
+        // value: Consecutive Sequence 的长度
         HashMap<Integer, Integer> map = new HashMap<>();
         for (int num : nums) {
             if (!map.containsKey(num)) {
