ef9c9abc21c2a18825c194299b5ad0d275ca9818
CodeToFLAG <hxyqbsmile@gmail.com>
2018-08-19 13:26:54 +0800

    Update and rename 6.29.2018/163 Missing Ranges.java to 163 Missing Ranges.java

--- 163 Missing Ranges.java
+++ 163 Missing Ranges.java
@@ -1,12 +1,7 @@
-// Handle overflow by converting to long.
-
-public class Solution {
-    /*
-     * @param nums: a sorted integer array
-     * @param lower: An integer
-     * @param upper: An integer
-     * @return: a list of its missing ranges
-     */
+// 考虑三点： 1. overflow， Handle overflow by converting to long.
+// 2. 开头和结尾
+// 3. 每次衔接，相差一个数就只加一个数，相差超过一个数需要加一个范围 a->b
+class Solution {
     public List<String> findMissingRanges(int[] nums, int lower, int upper) {
         // write your code here
         List<String> res = new ArrayList<>();
@@ -16,8 +11,7 @@ public class Solution {
         if (nums == null || nums.length == 0) {
             if (lower == upper) {
                 res.add(lower + "");
-            }
-            else {
+            } else {
                 res.add(lower + "->" + upper);
             }
             return res;
@@ -25,19 +19,16 @@ public class Solution {
         if (lower < (long)nums[0] - 1) {
             long a = (long)nums[0] - 1;
             res.add(lower + "->" + a);
-        }
-        else if (lower == (long)nums[0] - 1) {
+        } else if (lower == (long)nums[0] - 1) {
             res.add(lower + "");
         }
         for (int i = 0; i < nums.length - 1; i++) {
             if (nums[i] + 1 >= nums[i + 1]) {
                 continue;
-            }
-            else if (nums[i] + 2 == nums[i + 1]) {
+            } else if (nums[i] + 2 == nums[i + 1]) {
                 int a = nums[i] + 1;
                 res.add(a + "");
-            }
-            else if ((long)nums[i] + 2 < nums[i + 1]){
+            } else if ((long)nums[i] + 2 < nums[i + 1]){
                 long a = (long)nums[i] + 1;
                 int b = nums[i + 1] - 1;
                 res.add(a + "->" + b);
@@ -46,8 +37,7 @@ public class Solution {
         if ((long)nums[nums.length - 1] + 1 < upper) {
             long a = nums[nums.length - 1] + 1;
             res.add(a + "->" + upper);
-        }
-        else if ((long)nums[nums.length - 1] + 1 == upper) {
+        } else if ((long)nums[nums.length - 1] + 1 == upper) {
             res.add(upper + "");
         }
         return res;
