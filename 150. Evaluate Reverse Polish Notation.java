0073f552ded8e332e19d3ab5c866ee26c4e47f2f
CodeToFLAG <hxyqbsmile@gmail.com>
2018-08-19 14:53:36 +0800

    Update and rename 6.29.2018/150. Evaluate Reverse Polish Notation.java to 150. Evaluate Reverse Polish Notation.java

--- 150. Evaluate Reverse Polish Notation.java
+++ 150. Evaluate Reverse Polish Notation.java
@@ -1,6 +1,6 @@
 // pay attention to .equals() and == in String comparison.
 // == check physical address, .equals() check same string.
-
+// 用stack。遇到数字就push，遇到符号就pop两个数字出来，算出结果再push
 class Solution {
     public int evalRPN(String[] tokens) {
         if (tokens == null || tokens.length == 0) {
