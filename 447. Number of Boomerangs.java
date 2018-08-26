86d239e1e02f9f09c72337c05111376afac2ca48
CodeToFLAG <hxyqbsmile@gmail.com>
2018-08-26 13:59:54 +0800

    Update and rename 7.13.2018/447. Number of Boomerangs.java to 447. Number of Boomerangs.java

--- 447. Number of Boomerangs.java
+++ 447. Number of Boomerangs.java
@@ -3,25 +3,25 @@ class Solution {
         if (points == null || points.length == 0) {
             return 0;
         }
-        int count = 0;
         Map<Integer, Integer> map = new HashMap<>();
+        int count = 0;
+        // points[i]是Boomerangs的中点
         for (int i = 0; i < points.length; i++) {
             map.clear();
             for (int j = 0; j < points.length; j++) {
                 if (i != j) {
-                    int distance = distanceSqare(points[i], points[j]);
+                    int distance = norm2(points[i], points[j]);
                     map.put(distance, map.getOrDefault(distance, 0) + 1);
                 }
             }
-            for (int num : map.values()) {
-                count += num * (num - 1);
+            for (int n : map.values()) {
+                // 顺序可变，所以是permutation
+                count += n * (n - 1);
             }
         }
         return count;
     }
-    private int distanceSqare(int[] A, int[] B) {
-        int xx = A[0] - B[0];
-        int yy = A[1] - B[1];
-        return xx * xx + yy * yy;
+    private int norm2(int[] p1, int[] p2) {
+        return (p1[0] - p2[0]) * (p1[0] - p2[0]) + (p1[1] - p2[1]) * (p1[1] - p2[1]);
     }
 }
