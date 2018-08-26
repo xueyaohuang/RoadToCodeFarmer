4832b24943f35d05a075d23986236077a8fdba27
CodeToFLAG <hxyqbsmile@gmail.com>
2018-08-26 14:14:34 +0800

    Update 419. Battleships in a Board.java

--- 7.13.2018/419. Battleships in a Board.java
+++ 7.13.2018/419. Battleships in a Board.java
@@ -1,3 +1,4 @@
+// 把不能加的排除，剩下的加，快一些。
 class Solution {
     public int countBattleships(char[][] board) {
         if (board == null || board.length == 0) {
@@ -16,6 +17,37 @@ class Solution {
                     continue;
                 }
                 count++;
+            }
+        }
+        return count;
+    }
+}
+
+// 数哪些能加，麻烦一点
+class Solution {
+    public int countBattleships(char[][] board) {
+        if (board == null || board.length == 0 || board[0].length == 0) {
+            return 0;
+        }
+        int count = 0;
+        for (int i = 0; i < board.length; i++) {
+            if (i == 0 && board[i][0] == 'X') {
+                count++;
+            }
+            if (i > 0 && board[i][0] == 'X' && board[i - 1][0] == '.') {
+                count++;
+            }
+        }
+        for (int j = 1; j < board[0].length; j++) {
+            if (board[0][j] == 'X' && board[0][j - 1] == '.') {
+                count++;
+            }
+        }
+        for (int i = 1; i < board.length; i++) {
+            for (int j = 1; j < board[0].length; j++) {
+                if (board[i][j] == 'X' && board[i][j - 1] == '.' && board[i - 1][j] == '.') {
+                    count++;
+                }
             }
         }
         return count;
