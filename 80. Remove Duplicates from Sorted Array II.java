c51eab7fb02756b0222fef5df6879d1b82f0a3c4
CodeToFLAG <hxyqbsmile@gmail.com>
2018-08-29 01:21:14 +0800

    Update and rename 7.30.2018/80. Remove Duplicates from Sorted Array II.java to 80. Remove Duplicates from Sorted Array II.java

--- 80. Remove Duplicates from Sorted Array II.java
+++ 80. Remove Duplicates from Sorted Array II.java
@@ -8,7 +8,7 @@ class Solution {
         }
         int j = 2;
         for (int i = 2; i < nums.length; i++) {
-            // 注意这里是j - 2，不是i - 2，we have to look k steps backward in new updated array.
+            // 注意这里是j - 2，不是i - 2，因为现在要给nums[j]赋值，当然要从j往前看2个。we have to look k steps backward in new updated array.
             if (nums[i] > nums[j - 2]) { // 把j - 2换成j - k就推广到最多k个重复。
                 nums[j] = nums[i];
                 j++;
