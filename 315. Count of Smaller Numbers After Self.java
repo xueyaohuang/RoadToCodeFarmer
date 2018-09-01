e66675283d1c177101cf873ff2aea40e04c5c42c
CodeToFLAG <hxyqbsmile@gmail.com>
2018-09-01 19:42:36 +0800

    Update 315. Count of Smaller Numbers After Self.java

--- 315. Count of Smaller Numbers After Self.java
+++ 315. Count of Smaller Numbers After Self.java
@@ -1,3 +1,5 @@
+// https://leetcode.com/problems/count-of-smaller-numbers-after-self/discuss/76674/3-Ways-(Segment-Tree-Binary-Indexed-Tree-Merge-Sort)-clean-Java-code
+
 class Solution {
     public List<Integer> countSmaller(int[] nums) {
         List<Integer> res = new ArrayList<>();
