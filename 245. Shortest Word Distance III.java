12df0d1c205212bf4f767b16b4032ba9a2043da1
CodeToFLAG <hxyqbsmile@gmail.com>
2018-09-01 22:10:05 +0800

    Update and rename 7.13.2018/245. Shortest Word Distance III.java to 245. Shortest Word Distance III.java

--- 245. Shortest Word Distance III.java
+++ 245. Shortest Word Distance III.java
@@ -1,3 +1,4 @@
+// 实际上这样对idx1和idx2的更新考虑到了所有可能的组合，但是因为word1可能和word2一样，所以在一个idx处，两个idx可能变成一样。所以在一个idx处，要更新distance2次。
 class Solution {
     public int shortestWordDistance(String[] words, String word1, String word2) {
         int idx1 = -1;
