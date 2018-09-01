910c94f4ef7005a067bdc097c4a2afaf4d0aefef
CodeToFLAG <hxyqbsmile@gmail.com>
2018-09-01 16:50:02 +0800

    Update and rename 7.10.2018/337. House Robber III.java to 337. House Robber III.java

--- 337. House Robber III.java
+++ 337. House Robber III.java
@@ -6,6 +6,27 @@
  *     TreeNode(int x) { val = x; }
  * }
  */
+
+class Solution {
+    public int rob(TreeNode root) {
+        int[] res = robHelper(root);
+        return Math.max(res[0], res[1]);
+    }
+    private int[] robHelper(TreeNode root) {
+        if (root == null) {
+            return new int[2];
+        }
+        int[] res = new int[2];
+        int[] left = robHelper(root.left);
+        int[] right = robHelper(root.right);
+        // res[0]是不抢root
+        res[0] = Math.max(left[0], left[1]) + Math.max(right[0], right[1]);
+        // res[1]是抢root
+        res[1] = root.val + left[0] +right[0];
+        return res;
+    }
+}
+     
 class Solution {
     public int rob(TreeNode root) {
         if (root == null) {
