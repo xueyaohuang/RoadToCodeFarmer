d858852613ac5c38e2ce2fc13479469e81f69c9c
CodeToFLAG <hxyqbsmile@gmail.com>
2018-09-02 16:17:55 +0800

    Update 464. Can I Win.java

--- 464. Can I Win.java
+++ 464. Can I Win.java
@@ -1,3 +1,9 @@
+/*
+After solving several "Game Playing" questions in leetcode, I find them to be pretty similar. Most of them can be solved
+using the top-down DP approach, which "brute-forcely" simulates every possible state of the game. The key part for the
+top-down dp strategy is that we need to avoid repeatedly solving sub-problems. Instead, we should use some strategy to
+"remember" the outcome of sub-problems. Then when we see them again, we instantly know their result. 
+*/
 class Solution {
     private byte[] memo;
     public boolean canIWin(int maxChoosableInteger, int desiredTotal) {
