8fdebde82aea06f193e81340b89bccadc8f854fb
CodeToFLAG <hxyqbsmile@gmail.com>
2018-09-03 11:17:00 +0800

    Update and rename 7.16.2018/647. Palindromic Substrings.java to 647. Palindromic Substrings.java

--- 647. Palindromic Substrings.java
+++ 647. Palindromic Substrings.java
@@ -22,3 +22,25 @@ class Solution {
         return count;
     }
 }
+
+class Solution {
+    int count = 0;
+    public int countSubstrings(String s) {
+        if (s == null || s.length() == 0) {
+            return 0;
+        }
+        for (int i = 0; i < s.length(); i++) {
+            extendPalindrome(s, i, i);
+            extendPalindrome(s, i, i + 1);
+        }
+        return count;
+    }
+    
+    private void extendPalindrome(String s, int head, int tail) {
+        while (head >= 0 && tail < s.length() && s.charAt(head) == s.charAt(tail)) {
+            count++;
+            head--;
+            tail++;
+        }
+    }
+}
