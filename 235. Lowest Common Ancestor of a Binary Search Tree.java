1bc6949d119ca85003996b145b5020497b4f5c50
CodeToFLAG <hxyqbsmile@gmail.com>
2018-09-04 10:49:35 +0800

    Update and rename 7.17.2018/235. Lowest Common Ancestor of a Binary Search Tree.java to 235. Lowest Common Ancestor of a Binary Search Tree.java

--- 235. Lowest Common Ancestor of a Binary Search Tree.java
+++ 235. Lowest Common Ancestor of a Binary Search Tree.java
@@ -24,3 +24,15 @@ class Solution {
         return root;
     }
 }
+
+class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if ((p.val - root.val) * (q.val - root.val) <= 0) {
+            return root;
+        } else if (p.val < root.val) {
+            return lowestCommonAncestor(root.left, p, q);
+        } else {
+            return lowestCommonAncestor(root.right, p, q);
+        }
+    }
+}
