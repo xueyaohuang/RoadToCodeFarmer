939658d01067a86dffb5e535cee9ca9b446c99ff
CodeToFLAG <hxyqbsmile@gmail.com>
2018-09-04 12:10:43 +0800

    Update 89. Gray Code.java

--- 7.18.2018/89. Gray Code.java
+++ 7.18.2018/89. Gray Code.java
@@ -7,7 +7,6 @@ class Solution {
         return res;
     }
 }
-
 class Solution {
     public List<Integer> grayCode(int n) {
         List<Integer> res = new ArrayList<>();
@@ -21,3 +20,10 @@ class Solution {
         return res;
     }
 }
+// 找规律，重点在res.add(res.get(j) | (1 << i));
+// 从n=i到n=i+1，一直往res中的每个数的最高位或1.
+// n = 0：0
+// n = 1: 0, 1
+// n = 2: 0, 1, 11, 10
+// n = 3: 0, 1, 11, 10, 110, 111, 101, 100
+// n = 4: 0, 1, 11, 10, 110, 111, 101, 100, 1100, 1101, 1111, 1110, 1010, 1011, 1001, 1000
