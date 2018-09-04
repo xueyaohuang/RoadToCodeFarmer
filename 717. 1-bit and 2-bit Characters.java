772e4a9a334ab220af0f26b513b4118500fbb0d1
CodeToFLAG <hxyqbsmile@gmail.com>
2018-09-04 17:55:41 +0800

    Update and rename 7.18.2018/717. 1-bit and 2-bit Characters.java to 717. 1-bit and 2-bit Characters.java

--- 717. 1-bit and 2-bit Characters.java
+++ 717. 1-bit and 2-bit Characters.java
@@ -2,12 +2,11 @@ class Solution {
     public boolean isOneBitCharacter(int[] bits) {
         int len = bits.length;
         int i = 0;
-        while (i + 1 < len) {
-            if (bits[i] == 1) {
+        while (i < len - 1) {
+            if (bits[i] == 1) { // 遇到1，只能是2位，前进2
                 i += 2;
-            }
-            else {
-                i++;
+            } else { // 遇到0，一定是1位，前进1
+                i += 1;
             }
         }
         return i != len;
