8c65a467286197d216f073b2b51e02549fb08ae6
CodeToFLAG <hxyqbsmile@gmail.com>
2018-09-05 18:02:27 +0800

    Update 105. Construct Binary Tree from Preorder and Inorder Traversal.java

--- 105. Construct Binary Tree from Preorder and Inorder Traversal.java
+++ 105. Construct Binary Tree from Preorder and Inorder Traversal.java
@@ -49,58 +49,37 @@ public TreeNode buildTree(int[] preorder, int[] inorder) {
     return root;
 }
 
-## 106. Construct Binary Tree from Inorder and Postorder Traversal
-
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode(int x) { val = x; }
- * }
- */
 class Solution {
-    public TreeNode buildTree(int[] inorder, int[] postorder) {
-        return treeHelper(postorder.length - 1, 0, inorder.length - 1, inorder, postorder);
-    }
-    private TreeNode treeHelper(int posEnd, int inStart, int inEnd, int[] inorder, int[] postorder) {
-        if (posEnd < 0 || inStart > inEnd) {
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+
+        if (preorder == null || preorder.length == 0 || preorder.length != inorder.length) {
             return null;
         }
-        TreeNode root = new TreeNode(postorder[posEnd]);
-        int rootIndex = 0;
-        for (int i = 0; i < inorder.length; i++) {
-            if (inorder[i] == root.val) {
-                rootIndex = i;
+        HashMap<Integer,Integer> map=new HashMap<>();
+        for(int i=0;i<inorder.length;i++)
+            map.put(inorder[i], i);
+
+        TreeNode root=new TreeNode(preorder[0]);
+        TreeNode p=root;
+        Stack<TreeNode> tree=new Stack<>();
+        tree.push(p);
+
+        for(int i=1;i<preorder.length;i++){
+            int temp=map.get(preorder[i]);
+            TreeNode node=new TreeNode(preorder[i]);
+
+            if(temp<map.get(tree.peek().val)){
+                p.left=node;
+                p=p.left;
             }
+            else {
+                while(!tree.isEmpty()&&temp>map.get(tree.peek().val))
+                    p=tree.pop();
+                p.right=node;
+                p=p.right;
+            }
+            tree.push(node);
         }
-        root.left = treeHelper(posEnd - (inEnd - rootIndex) - 1, inStart, rootIndex - 1, inorder, postorder);
-        root.right = treeHelper(posEnd - 1, rootIndex + 1, inEnd, inorder, postorder);
         return root;
     }
-}
-
- public TreeNode buildTree(int[] inorder, int[] postorder) {
-        
-        if( inorder.length == 0 ) return null;
-        // last element in the postorder is the root of the tree
-         TreeNode root = new TreeNode(postorder[postorder.length-1]);
-         Stack<TreeNode> st = new Stack<>();
-         st.push(root);
-        
-        for(int i=postorder.length-2, j = inorder.length-1;i>=0;--i){
-            //postorder[i] is the right child till the element in inorder traversal is not equal (i.e.rightmost node)
-            if( st.peek().val != inorder[j] ){
-                st.push(st.peek().right = new TreeNode(postorder[i]));
-            }else{
-                TreeNode temp = null;
-                //pop  till all the elements matching inorder elements are removed 
-                while(!st.empty() && st.peek().val == inorder[j]){
-                     temp = st.pop(); j--;
-                }
-                st.push(temp.left =  new TreeNode(postorder[i])); //continue in post-order fashion
-            } 
-        }
-       return root;
 }
