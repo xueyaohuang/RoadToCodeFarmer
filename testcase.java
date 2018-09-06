543658d310c7afcdd05b1bc690eb1d77bec605e1
CodeToFLAG <hxyqbsmile@gmail.com>
2018-09-06 23:59:48 +0800

    Update testcase.java

--- testcase.java
+++ testcase.java
@@ -23,12 +23,12 @@ class Main {
     
     public static void main(String[] args) {
         
-        TreeNode t1 = new TreeNode(1);
-        TreeNode t2 = new TreeNode(2);
-        TreeNode t3 = new TreeNode(3);
-        TreeNode t4 = new TreeNode(4);
-        TreeNode t5 = new TreeNode(2);
-        TreeNode t6 = new TreeNode(4);
+        TreeNode t1 = new TreeNode(5);
+        TreeNode t2 = new TreeNode(1);
+        TreeNode t3 = new TreeNode(5);
+        TreeNode t4 = new TreeNode(5);
+        TreeNode t5 = new TreeNode(5);
+        TreeNode t6 = new TreeNode(5);
         TreeNode t7 = new TreeNode(4);
         
         t1.left = t2;
