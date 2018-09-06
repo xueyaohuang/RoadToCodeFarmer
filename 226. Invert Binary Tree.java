9bff48afea62d45fef36cc51ed77a6fc56463534
CodeToFLAG <hxyqbsmile@gmail.com>
2018-09-06 14:14:18 +0800

    Update 226. Invert Binary Tree.java

--- 226. Invert Binary Tree.java
+++ 226. Invert Binary Tree.java
@@ -18,3 +18,27 @@ class Solution {
         return root;
     }
 }
+
+class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) {
+            return root;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            TreeNode node = queue.poll();
+            TreeNode left = node.left;
+            TreeNode right = node.right;
+            node.left = right;
+            node.right = left;
+            if (node.left != null) {
+                queue.offer(node.left);
+            }
+            if (node.right != null) {
+                queue.offer(node.right);
+            }
+        }
+        return root;
+    }
+}
