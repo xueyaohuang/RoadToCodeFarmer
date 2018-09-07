88550b1a234cdc2dc39731a4a042fcd64a38501d
CodeToFLAG <hxyqbsmile@gmail.com>
2018-09-07 16:57:47 +0800

    Update and rename 7.19.2018/388. Longest Absolute File Path.java to 388. Longest Absolute File Path.java

--- 388. Longest Absolute File Path.java
+++ 388. Longest Absolute File Path.java
@@ -7,9 +7,9 @@ class Solution {
         for (String s : strs) {
             int numTab = s.lastIndexOf("\t") + 1; // number of "\t"
             while (numTab + 1 < stack.size()) {  // find parent
-                stack.pop();  
+                stack.pop();  // 把与当前文件（夹）平级的文件（夹）的长度pop出去
             }
-            int curLen = stack.peek() + s.length() - numTab + 1;  // remove "/t", add"/"
+            int curLen = stack.peek() + s.length() - numTab + 1;  // remove "/t", add"/", "/"是最终结果里面要加的，不是s里面的
             stack.push(curLen);
             if (s.contains(".")) {
                 maxLen = Math.max(maxLen, curLen - 1); // if s contains ".", it has no "/", so curLen - 1.
@@ -18,9 +18,6 @@ class Solution {
         return maxLen;
     }
 }
-
-
-
 // "dir \tsubdir1 \tsubdir2 \t\tfile.ext"
     
 // dir
