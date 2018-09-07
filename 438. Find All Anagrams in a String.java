4ce72190e7951e1439665806d8afbf8ed25455cb
CodeToFLAG <hxyqbsmile@gmail.com>
2018-09-07 12:46:46 +0800

    Update 438. Find All Anagrams in a String.java

--- 438. Find All Anagrams in a String.java
+++ 438. Find All Anagrams in a String.java
@@ -37,26 +37,39 @@ class Solution {
 
 class Solution {
     public List<Integer> findAnagrams(String s, String p) {
+        if (s == null || p == null) {
+            return new ArrayList<>();
+        }
         List<Integer> res = new ArrayList<>();
-        int[] map = new int[256];
-        for (char c : p.toCharArray()) {
-            map[c]++;
+        int[] map = new int[26];
+        for (char c : p.toCharArray()) { // p的map，不是s的map
+            map[c - 'a']++;
         }
         int left = 0;
-        int count =0;
-        for (int right = 0; right < s.length(); right++) {
-            char c = s.charAt(right);
-            if (map[c]-- > 0) {
+        int right = 0;
+        int count = 0;
+        int lenS = s.length();
+        int lenP = p.length();
+        for (; right < lenS; right++) {
+            char cright = s.charAt(right);
+            // 先找到一个window，这个window内包含了p中所有的letter
+            // map[cright - 'a'] > 0 说明了，在s中又找到一个p中也有的letter
+            // 如果cright在p中，那么map[cright - 'a']在找全p中letter之前一直是大于0的。
+            if (map[cright - 'a'] > 0) {
                 count++;
             }
-            while (count == p.length()) {
-                char c1 = s.charAt(left);
-                if (count == right - left + 1) {
+            map[cright - 'a']--;
+            // count == lenP说明p中的letter在当前left到right的window中找全了，但是不一定连续构成Anagrams，还要检查window的长度
+            while (count == lenP) {
+                if (right - left + 1 == lenP) {
                     res.add(left);
                 }
-                if (map[c1]++ >= 0) {
+                char cleft = s.charAt(left);
+                // 由于left要向前移动，需要从count中减去移动走的p中的letter
+                if (map[cleft - 'a'] >= 0) {
                     count--;
                 }
+                map[cleft - 'a']++;
                 left++;
             }
         }
