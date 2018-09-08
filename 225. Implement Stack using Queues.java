1f70ed87e86f4f1cb92d34db732ecad968efb8ec
CodeToFLAG <hxyqbsmile@gmail.com>
2018-09-09 00:25:39 +0800

    Update 225. Implement Stack using Queues.java

--- 225. Implement Stack using Queues.java
+++ 225. Implement Stack using Queues.java
@@ -43,58 +43,31 @@ class MyStack {
 
 
 class MyStack {
-
-    LinkedList<Integer> queue1 = null;
-    LinkedList<Integer> queue2 = null;
-    
-    /** Initialize your data structure here. */
-    public MyStack() {
-        queue1 = new LinkedList<Integer>();
-        queue2 = new LinkedList<Integer>();
-    }
-    
-    /** Push element x onto stack. */
+    //using two queue. The push is inefficient.
+    private Queue<Integer> q1 = new LinkedList<Integer>();
+    private Queue<Integer> q2 = new LinkedList<Integer>();
     public void push(int x) {
-        if (empty()) {
-            queue1.offer(x);
-        } else {
-            if (queue1.size() > 0) {
-                queue2.offer(x);
-                for (int i = 0; i < queue1.size(); i++)
-                    queue2.offer(queue1.poll());
-            } else if (queue2.size() > 0) {
-                queue1.offer(x);
-                for (int i = 0; i < queue2.size(); i++)
-                    queue1.offer(queue2.poll());
-            }
+        if(q1.isEmpty()) {
+            q1.add(x);
+            for(int i = 0; i < q2.size(); i ++)
+                q1.add(q2.poll());
+        }else {
+            q2.add(x);
+            for(int i = 0; i < q1.size(); i++)
+                q2.add(q1.poll());
         }
     }
-    
-    /** Removes the element on top of the stack and returns that element. */
-    public int pop() {
-        if (queue1.size() > 0)
-            return queue1.poll();
-        else if (queue2.size() > 0)
-            return queue2.poll();
-        
-        return -1;
+
+    public void pop() {
+        if(!q1.isEmpty()) 
+            q1.poll();
+        else
+            q2.poll();
     }
-    
-    /** Get the top element. */
     public int top() {
-         if (queue1.size() > 0)
-            return queue1.peek();
-        else if (queue2.size() > 0)
-            return queue2.peek();
-        
-        return -1;
+        return q1.isEmpty() ? q2.peek() : q1.peek();
     }
-    
-    /** Returns whether the stack is empty. */
     public boolean empty() {
-        if (queue1.size() == 0 && queue2.size() == 0)
-            return true;
-        
-        return false;
+        return q1.isEmpty() && q2.isEmpty();
     }
 }
