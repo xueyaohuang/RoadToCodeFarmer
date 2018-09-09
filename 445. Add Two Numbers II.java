e746b365d2e39db955621531836f900f071d23c6
CodeToFLAG <hxyqbsmile@gmail.com>
2018-09-09 11:47:58 +0800

    Update and rename 7.7.2018/445. Add Two Numbers II.java to 445. Add Two Numbers II.java

--- 445. Add Two Numbers II.java
+++ 445. Add Two Numbers II.java
@@ -48,30 +48,30 @@ class Solution {
 
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        Stack<Integer> s1 = new Stack<>();
-        Stack<Integer> s2 = new Stack<>();
+        Deque<ListNode> stack1 = new ArrayDeque<>();
+        Deque<ListNode> stack2 = new ArrayDeque<>();
         ListNode list = new ListNode(0);
         int sum = 0;
         while (l1 != null) {
-            s1.push(l1.val);
+            stack1.push(l1);
             l1 = l1.next;
         }
         while (l2 != null) {
-            s2.push(l2.val);
+            stack2.push(l2);
             l2 = l2.next;
         }
-        while (!s1.isEmpty() || !s2.isEmpty()) {
-            if (!s1.isEmpty()) {
-                sum += s1.pop();
+        while (!stack1.isEmpty() || !stack2.isEmpty()) {
+            if (!stack1.isEmpty()) {
+                sum += stack1.pop().val;
             }
-            if (!s2.isEmpty()) {
-                sum += s2.pop();
+            if (!stack2.isEmpty()) {
+                sum += stack2.pop().val;
             }
             list.val = sum % 10;
-            ListNode head = new ListNode(sum / 10);
+            sum /= 10;
+            ListNode head = new ListNode(sum);
             head.next = list;
             list = head;
-            sum /= 10;
         }
         return list.val == 0 ? list.next : list;
     }
