04799ffa6bd45b2418373142772a48c530437120
CodeToFLAG <hxyqbsmile@gmail.com>
2018-09-10 12:38:29 +0800

    Update 42. Trapping Rain Water.java

--- 42. Trapping Rain Water.java
+++ 42. Trapping Rain Water.java
@@ -1,3 +1,9 @@
+// Instead of calculating area by height*width, we can think it in a cumulative way.
+// In other words, sum water amount of each bin(width=1).
+// Search from left to right and maintain a max height of left and right separately, which is like a one-side wall of partial container. 
+// Fix the higher one and flow water from the lower part. 
+// For example, if current height of left is lower, we fill water in the left bin. Until left meets right, we filled the whole container.
+
 class Solution {
     public int trap(int[] height) {
         int l = 0;
