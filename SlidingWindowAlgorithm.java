9f99a3e810a69a3da2294475d2b8ed648c5cc331
CodeToFLAG <hxyqbsmile@gmail.com>
2018-09-11 21:17:45 +0800

    Update SlidingWindowAlgorithm.java

--- SlidingWindowAlgorithm.java
+++ SlidingWindowAlgorithm.java
@@ -1,34 +1,3 @@
 // https://leetcode.com/problems/find-all-anagrams-in-a-string/discuss/92007/Sliding-Window-algorithm-template-to-solve-all-the-Leetcode-substring-search-problem.
-class Solution {
-    public List<Integer> findAnagrams(String s, String p) {
-        List<Integer> res = new ArrayList<>();
-        if (s == null || s.length() < p.length()) {
-            return res;
-        }
-        int lenS = s.length();
-        int lenP = p.length();
-        for (int i = 0; i <= lenS - lenP; i++) {
-            if (isAnagram(s.substring(i, i + lenP), p)) {
-                res.add(i);
-            }
-        }
-        return res;
-    }
-    private boolean isAnagram(String s, String t) {
-        if (s.length() != t.length()) {
-            return false;
-        }
-        int[] freq = new int[26];
-        for (int i = 0; i < s.length(); i++) {
-            freq[s.charAt(i) - 'a']++;
-            freq[t.charAt(i) - 'a']--;
-        }
-        for (int i = 0; i < 26; i++) {
-            if (freq[i] != 0) {
-                return false;
-            }
-        }
-        return true;
-    }
-}
+
 
