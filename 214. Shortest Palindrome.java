891cdff9a42b67e1729a4e9a018ebb35d50cb58f
CodeToFLAG <hxyqbsmile@gmail.com>
2018-09-11 14:25:54 +0800

    Update 214. Shortest Palindrome.java

--- 214. Shortest Palindrome.java
+++ 214. Shortest Palindrome.java
@@ -29,7 +29,28 @@ class Solution {
 // Each iteration of shortestPalindrome is linear in size of substring and the maximum number of recursive calls can be n/2 times as shown in the Intuition section.
 // Let the time complexity of the algorithm be T(n). Since, at the each step for the worst case, the string can be divide into 2 parts and we require only one part for further computation. 
 // Hence, the time complexity for the worst case can be represented as : T(n)=T(n-2)+O(n). So, T(n) = O(n) + O(n-2) + O(n-4) + ... + O(1), which is O(n^2)
-
+// https://leetcode.com/problems/shortest-palindrome/solution/
+// sol 2
+class Solution {
+    public String shortestPalindrome(String s) {
+        int i = 0;
+        int len = s.length();
+        // 从头开始，找到包含从头开始最长palindrome的substring
+        // for loop结束后，s.substring(0, i)不一定是palindrome，但是一定包含从头开始的最长palindrome
+        // for loop结束后，s.substring(i)肯定不是palindrome的一部分，需要把他reverse后接到s的最开始
+        // 之后递归调用
+        for (int j = len - 1; j >= 0; j--) {
+            if (s.charAt(i) == s.charAt(j)) {
+                i++;
+            }
+        }
+        if (i == len) {
+            return s;
+        }
+        StringBuilder sb = new StringBuilder(s.substring(i));
+        return sb.reverse().toString() + shortestPalindrome(s.substring(0, i)) + s.substring(i);
+    }
+}
 
 
 // kmp
