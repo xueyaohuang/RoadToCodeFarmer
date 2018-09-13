f425e7ac88ed7cbc53ea02ecaf8574c80d14b817
CodeToFLAG <hxyqbsmile@gmail.com>
2018-09-13 09:33:15 -0500

    Update and rename 7.20.2018/209. Minimum Size Subarray Sum.java to 76. Minimum Window Substring.java

--- 7.20.2018/209. Minimum Size Subarray Sum.java
+++
@@ -1,2 +0,0 @@
-// https://leetcode.com/problems/minimum-size-subarray-sum/discuss/59110/O(N)-template-for-Minimum-Size-Subarray-Sum-and-Minimum-Window-Substring-and-Longest-Substring-Without-Repeating-Characters
-// https://leetcode.com/problems/minimum-size-subarray-sum/discuss/59123/O(N)O(NLogN)-solutions-both-O(1)-space
---
+++ 76. Minimum Window Substring.java
@@ -0,0 +1,39 @@
+class Solution {
+    public String minWindow(String s, String t) {
+        if (s == null || s.length() == 0) {
+            return "";
+        }
+        int lenS = s.length();
+        int lenT = t.length();
+        int[] freq = new int[256];
+        for (char c : t.toCharArray()) {
+            freq[c]++;
+        }
+        int start = 0;
+        int head = 0;
+        int count = 0;
+        int minLen = Integer.MAX_VALUE;
+        for (int end = 0; end < lenS; end++) {
+            char cend = s.charAt(end);
+            if (freq[cend]-- > 0) { // freq[cend]无论如何都会--
+                count++;
+            }
+            // count == lenT保证当前window内包含了所有T的char
+            while (count == lenT) {
+                char cstart = s.charAt(start);
+                if (minLen > end - start + 1) {
+                    head = start;
+                    minLen = end - head + 1;
+                }
+                if (++freq[cstart] > 0) { // freq[cend]无论如何都会++，并且是先++再判断
+                    count--;
+                }
+                start++; 
+            }
+        }
+        if (minLen == Integer.MAX_VALUE) {
+            return "";
+        }
+        return s.substring(head, head + minLen);
+    }
+}
