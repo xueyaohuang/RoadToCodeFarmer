a68073a21cef1c8a30e7dd1cb1ad5a2507691788
CodeToFLAG <hxyqbsmile@gmail.com>
2018-09-14 14:29:26 -0500

    Update 132. Palindrome Partitioning II.java

--- 132. Palindrome Partitioning II.java
+++ 132. Palindrome Partitioning II.java
@@ -1,3 +1,30 @@
+// This can be solved by two points:
+
+// cut[j] is the minimum of cut[i - 1] + 1 (i <= j), if [i, j] is palindrome.
+// If [i, j] is palindrome, [i + 1, j - 1] is palindrome, and c[j] == c[i].
+
+class Solution {
+    public int minCut(String s) {
+        if (s == null || s.length() == 0) {
+            return 0;
+        }
+        int len = s.length();
+        boolean[][] isPalindrome = new boolean[len][len];
+        int[] cut = new int[len];
+        for (int j = 0; j < len; j++) {
+            int min = j;
+            for (int i = 0; i <= j; i++) {
+                if (s.charAt(i) == s.charAt(j) && (i + 1 >= j || isPalindrome[i + 1][j - 1])) {
+                    isPalindrome[i][j] = true;
+                    min = i == 0 ? 0 : Math.min(min, cut[i - 1] + 1);
+                }
+            }
+            cut[j] = min;
+        }
+        return cut[len - 1];
+    }
+}
+
 class Solution {
     public int minCut(String s) {
         if (s == null || s.length() == 0) {
