7bfd6d58201af1b3ece09840ec2456aef65b336c
CodeToFLAG <hxyqbsmile@gmail.com>
2018-09-14 05:16:10 -0500

    Update 78. Subsets.java

--- 78. Subsets.java
+++ 78. Subsets.java
@@ -1,18 +1,19 @@
+// backtrack, 如果设置了start参数，就是遍历所有可能的combination。如果没有start，每次递归都从第一个element开始，则便利所有可能的permutation。
+// 这个题就是枚举所有可能的组合，没有任何限制，相当于没有退出条件，上来就把temp加入res。
+// 这种方法，从头到尾一直用同一个temp在尝试各种组合。
 class Solution {
     public List<List<Integer>> subsets(int[] nums) {
-        List<List<Integer>> list = new ArrayList<>();
-        list.add(new ArrayList<Integer>());
-        if (nums == null || nums.length == 0) {
-            return list;
+        List<List<Integer>> res = new ArrayList<>();
+        backtrack(nums, res, new ArrayList<>(), 0);
+        return res;
+    }
+    
+    private void backtrack(int[] nums, List<List<Integer>> res, List<Integer> temp, int start) {
+        res.add(new ArrayList<>(temp));
+        for (int i = start; i < nums.length; i++) {
+            temp.add(nums[i]);
+            backtrack(nums, res, temp, i + 1);
+            temp.remove(temp.size() - 1);
         }
-        for (int num : nums) {
-            int tmpsize = list.size();
-            for (int i = 0; i < tmpsize; i++) {
-                List<Integer> temp = new ArrayList<>(list.get(i));
-                temp.add(num);
-                list.add(temp);
-            }
-        }
-        return list;
     }
 }
