7278c944b1bdc227020621043ccdec7a5aa15905
CodeToFLAG <hxyqbsmile@gmail.com>
2018-09-14 09:44:34 -0500

    Update 47. Permutations II.java

--- 47. Permutations II.java
+++ 47. Permutations II.java
@@ -18,9 +18,14 @@ class Solution {
                 continue;
             }
             // 与combination相比，多了一个检查used[i - 1]。因为combination不会loop 当前index之前的数，当前index之后的数一定是没有用过的
+            // 这种写法是最后一次遇到这种permutation的时候加进去。
             if (i > 0 && nums[i] == nums[i - 1] && used[i - 1]) { // 跳过重复，考虑顺序就i > 0 && ...,不考虑顺序就i > start && ...
                 continue;
             }
+//             // 这种写法是第一次遇到这种permutation的时候加进去。
+//             if (i < nums.length - 1 && nums[i] == nums[i + 1] && used[i + 1]) {
+//                 continue;
+//             }
             temp.add(nums[i]);
             used[i] = true;
             backtrack(nums, res, temp, used);
