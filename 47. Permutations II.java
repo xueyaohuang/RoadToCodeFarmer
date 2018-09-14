969eef3421dd2fe1c1845b013a3f7af0f89eaa56
CodeToFLAG <hxyqbsmile@gmail.com>
2018-09-14 09:35:11 -0500

    Update and rename 7.22.2018/47. Permutations II.java to 47. Permutations II.java

---
+++ 47. Permutations II.java
@@ -0,0 +1,31 @@
+// Permutations问题没有start/position这个参数，因为顺序matters，combination里面需要start/position，因为顺序改了算重复。
+class Solution {
+    public List<List<Integer>> permuteUnique(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        boolean[] used = new boolean[nums.length];
+        Arrays.sort(nums);
+        backtrack(nums, res, new ArrayList<>(), used);
+        return res;
+    }
+    
+    private void backtrack(int[] nums, List<List<Integer>> res, List<Integer> temp, boolean[] used) {
+        if (temp.size() == nums.length) {
+            res.add(new ArrayList<>(temp));
+            return;
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if (used[i]) {
+                continue;
+            }
+            // 与combination相比，多了一个检查used[i - 1]。因为combination不会loop 当前index之前的数，当前index之后的数一定是没有用过的
+            if (i > 0 && nums[i] == nums[i - 1] && used[i - 1]) { // 跳过重复，考虑顺序就i > 0 && ...,不考虑顺序就i > start && ...
+                continue;
+            }
+            temp.add(nums[i]);
+            used[i] = true;
+            backtrack(nums, res, temp, used);
+            temp.remove(temp.size() - 1);
+            used[i] = false;
+        }
+    }
+}
