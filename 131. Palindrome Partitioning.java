b1ca912b8a52d79c5d1e3e0afa0d47306f66566a
CodeToFLAG <hxyqbsmile@gmail.com>
2018-09-14 11:16:23 -0500

    Update 131. Palindrome Partitioning.java

--- 131. Palindrome Partitioning.java
+++ 131. Palindrome Partitioning.java
@@ -30,3 +30,35 @@ class Solution {
         return true;
     }
 }
+
+// way better, 先构建一个lookup table，isPalindrome[i][j]
+class Solution {
+    public List<List<String>> partition(String s) {
+        List<List<String>> res = new ArrayList<>();
+        int len = s.length();
+        boolean[][] isPalindrome = new boolean[len][len];
+        for (int i = len - 1; i >= 0; i--) {
+            for (int j = i; j < len; j++) {
+                if (s.charAt(i) == s.charAt(j) && (j - i < 2 || isPalindrome[i + 1][j - 1])) {
+                    isPalindrome[i][j] = true;
+                }
+            }
+        }
+        backtrack(s, res, new ArrayList<>(), 0, isPalindrome);
+        return res;
+    }
+    
+    private void backtrack(String s, List<List<String>> res, List<String> temp, int start, boolean[][] isPalindrome) {
+        if (start == s.length()) {
+            res.add(new ArrayList<>(temp));
+            return;
+        }
+        for (int i = start; i < s.length(); i++) {
+            if (isPalindrome[start][i]) {
+                temp.add(s.substring(start, i + 1));
+                backtrack(s, res, temp, i + 1, isPalindrome);
+                temp.remove(temp.size() - 1);
+            }
+        }
+    }
+}
