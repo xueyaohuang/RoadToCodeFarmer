fcf11b05cf6a9e7265105fda1909ebcc4f7c9fc1
CodeToFLAG <hxyqbsmile@gmail.com>
2018-09-16 11:15:18 -0500

    Update 191. Number of 1 Bits.java

--- 191. Number of 1 Bits.java
+++ 191. Number of 1 Bits.java
@@ -9,3 +9,15 @@ public class Solution {
         return count;
     }
 }
+
+public class Solution {
+    // you need to treat n as an unsigned value
+    public int hammingWeight(int n) {
+        int res = 0;
+        while (n != 0) {
+            n &= n - 1;
+            res++;
+        }
+        return res;
+    }
+}
