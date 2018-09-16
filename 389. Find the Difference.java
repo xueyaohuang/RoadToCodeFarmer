5dc1e739226f21fd4d21fa6a373a145475b42e03
CodeToFLAG <hxyqbsmile@gmail.com>
2018-09-16 11:06:47 -0500

    Update 389. Find the Difference.java

--- 389. Find the Difference.java
+++ 389. Find the Difference.java
@@ -15,3 +15,16 @@ class Solution {
         return '\0';
     }
 }
+
+class Solution {
+    public char findTheDifference(String s, String t) {
+        int res = 0;
+        for (char c : s.toCharArray()) {
+            res ^= c;
+        }
+        for (char c : t.toCharArray()) {
+            res ^= c;
+        }
+        return (char)res;
+    }
+}
