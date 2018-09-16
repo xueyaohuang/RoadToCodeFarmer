b4536cc0753a09831ed2e9c282f27d1f7c0505ee
CodeToFLAG <hxyqbsmile@gmail.com>
2018-09-16 07:32:16 -0500

    Update 301. Remove Invalid Parentheses.java

--- 301. Remove Invalid Parentheses.java
+++ 301. Remove Invalid Parentheses.java
@@ -1,157 +1,65 @@
+// 1. Limit max removal rmL and rmR for backtracking boundary. Otherwise it will exhaust all possible valid substrings, not shortest ones.
+//     rmL，rmR是需要remove的open，close括号个数，必须都是大于等于0的，才能保证移除的括号数最少。
+// 2. Scan from left to right, avoiding invalid strs (on the fly) by checking num of open parens.
+//    open表示open与close括号之差，在任何时候，')'不能比'('多，所以open<0就是invalid，直接return
+// 3. If it's '(', either use it, or remove it.
+// 4. If it's ')', either use it, or remove it.
+// 5. Otherwise just append it.
+
+// In each step, make sure:
+// 1. i does not exceed s.length().
+// 2. Max removal rmL rmR and num of open parens are non negative.
+// 3. De-duplicate by adding to a HashSet.
+
+// 标准的backtrack有for loop，这里没有，因为这个题需要Remove the minimum number of invalid parentheses。也就是说结果需要时最长的，如果有for loop，从start接着开始，那么结果可能不是最长的。
+// backtrack中for loop的作用，是从给定序列中间取一个，作为开头。
+
 class Solution {
-    //follow the highest voted post
-    //
-    
-    //total time complexity? N * 2 ^ N?
-    //can we optimize the complexity since the string operation is expensive
-    //you should also be able to write BFS and simplied version(two pass, one pass)
-    
     public List<String> removeInvalidParentheses(String s) {
-        List<String> res = new ArrayList<>();
-        removeHelper(s, res, 0, 0, '(', ')');
-        return res;
-    }
-    
-    private void removeHelper(String s, List<String> res, int start, int lastRemove, char plusChar, char minusChar){
-        int count = 0;
-        for (int i = start; i < s.length(); i++){
-            if (s.charAt(i) == plusChar) count++;
-            if (s.charAt(i) == minusChar) count--;
-            
-            if (count < 0){
-                for (int j = lastRemove; j <= i; j++){
-                    if (s.charAt(j) == minusChar && (j == lastRemove || s.charAt(j) != s.charAt(j - 1))){
-                        removeHelper(s.substring(0, j) + s.substring(j + 1), res, i, j, plusChar, minusChar);
-                    }
+        if (s == null) {
+            return new ArrayList<>();
+        }
+        // 需要remove的open/close 括号个数
+        int rmOpen = 0, rmClose = 0;
+        Set<String> res = new HashSet<>();
+        
+        for (char c : s.toCharArray()) {
+            if (c == '(') {
+                rmOpen++;
+            } else if (c == ')') {
+                if (rmOpen > 0) {
+                    rmOpen--;
+                } else {
+                    rmClose++;
                 }
-                return; //should be return here, since the previous part has no wrong minusChar
             }
         }
-        
-        if (plusChar == '(' && count == 0){
-            res.add(s);
+        backtrack(s, res, rmOpen, rmClose, 0, 0, new StringBuilder());
+        return new ArrayList<String>(res);
+    }
+    
+    private void backtrack(String s, Set<String> res, int rmOpen, int rmClose, int open, int index, StringBuilder sb) {
+        if (rmOpen < 0 || rmClose < 0 || open < 0) {
             return;
         }
-        
-        StringBuilder sb = new StringBuilder(s);
-        String reversedS = sb.reverse().toString();
-        
-        if (plusChar == '('){
-            removeHelper(reversedS, res, 0, 0, ')', '(');
-        } else {
-            res.add(reversedS);
-        }
-        
-    }
-}
-
-class Solution {
-public List<String> removeInvalidParentheses(String s) {
-    int rmL = 0, rmR = 0;
-    for (int i = 0; i < s.length(); i++) {
-        if (s.charAt(i) == '(') {
-            rmL++;
-        } else if (s.charAt(i) == ')') {
-            if (rmL != 0) {
-                rmL--;
-            } else {
-                rmR++;
+        if (index == s.length()) {
+            if (rmOpen == 0 && rmClose == 0 && open == 0) {
+                res.add(sb.toString());
             }
+            return;
+        }
+        char c = s.charAt(index);
+        if (c == '(') {
+            backtrack(s, res, rmOpen - 1, rmClose, open, index + 1, sb); // not use '('
+            backtrack(s, res, rmOpen, rmClose, open + 1, index + 1, sb.append(c)); // use '('
+            sb.setLength(sb.length() - 1); // 返回上层recursion，remove当前加入的元素
+        } else if (c == ')') {
+            backtrack(s, res, rmOpen, rmClose - 1, open, index + 1, sb); // not use ')'
+            backtrack(s, res, rmOpen, rmClose, open - 1, index + 1, sb.append(c)); // use ')'
+            sb.setLength(sb.length() - 1);
+        } else {
+            backtrack(s, res, rmOpen, rmClose, open, index + 1, sb.append(c));
+            sb.setLength(sb.length() - 1);
         }
     }
-    Set<String> res = new HashSet<>();
-    dfs(s, 0, res, new StringBuilder(), rmL, rmR, 0);
-    return new ArrayList<String>(res);
 }
-
-public void dfs(String s, int i, Set<String> res, StringBuilder sb, int rmL, int rmR, int open) {
-    if (rmL < 0 || rmR < 0 || open < 0) {
-        return;
-    }
-    if (i == s.length()) {
-        if (rmL == 0 && rmR == 0 && open == 0) {
-            res.add(sb.toString());
-        }        
-        return;
-    }
-
-    char c = s.charAt(i); 
-    int len = sb.length();
-
-    if (c == '(') {
-        dfs(s, i + 1, res, sb, rmL - 1, rmR, open);		    // not use (
-    	dfs(s, i + 1, res, sb.append(c), rmL, rmR, open + 1);       // use (
-
-    } else if (c == ')') {
-        dfs(s, i + 1, res, sb, rmL, rmR - 1, open);	            // not use  )
-    	dfs(s, i + 1, res, sb.append(c), rmL, rmR, open - 1);  	    // use )
-
-    } else {
-        dfs(s, i + 1, res, sb.append(c), rmL, rmR, open);	
-    }
-
-    sb.setLength(len);        
-}
-}
-
-
-public class Solution {
-    public List<String> removeInvalidParentheses(String s) {
-      List<String> res = new ArrayList<>();
-      
-      // sanity check
-      if (s == null) return res;
-      
-      Set<String> visited = new HashSet<>();
-      Queue<String> queue = new LinkedList<>();
-      
-      // initialize
-      queue.add(s);
-      visited.add(s);
-      
-      boolean found = false;
-      
-      while (!queue.isEmpty()) {
-        s = queue.poll();
-        
-        if (isValid(s)) {
-          // found an answer, add to the result
-          res.add(s);
-          found = true;
-        }
-      
-        if (found) continue;
-      
-        // generate all possible states
-        for (int i = 0; i < s.length(); i++) {
-          // we only try to remove left or right paren
-          if (s.charAt(i) != '(' && s.charAt(i) != ')') continue;
-        
-          String t = s.substring(0, i) + s.substring(i + 1);
-        
-          if (!visited.contains(t)) {
-            // for each state, if it's not visited, add it to the queue
-            queue.add(t);
-            visited.add(t);
-          }
-        }
-      }
-      
-      return res;
-    }
-    
-    // helper function checks if string s contains valid parantheses
-    boolean isValid(String s) {
-      int count = 0;
-    
-      for (int i = 0; i < s.length(); i++) {
-        char c = s.charAt(i);
-        if (c == '(') count++;
-        if (c == ')' && count-- == 0) return false;
-      }
-    
-      return count == 0;
-    }
-}
-
-()))(
