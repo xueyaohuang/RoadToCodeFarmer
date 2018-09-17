4b58bbcfca533e2994ba5ddbceb082c23df72618
CodeToFLAG <hxyqbsmile@gmail.com>
2018-09-17 09:26:23 -0500

    Update 318. Maximum Product of Word Lengths.java

--- 318. Maximum Product of Word Lengths.java
+++ 318. Maximum Product of Word Lengths.java
@@ -4,20 +4,23 @@ class Solution {
             return 0;
         }
         int len = words.length;
-        int[] value = new int[len];
+        int[] bitMap = new int[len];
+        int max = 0;
+        
         for (int i = 0; i < len; i++) {
             for (int j = 0; j < words[i].length(); j++) {
-                value[i] |= 1 << (words[i].charAt(j) - 'a');
+                bitMap[i] |= 1 << (words[i].charAt(j) - 'a');
             }
         }
-        int max = 0;
+        
         for (int i = 0; i < len; i++) {
             for (int j = i + 1; j < len; j++) {
-                if ((value[i] & value[j]) == 0 && words[i].length() * words[j].length() > max) {
-                    max = words[i].length() * words[j].length();
+                if ((bitMap[i] & bitMap[j]) == 0) {
+                    max = Math.max(max, words[i].length() * words[j].length());
                 }
             }
         }
+        
         return max;
     }
 }
