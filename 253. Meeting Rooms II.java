c1d3cf3e82766411cceccbbb9d270eb7ec14f3f8
CodeToFLAG <hxyqbsmile@gmail.com>
2018-09-19 09:06:13 -0500

    Update 253. Meeting Rooms II.java

--- 253. Meeting Rooms II.java
+++ 253. Meeting Rooms II.java
@@ -34,3 +34,26 @@ class Solution {
         return room;
     }
 }
+
+public class Solution {
+    public int minMeetingRooms(Interval[] intervals) {
+        Map<Integer, Integer> map = new TreeMap<Integer, Integer>(); // Sort Key based on nature order
+        for (Interval i : intervals) {
+            if (map.containsKey(i.start)) {
+                map.put(i.start, map.get(i.start)+1);
+            } else {
+                map.put(i.start, 1);
+            }
+            if (map.containsKey(i.end)) {
+                map.put(i.end, map.get(i.end)-1);
+            } else {
+                map.put(i.end, -1);
+            }
+        }
+        int maxRoom = 0; int curRoom = 0;
+        for (int i : map.keySet()) {
+            maxRoom = Math.max(maxRoom, curRoom += map.get(i));
+        }
+        return maxRoom;
+    }
+}
