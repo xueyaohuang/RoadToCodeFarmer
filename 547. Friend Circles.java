1e54aa3933e3aea6c7a9eac384706b89fe9e1a14
CodeToFLAG <hxyqbsmile@gmail.com>
2018-09-19 18:08:30 -0500

    Create 547. Friend Circles.java

---
+++ 547. Friend Circles.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int findCircleNum(int[][] M) {
+        int count = 0;
+        int[] visited = new int[M.length];
+        for (int i = 0; i < M.length; i++) {
+            if (visited[i] == 0) {
+                count++;
+                dfs(M, visited, i);
+            }
+        }
+        return count;
+    }
+    private void dfs(int[][] M, int[] visited, int i) {
+        for (int j = 0; j < M.length; j++) {
+            if (M[i][j] == 1 && visited[j] == 0) {
+                visited[j] = 1;
+                dfs(M, visited, j);
+            }
+        }
+    }
+}
