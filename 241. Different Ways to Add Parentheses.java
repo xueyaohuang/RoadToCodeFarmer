7c19757e05310804077c503039ad97402981b775
CodeToFLAG <hxyqbsmile@gmail.com>
2018-09-20 08:22:06 -0500

    Update 241. Different Ways to Add Parentheses.java

--- 241. Different Ways to Add Parentheses.java
+++ 241. Different Ways to Add Parentheses.java
@@ -1,41 +1,57 @@
-public class Solution { 
+class Solution {
     
-    private Map<String,List<Integer>> memo = new HashMap<>();
+    // cache，避免计算相同的input
+    private Map<String, List<Integer>> map = new HashMap<>();
     
     public List<Integer> diffWaysToCompute(String input) {
-        int len = input.length();
-        // check history
-        if (memo.containsKey(input)) { return memo.get(input); }
-        List<Integer> result = new ArrayList<>();
-        // base case
-        if (isDigit(input)) {
-            result.add(Integer.parseInt(input));
-            memo.put(input,result);
-            return result;
+        // 检查这个input是否被计算过
+        if (map.containsKey(input)) {
+            return map.get(input);
         }
-        // recursion (divid & conquer)
+        
+        int len = input.length();
+        List<Integer> res = new ArrayList<>();
+        
+        // 如果input是纯数字，直接加进去
+        if (isDigit(input)) {
+            res.add(Integer.parseInt(input));
+            map.put(input, res);
+            return res;
+        }
+        
+        // divide and conquer，把input按运算符分成左右两边分别计算。
+        // 需要在每一个运算符都做分治。
         for (int i = 0; i < len; i++) {
             char c = input.charAt(i);
             if (c == '+' || c == '-' || c == '*') {
-                List<Integer> left = diffWaysToCompute(input.substring(0,i));
-                List<Integer> right = diffWaysToCompute(input.substring(i+1,len));
-                for (Integer il : left) {
-                    for (Integer ir : right) {
-                        switch (c) {
-                            case '+': result.add(il + ir); break;
-                            case '-': result.add(il - ir); break;
-                            case '*': result.add(il * ir); break;
+                String left = input.substring(0, i);
+                String right = input.substring(i + 1);
+                List<Integer> leftRes = diffWaysToCompute(left);
+                List<Integer> rightRes = diffWaysToCompute(right);
+                for (int a : leftRes) {
+                    for (int b : rightRes) {
+                        if (c == '+') {
+                            res.add(a + b);
+                        } else if (c == '-') {
+                            res.add(a - b);
+                        } else {
+                            res.add(a * b);
                         }
                     }
                 }
             }
         }
-        memo.put(input,result);
-        return result;
+        // cache result
+        map.put(input, res);
+        return res;
     }
-    private boolean isDigit(String s) {
-        for (Character c : s.toCharArray()) {
-            if (!Character.isDigit(c)) { return false; }
+    
+    // 判断input是否为纯digit
+    private boolean isDigit(String input) {
+        for (char c : input.toCharArray()) {
+            if (!Character.isDigit(c)) {
+                return false;
+            }
         }
         return true;
     }
