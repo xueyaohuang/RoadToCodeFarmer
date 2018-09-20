cd6593ec24bd94b2e2b41531da769bd2e3c80dcd
CodeToFLAG <hxyqbsmile@gmail.com>
2018-09-19 23:25:01 -0500

    Update 547. Friend Circles.java

--- 547. Friend Circles.java
+++ 547. Friend Circles.java
@@ -1,21 +1,99 @@
+// DFS,O(n^2)
 class Solution {
     public int findCircleNum(int[][] M) {
+        if (M == null || M.length == 0) {
+            return 0;
+        }
+        int n = M.length;
         int count = 0;
-        int[] visited = new int[M.length];
-        for (int i = 0; i < M.length; i++) {
-            if (visited[i] == 0) {
+        boolean[] visited = new boolean[n];
+        // 只需要一个for loop
+        for (int i = 0; i < n; i++) {
+            if (!visited[i]) {
                 count++;
-                dfs(M, visited, i);
+                visited[i] = true;
+                findFriend(M, i, visited);
             }
         }
         return count;
     }
-    private void dfs(int[][] M, int[] visited, int i) {
+    
+    private void findFriend(int[][] M, int i, boolean[] visited) {
         for (int j = 0; j < M.length; j++) {
-            if (M[i][j] == 1 && visited[j] == 0) {
-                visited[j] = 1;
-                dfs(M, visited, j);
+            if (M[i][j] == 1 && !visited[j]) {
+                visited[j] = true;
+                findFriend(M, j, visited);
             }
         }
+    }
+}
+
+// Union find
+class Solution {
+    
+    public class UnionFind {
+        
+        int count;
+        int[] parent;
+        int[] rank;
+        
+        public UnionFind(int n) {
+            count = n;
+            parent = new int[n];
+            rank = new int[n];
+            for (int i = 0; i < n; i++) {
+                parent[i] = i;
+                rank[i] = 0;
+            }
+        }
+        
+        public int find(int p) {
+            while (p != parent[p]) {
+                parent[p] = parent[parent[p]];
+                p = parent[p];
+            }
+            return p;
+        }
+        
+        public boolean isConnected(int p, int q) {
+            return find(p) == find(q);
+        }
+        
+        public void union(int p, int q) {
+            int rootp = find(p);
+            int rootq = find(q);
+            if (rootp == rootq) {
+                return;
+            }
+            if (rank[rootp] > rank[rootq]) {
+                parent[rootq] = rootp;
+            } else if (rank[rootp] < rank[rootq]) {
+                parent[rootp] = rootq;
+            } else {
+                parent[rootq] = rootp;
+                rank[rootp]++;
+            }
+            count--;
+        }
+        
+        public int count() {
+            return count;
+        }
+    }
+    
+    public int findCircleNum(int[][] M) {
+        if (M == null || M.length == 0) {
+            return 0;
+        }
+        int n = M.length;
+        UnionFind uf = new UnionFind(n);
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (M[i][j] == 1) {
+                    uf.union(i, j);
+                }
+            }
+        }
+        return uf.count();
     }
 }
