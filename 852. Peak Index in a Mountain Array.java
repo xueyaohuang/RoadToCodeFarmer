21a6359278b25e8cdbb160ae796afe273fc81e93
CodeToFLAG <hxyqbsmile@gmail.com>
2018-09-21 18:47:15 -0500

    Update 852. Peak Index in a Mountain Array.java

--- 852. Peak Index in a Mountain Array.java
+++ 852. Peak Index in a Mountain Array.java
@@ -1,3 +1,6 @@
+// 此题start可以加1，end也可以减1.
+// 看似start不能加1，因为若start刚好在peak，加1就越界了。
+// 但是如果start在peak的位置，先检查的是if (A[mid] > A[mid - 1] && A[mid] > A[mid + 1])，会直接返回peak。
 class Solution {
     public int peakIndexInMountainArray(int[] A) {
         int start = 0;
@@ -7,7 +10,7 @@ class Solution {
             if (A[mid] > A[mid - 1] && A[mid] > A[mid + 1]) {
                 return mid;
             } else if (A[mid] > A[mid - 1]) {
-                start = mid;
+                start = mid + 1;
             } else {
                 end = mid - 1;
             }
