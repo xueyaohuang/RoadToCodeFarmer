5d6fc4c3f7073bee5c9924c8e38d5118292c92ca
CodeToFLAG <hxyqbsmile@gmail.com>
2018-09-20 23:15:08 -0500

    Create 62. Unique Paths.java

---
+++ 62. Unique Paths.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int uniquePaths(int m, int n) {
+        int[][] ways = new int[m][n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == 0 || j == 0) {
+                    ways[i][j] = 1;
+                } else {
+                    ways[i][j] = ways[i - 1][j] + ways[i][j - 1];
+                }
+            }
+        }
+        return ways[m - 1][n - 1];
+    }
+}
+
+class Solution {
+    public int uniquePaths(int m, int n) {
+        int[] ways = new int[n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == 0 || j == 0) {
+                    ways[j] = 1;
+                } else {
+                    ways[j] = ways[j] + ways[j - 1]; // ways[j] += ways[j - 1];
+                }
+            }
+        }
+        return ways[n - 1];
+    }
+}
