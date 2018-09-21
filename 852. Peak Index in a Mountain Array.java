d029e98e02614eead34a0098908d71a17e989041
CodeToFLAG <hxyqbsmile@gmail.com>
2018-09-21 18:43:23 -0500

    Update 852. Peak Index in a Mountain Array.java

--- 852. Peak Index in a Mountain Array.java
+++ 852. Peak Index in a Mountain Array.java
@@ -3,15 +3,13 @@ class Solution {
         int start = 0;
         int end = A.length - 1;
         while (start < end) {
-            int mid = (start + end) / 2;
+            int mid = (start + end + 1) / 2;
             if (A[mid] > A[mid - 1] && A[mid] > A[mid + 1]) {
                 return mid;
-            }
-            else if (A[mid] > A[mid - 1]) {
-                start = mid + 1;
-            }
-            else {
-                end = mid;
+            } else if (A[mid] > A[mid - 1]) {
+                start = mid;
+            } else {
+                end = mid - 1;
             }
         }
         return start;
