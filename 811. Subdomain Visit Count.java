085df129f505f3647da7acd07cf0417897424799
CodeToFLAG <hxyqbsmile@gmail.com>
2018-09-21 11:38:58 -0500

    Update 811. Subdomain Visit Count.java

--- 811. Subdomain Visit Count.java
+++ 811. Subdomain Visit Count.java
@@ -1,2 +1,28 @@
-.
-
+class Solution {
+    public List<String> subdomainVisits(String[] cpdomains) {
+        if (cpdomains == null || cpdomains.length == 0) {
+            return new ArrayList<>();
+        }
+        Map<String, Integer> map = new HashMap<>();
+        List<String> res = new ArrayList<>();
+        for (String cpdomain : cpdomains) {
+            String[] cp = cpdomain.split(" ");
+            int count = Integer.parseInt(cp[0]);
+            String domain = cp[1];
+            map.put(domain, map.getOrDefault(domain, 0) + count);
+            for (int i = 0; i < domain.length(); i++) {
+                if (domain.charAt(i) == '.') {
+                    String subdomain = domain.substring(i + 1);
+                    map.put(subdomain, map.getOrDefault(subdomain, 0) + count);
+                }   
+            }
+        }
+        // for (Map.Entry<String, Integer> entry : map.entrySet()) {
+        //     res.add(entry.getValue() + " " + entry.getKey());
+        // }
+        for (String domain : map.keySet()) {
+            res.add(map.get(domain) + " " + domain);
+        }
+        return res;
+    }
+}
