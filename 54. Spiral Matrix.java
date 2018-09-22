caf53aa7254ecd0e8a00fc5964838976e888b44e
CodeToFLAG <hxyqbsmile@gmail.com>
2018-09-21 21:59:57 -0500

    Update 54. Spiral Matrix.java

--- 54. Spiral Matrix.java
+++ 54. Spiral Matrix.java
@@ -36,3 +36,53 @@ class Solution {
      return list;   
     }
 }
+
+class Solution {
+    public List<Integer> spiralOrder(int[][] matrix) {
+        if (matrix == null || matrix.length == 0) {
+            return new ArrayList<>();
+        }
+        List<Integer> res = new ArrayList<>();
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int row = 0;
+        int col = 0;
+        int count = 0;
+        boolean[][] visited = new boolean[m][n];
+        for (int i = 0; i < m * n; i++) {
+            res.add(matrix[row][col]);
+            visited[row][col] = true;
+            // right
+            if (count % 4 == 0) {
+                if (col + 1 == n || visited[row][col + 1] == true) {
+                    row++;
+                    count++;
+                } else {
+                    col++;
+                } 
+            } else if (count % 4 == 1) {  // down
+                if (row + 1 == m || visited[row + 1][col] == true) {
+                    col--;
+                    count++;
+                } else {
+                    row++;
+                }
+            } else if (count % 4 == 2) {  // left
+                if (col == 0 || visited[row][col - 1] == true) {
+                    row--;
+                    count++;
+                } else {
+                    col--;
+                }
+            } else {  // up
+                if (row == 0 || visited[row - 1][col] == true) {
+                    col++;
+                    count++;
+                } else {
+                    row--;
+                }
+            }
+        }  
+     return res;   
+    }
+}
