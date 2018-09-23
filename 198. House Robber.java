da231cbb8dbac0f0264a2e4f8926117c160e11e2
CodeToFLAG <hxyqbsmile@gmail.com>
2018-09-23 16:22:43 -0500

    Update 198. House Robber.java

--- 198. House Robber.java
+++ 198. House Robber.java
@@ -13,3 +13,27 @@ class Solution {
         return Math.max(rob, notRob);
     }
 }
+
+
+// 变种：给定一个int array，元素有正有负。求一个sum最大的subsequence，保证每次跳过的元素最多为一个。
+// 注意是subsequence，并且每次最多跳过一个元素，所以subsequence长度最少是len/2-1
+// e.g. [-1,-2,-3,-4,-5]，最大是-6，取-2，-4.
+// [-3,2,4,-1,-2,-5]，最大是4，取2，4，-2
+
+public int maxSubsequenceSum(int[] nums) {
+    if (nums == null || nums.length == 0) {
+        return 0;
+    }
+    int len = nums.length;
+    int pick = 0;
+    int notPick = 0;
+    for (int i = 0; i < len; i++) {
+        int prePick = pick;
+        pick = Math.max(pick, notPick) + nums[i]; // 这次pick，前面那次可以pick，也可以不pick
+        notPick = prePick; // 这次不pick，前面那次必定要pick。
+    }
+    return Math.max(pick, notPick);
+}
+
+// 类似的题还有：Maximum sum subarray removing at most one element
+// https://www.geeksforgeeks.org/maximum-sum-subarray-removing-one-element/
