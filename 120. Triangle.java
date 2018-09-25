21b589f59d829df12cbf2525da5f98b3a913d34d
CodeToFLAG <hxyqbsmile@gmail.com>
2018-09-25 14:52:38 -0500

    Update 120. Triangle.java

--- 120. Triangle.java
+++ 120. Triangle.java
@@ -32,3 +32,49 @@ class Solution {
         return res[0];
     }
 }
+
+/*
+print the path
+
+public static List<Integer> minimumTotal(List<List<Integer>> triangle) {
+
+        List<Integer> list = new ArrayList<>();
+        list.add(triangle.get(0).get(0));
+        int height = triangle.size();
+        int[][] path = new int[height][height];
+        List<Integer> ans = new ArrayList<>();
+        
+        for (int i  = 1; i < height; i++) {
+            list.add(0, list.get(0) + triangle.get(i).get(0));
+            path[i][0] = 0;
+            for (int j = 1; j < i; j++) {
+                if (list.get(j) <= list.get(j + 1)) {
+                    path[i][j] = j - 1;
+                    list.set(j, triangle.get(i).get(j) + list.get(j));
+                } else {
+                    path[i][j] = j;
+                    list.set(j, triangle.get(i).get(j) + list.get(j + 1));
+                }
+            }
+            list.set(i, list.get(i) + triangle.get(i).get(i));
+            path[i][i] = i - 1;
+        }
+        int res = Integer.MAX_VALUE;
+        int last = 0;
+        for (int i = 0; i < list.size(); i++) {
+            if (list.get(i) <= res) {
+                res = list.get(i);
+                last = i;
+            }
+        }
+        
+        ans.add(last);
+        int level = height - 1;
+        while (level > 0) {
+            ans.add(0, path[level][last]);
+            last = path[level][last];
+            level--;
+        }
+        return ans;
+    }
+*/
