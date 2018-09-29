b14a7911c28bf2c9c1a50259dcddd6318079827b
CodeToFLAG <hxyqbsmile@gmail.com>
2018-09-29 00:29:16 -0500

    Update 269. Alien Dictionary.java

--- 269. Alien Dictionary.java
+++ 269. Alien Dictionary.java
@@ -4,7 +4,9 @@ class Solution {
         if (words == null || words.length == 0) {
             return "";
         }
+        
         Map<Character, Integer> inDegree = new HashMap<>();
+        // 一个char，排在它后面的char有哪些
         Map<Character, Set<Character>> charAfter = new HashMap<>();
         
         for (String word : words) {
@@ -28,7 +30,7 @@ class Solution {
                         charAfter.get(c1).add(c2);
                         inDegree.put(c2, inDegree.get(c2) + 1);
                     }
-                    break;
+                    break; // 找到第一个不同后就break
                 }  
             } 
         }
@@ -59,51 +61,75 @@ class Solution {
 }
 
 // DFS
-
-private final int N = 26;
-public String alienOrder(String[] words) {
-    boolean[][] adj = new boolean[N][N];
-    int[] visited = new int[N];
-    buildGraph(words, adj, visited);
-
-    StringBuilder sb = new StringBuilder();
-    for(int i = 0; i < N; i++) {
-        if(visited[i] == 0) {                 // unvisited
-            if(!dfs(adj, visited, sb, i)) return "";
+class Solution {
+    public String alienOrder(String[] words) {
+        if (words == null || words.length == 0) {
+            return "";
         }
-    }
-    return sb.reverse().toString();
-}
-
-public boolean dfs(boolean[][] adj, int[] visited, StringBuilder sb, int i) {
-    visited[i] = 1;                            // 1 = visiting
-    for(int j = 0; j < N; j++) {
-        if(adj[i][j]) {                        // connected
-            if(visited[j] == 1) return false;  // 1 => 1, cycle   
-            if(visited[j] == 0) {              // 0 = unvisited
-                if(!dfs(adj, visited, sb, j)) return false;
-            }
-        }
-    }
-    visited[i] = 2;                           // 2 = visited
-    sb.append((char) (i + 'a'));
-    return true;
-}
-
-public void buildGraph(String[] words, boolean[][] adj, int[] visited) {
-    Arrays.fill(visited, -1);                 // -1 = not even existed
-    for(int i = 0; i < words.length; i++) {
-        for(char c : words[i].toCharArray()) visited[c - 'a'] = 0;
-        if(i > 0) {
-            String w1 = words[i - 1], w2 = words[i];
-            int len = Math.min(w1.length(), w2.length());
-            for(int j = 0; j < len; j++) {
-                char c1 = w1.charAt(j), c2 = w2.charAt(j);
-                if(c1 != c2) {
-                    adj[c1 - 'a'][c2 - 'a'] = true;
-                    break;
+        
+        boolean[][] graph = new boolean[26][26];
+        // 0: not exist
+        // 1: not visited
+        // 2: visiting
+        // 3: finished visiting
+        int[] visited = new int[26];
+        StringBuilder sb = new StringBuilder();
+        buildGraph(words, visited, graph);
+        
+        for (int i = 0; i < 26; i++) {
+            if (visited[i] == 1) {    // DFS, 必须检查没访问过才开始访问
+                if (hasCycle(graph, i, visited, sb)) {
+                    return "";
                 }
             }
+        }
+        return sb.reverse().toString();
+    }
+    
+    public boolean hasCycle(boolean[][] graph, int i, int[] visited, StringBuilder sb) {
+        if (visited[i] == 3) {
+            return false;
+        }
+
+        visited[i] = 2;
+        for (int j = 0; j < 26; j++) {
+            if (graph[i][j]) {
+                if (visited[j] == 2) {  // 不能少
+                    return true;
+                }
+                if (visited[j] == 1) {
+                    if (hasCycle(graph, j, visited, sb)) {
+                        return true;
+                    }
+                }
+            }
+        }
+        visited[i] = 3;
+        sb.append((char)(i + 'a'));
+        return false;
+    }
+        
+    public void buildGraph(String[] words, int[] visited, boolean[][] graph) {
+        for (int i = 0; i < words.length; i++) {
+            
+            for (char c : words[i].toCharArray()) {
+                visited[c - 'a'] = 1;
+            }
+            
+            if (i > 0) {
+                String word1 = words[i - 1];
+                String word2 = words[i];
+                int len = Math.min(word1.length(), word2.length());
+                for (int j = 0; j < len; j++) {
+                    char c1 = word1.charAt(j);
+                    char c2 = word2.charAt(j);
+                    if (c1 != c2) {
+                        graph[c1 - 'a'][c2 - 'a'] = true;
+                        break;
+                    }
+                }
+            }
+            
         }
     }
 }
