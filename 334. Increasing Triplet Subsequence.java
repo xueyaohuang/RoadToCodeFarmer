3494a767e8c5971e9f86e31b45a86a91c3762fc9
CodeToFLAG <hxyqbsmile@gmail.com>
2018-09-30 10:47:17 -0500

    Update and rename 7.6.2018/334. Increasing Triplet Subsequence.java to 334. Increasing Triplet Subsequence.java

--- 334. Increasing Triplet Subsequence.java
+++ 334. Increasing Triplet Subsequence.java
@@ -6,13 +6,13 @@ class Solution {
         int first = Integer.MAX_VALUE;
         int second = Integer.MAX_VALUE;
         for (int i = 0; i < nums.length; i++) {
-            if (nums[i] <= first) {
+            if (nums[i] <= first) {  // 先更新最小的，这样保证最小的一定在第二小的之前
                 first = nums[i];
             }
-            else if (nums[i] <= second) {
+            else if (nums[i] <= second) {  // 再更新第二小的，这样保证第二小的一定在第三之前
                 second = nums[i];
             }
-            else {
+            else {  // 跳过了前两个，这个比前两个大，又能保证顺序，返回true
                 return true;
             }
         }
