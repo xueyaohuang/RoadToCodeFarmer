fe16c7e1581922aeb964b4bdf146180c75e5a77b
CodeToFLAG <hxyqbsmile@gmail.com>
2018-10-04 15:44:45 -0500

    Update 416. Partition Equal Subset Sum.java

--- 416. Partition Equal Subset Sum.java
+++ 416. Partition Equal Subset Sum.java
@@ -1,3 +1,37 @@
+// 背包问题的变化
+class Solution {
+    public boolean canPartition(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return true;
+        }
+        int sum = 0;
+        for (int num : nums) {
+            sum += num;
+        }
+        if (sum % 2 == 1) {
+            return false;
+        }
+        sum /= 2;
+        int len = nums.length;
+        boolean[][] dp = new boolean[len + 1][sum + 1];
+        for (int i = 0; i<= len; i++) {
+            dp[i][0] = true;
+        }
+        
+        for (int i = 1; i <= len; i++) {
+            for (int j = 1; j <= sum; j++) {
+                dp[i][j] = dp[i - 1][j];
+                if (j >= nums[i - 1]) {
+                    dp[i][j] = dp[i - 1][j] || dp[i - 1][j - nums[i - 1]];
+                }
+            }
+        }
+        
+        return dp[len][sum];
+    }
+}
+
+// 优化空间复杂度，内层的for loop需要反转
 class Solution {
     public boolean canPartition(int[] nums) {
         if (nums == null || nums.length == 0) {
