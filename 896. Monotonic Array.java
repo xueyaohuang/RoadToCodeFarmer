58148c1f7ae0f9ccae1b32af1e0626c272c991a1
CodeToFLAG <hxyqbsmile@gmail.com>
2018-10-06 01:23:42 -0500

    Create 896. Monotonic Array.java

---
+++ 896. Monotonic Array.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean isMonotonic(int[] A) {
+        return isMonotonicIncreasing(A) || isMonotonicDecreasing(A);
+    }
+    
+    private boolean isMonotonicIncreasing(int[] A) {
+        int len = A.length;
+        for (int i = 0; i < len - 1; i++) {
+            if (A[i] > A[i + 1]) {
+                return false;
+            }
+        }
+        return true;
+    }
+    
+    private boolean isMonotonicDecreasing(int[] A) {
+        int len = A.length;
+        for (int i = 0; i < len - 1; i++) {
+            if (A[i] < A[i + 1]) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
