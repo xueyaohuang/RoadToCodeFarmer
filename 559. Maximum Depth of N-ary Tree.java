ee5e14af0afdfe4a74eeada79d6ce7d90ea3af04
CodeToFLAG <hxyqbsmile@gmail.com>
2018-10-07 01:36:03 -0500

    Create 559. Maximum Depth of N-ary Tree.java

---
+++ 559. Maximum Depth of N-ary Tree.java
@@ -0,0 +1,26 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val,List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+class Solution {
+    public int maxDepth(Node root) {
+        if (root == null) {
+            return 0;
+        }
+        int max = 0;
+        for (Node child : root.children) {
+            max = Math.max(max, maxDepth(child));
+        }
+        return max + 1;
+    }
+}
