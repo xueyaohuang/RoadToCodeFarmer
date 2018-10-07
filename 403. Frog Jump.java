b938b3e8f0eddf67445cdd27fabcef95fac9ab91
CodeToFLAG <hxyqbsmile@gmail.com>
2018-10-07 12:22:41 -0500

    Update 403. Frog Jump.java

--- 403. Frog Jump.java
+++ 403. Frog Jump.java
@@ -32,3 +32,46 @@ class Solution {
         return false;
     }
 }
+
+class Solution {
+    public boolean canCross(int[] stones) {
+        if (stones[1] - stones[0] != 1) {
+            return false;
+        } else if (stones.length == 2) {
+            return true;
+        }
+
+        int jump;
+        for (int i = stones.length - 2; i > 0; i--) {
+            jump = stones[stones.length - 1] - stones[i];
+            if (jump > i + 1) {
+                return false;
+            }
+            if (canJump(stones, jump, i)) {
+                return true;
+            }
+        }
+        return false;
+    }
+    public static boolean canJump(int[] stones, int jump, int stone) {
+        if (stone == 1) {
+            return jump < 3;
+        } else if (stone == 0) {
+            return false;
+        }
+        for (int cur = jump - 1; cur <= jump + 1; cur++) {
+            if (cur < 1) {
+                continue;
+            }
+            for (int i = stone - 1; i > 0; i--) {
+                if (stones[stone] - stones[i] > jump + 1) {
+                    break;
+                }
+                if (stones[stone] - stones[i] == cur && canJump(stones, cur, i)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
