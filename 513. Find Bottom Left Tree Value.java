d540bcd653b7a0e6b8a4635c66328bf0d876f5ed
CodeToFLAG <hxyqbsmile@gmail.com>
2018-10-10 20:24:36 -0500

    Update 513. Find Bottom Left Tree Value.java

--- 513. Find Bottom Left Tree Value.java
+++ 513. Find Bottom Left Tree Value.java
@@ -46,3 +46,24 @@ class Solution {
         return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
     }
 }
+
+class Solution {
+    int maxDepth = 0;
+    int res = 0;
+    public int findBottomLeftValue(TreeNode root) {
+        dfs(root, 1);
+        return res;
+    }
+    
+    private void dfs(TreeNode root, int depth) {
+        if (root == null) {
+            return;
+        }
+        if (depth > maxDepth) {
+            res = root.val;
+            maxDepth = depth;
+        }
+        dfs(root.left, depth + 1);
+        dfs(root.right, depth + 1);
+    }
+}
