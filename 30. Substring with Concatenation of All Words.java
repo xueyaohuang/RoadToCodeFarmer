7c22f0c110bc32363e661b9407f763e0ceec9985
CodeToFLAG <hxyqbsmile@gmail.com>
2018-10-17 15:17:29 -0500

    Update 30. Substring with Concatenation of All Words.java

--- 30. Substring with Concatenation of All Words.java
+++ 30. Substring with Concatenation of All Words.java
@@ -1,68 +1,96 @@
+// O(kn)
+// k is the length of String[] words, n is the length of s
 class Solution {
     public List<Integer> findSubstring(String s, String[] words) {
-        int n = s.length(), m = words.length;
-        List<Integer> result = new ArrayList<>();
-        if (m == 0) return result;
-        int l = words[0].length();
-        
-        Map<String, Integer> map = new HashMap<>();
-        for (String w : words) {
-            map.put(w, map.getOrDefault(w, 0) + 1);
+        if (s == null || words == null || words.length == 0) {
+            return new ArrayList<>();
         }
         
-        for (int i = 0; i < l; i++) {
-            for (int j = i; j + m * l <= n; j = j + l) {
-                String ss = s.substring(j, j + m * l);
-                Map<String, Integer> temp = new HashMap<>();
-                for (int k = m - 1; k >= 0; k--) {
-                    String w = ss.substring(k * l, (k + 1) * l);
-                    int count = temp.getOrDefault(w, 0) + 1;
-                    
-                    if (count > map.getOrDefault(w, 0)) {
-                        j = j + k * l;
-                        break;
-                    } else if (k == 0) {
-                        result.add(j);
-                    } else {
-                        temp.put(w, count);
+        Map<String, Integer> map = new HashMap<>();
+        List<Integer> res = new ArrayList<>();
+        int lens = s.length();
+        int lenws = words.length;
+        int lenwd = words[0].length();
+        int lenWindow = lenws * lenwd;
+        
+        for (String word : words) {
+            map.put(word, map.getOrDefault(word, 0) + 1);
+        }
+        
+        for (int i = 0; i + lenWindow <= lens; i++) {
+            String window = s.substring(i, i + lenWindow);
+            Map<String, Integer> windowMap = new HashMap<>();
+            
+            for (int j = 0; j < lenws; j++) {
+                String curWord = window.substring(j * lenwd, (j + 1) * lenwd);
+                int count = windowMap.getOrDefault(curWord, 0) + 1;
+                if (count > map.getOrDefault(curWord, 0)) {
+                    break;
+                } else if (j == lenws - 1) {
+                    res.add(i);
+                } else {
+                    windowMap.put(curWord, count);
+                }
+            }
+        }
+        
+        return res;
+    }
+}
+
+// O(n) sliding window
+// outer for loop O(length of each word(wl))
+// inner for loop, it is a sliding window, O(n/wl)
+// mostly 2 times travel for each word, one left side of the window, the other right side of the window
+// so, time complexity O(wl * 2 * n/wl) = O(n)
+class Solution {
+    public List<Integer> findSubstring(String s, String[] words) {
+        if (s == null || words == null || words.length == 0) {
+            return new ArrayList<>();
+        }
+        
+        Map<String, Integer> map = new HashMap<>();
+        Map<String, Integer> windowMap = new HashMap<>();
+        List<Integer> res = new ArrayList<>();
+        int lens = s.length();
+        int lenws = words.length;
+        int lenwd = words[0].length();
+        int lenWindow = lenws * lenwd;
+        
+        for (String word : words) {
+            map.put(word, map.getOrDefault(word, 0) + 1);
+        }
+        
+        for (int i = 0; i < lenwd; i++) {
+            int left = i, right = i, count = 0;
+            windowMap = new HashMap<>();
+            // sliding window right pointer
+            for (; right + lenwd <= lens; right += lenwd) {
+                String curWord = s.substring(right, right + lenwd);
+                // 没有当前word，所有清零重来
+                if (!map.containsKey(curWord)) {
+                    count = 0;
+                    left = right + lenwd;
+                    windowMap = new HashMap<>();
+                } else {
+                    windowMap.put(curWord, windowMap.getOrDefault(curWord, 0) + 1);
+                    count++;
+                    // 移动left pointer
+                    while (windowMap.get(curWord) > map.get(curWord)) {
+                        String removedLeft = s.substring(left, left + lenwd);
+                        windowMap.put(removedLeft, windowMap.get(removedLeft) - 1);
+                        count--;
+                        left += lenwd;
+                    }
+                    if (count == lenws) {
+                        res.add(left);
                     }
                 }
             }
         }
-        return result;        
+        return res;
     }
 }
 
 
-class Solution {
-    public List<Integer> findSubstring(String s, String[] words) {
-        int n = s.length(), m = words.length;
-        List<Integer> result = new ArrayList<>();
-        if (m == 0) return result;
-        int l = words[0].length();
-        
-        Map<String, Integer> map = new HashMap<>();
-        for (String w : words) {
-            map.put(w, map.getOrDefault(w, 0) + 1);
-        }
-        
-        for (int i = 0; i + m * l <= n; i++) {
-            String ss = s.substring(i, i + m * l);
-            Map<String, Integer> temp = new HashMap<>();
-            for (int k = m - 1; k >= 0; k--) {
-                String w = ss.substring(k * l, (k + 1) * l);
-                int count = temp.getOrDefault(w, 0) + 1;
 
-                if (count > map.getOrDefault(w, 0)) {
-                    break;
-                } else if (k == 0) {
-                    result.add(i);
-                } else {
-                    temp.put(w, count);
-                }
-            }
-            
-        }
-        return result;        
-    }
-}
