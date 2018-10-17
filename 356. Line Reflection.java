7ad90d9e2e9b66c971751a304b8505037e24baea
CodeToFLAG <hxyqbsmile@gmail.com>
2018-10-17 18:08:37 -0500

    Update 356. Line Reflection.java

--- 356. Line Reflection.java
+++ 356. Line Reflection.java
@@ -31,3 +31,29 @@ class Solution {
     }
 }
 
+class Solution {
+    public boolean isReflected(int[][] points) {
+        int max = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
+        Set<String> set = new HashSet<>();
+        
+        for (int[] p : points) {
+            max = Math.max(max, p[0]);
+            min = Math.min(min, p[0]);
+            set.add(p[0] + "," + p[1]);
+        }
+        
+        int sum = min + max;
+        
+        for (String s : set) {
+            String[] strs = s.split(",");
+            int x = sum - Integer.parseInt(strs[0]);
+            int y = Integer.parseInt(strs[1]);
+            String oppo = x + "," + y;
+            if (!set.contains(oppo)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
