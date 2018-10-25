ea37c2b05d045bdcd0eca5771a55593fa85d450d
CodeToFLAG <hxyqbsmile@gmail.com>
2018-10-25 12:15:06 -0500

    Update 298. Binary Tree Longest Consecutive Sequence.java

--- 298. Binary Tree Longest Consecutive Sequence.java
+++ 298. Binary Tree Longest Consecutive Sequence.java
@@ -15,6 +15,7 @@ class Solution {
         return longestConsecutiveHelper(root, root.val, 0);
     }
     
+    // dfs，在每个节点都把curMax与当前最大比较
     private int longestConsecutiveHelper(TreeNode root, int target, int curMax) {
         if (root == null) {
             return curMax;
@@ -27,5 +28,37 @@ class Solution {
         int left = longestConsecutiveHelper(root.left, root.val + 1, curMax);
         int right = longestConsecutiveHelper(root.right, root.val + 1, curMax);
         return Math.max(curMax, Math.max(left, right));
+    }
+}
+
+class Solution {
+    public int longestConsecutive(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int[] res = new int[1];
+        longestConsecutiveHelper(root, res);
+        return res[0];
+    }
+    
+    private int longestConsecutiveHelper(TreeNode root, int[] res) {
+        if (root == null) {
+            return 0;
+        }
+        
+        int left = longestConsecutiveHelper(root.left, res);
+        int right = longestConsecutiveHelper(root.right, res);
+        int len = 0;
+        
+        if (root.left != null && root.left.val == root.val + 1) {
+            len = left;
+        }
+        if (root.right != null && root.right.val == root.val + 1) {
+            len = Math.max(len, right);
+        }
+        
+        res[0] = Math.max(res[0], len + 1);
+        
+        return len + 1;
     }
 }
