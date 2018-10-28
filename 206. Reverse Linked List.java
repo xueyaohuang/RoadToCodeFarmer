f36f63794381d36766b3a68358d27aa4162f62f2
CodeToFLAG <hxyqbsmile@gmail.com>
2018-10-28 16:30:35 -0500

    Update 206. Reverse Linked List.java

--- 206. Reverse Linked List.java
+++ 206. Reverse Linked List.java
@@ -18,6 +18,19 @@ class Solution {
         return prev;
     }
 }
+
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode reversedHead = reverseList(head.next);
+        head.next.next = head;
+        head.next = null;
+        return reversedHead;
+    }
+}
+
 class Solution {
     public ListNode reverseList(ListNode head) {
         return helper(head, null);
