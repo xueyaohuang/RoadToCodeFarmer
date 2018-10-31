4cfe7a3ba14fa4d303418a05497a305f459a3800
CodeToFLAG <hxyqbsmile@gmail.com>
2018-10-31 03:32:00 -0500

    Create 115. Distinct Subsequences.java

---
+++ 115. Distinct Subsequences.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int numDistinct(String s, String t) {
+        if (s == null || t == null) {
+            return 0;
+        }
+        int lens = s.length();
+        int lent = t.length();
+        int[][] dp = new int[lens + 1][lent + 1];
+        
+        for (int i = 0; i < lens; i++) {
+            dp[i][0] = 1;
+        }
+        
+        for (int i = 1; i <= lens; i++) {
+            for (int j = 1; j <= lent; j++) {
+                if (s.charAt(i - 1) == t.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j];
+                } else {
+                    dp[i][j] = dp[i - 1][j];
+                }
+            }
+        }
+        return dp[lens][lent];
+    }
+}
