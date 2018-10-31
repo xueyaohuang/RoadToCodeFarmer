3bd01b12cf576e710a2920fe89fa9a63fa2ce6e8
CodeToFLAG <hxyqbsmile@gmail.com>
2018-10-31 02:34:37 -0500

    Create 889. Construct Binary Tree from Preorder and Postorder Traversal.java

---
+++ 889. Construct Binary Tree from Preorder and Postorder Traversal.java
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public TreeNode constructFromPrePost(int[] pre, int[] post) {
+        if (pre == null || post == null) {
+            return null;
+        }
+        int len = pre.length;
+        // stack里放的是没有完成的node，当stack.peek().val == post[j]，说明这个node和它的子树完全建立好了，就把这个node
+        // 从stack里pop出来
+        Deque<TreeNode> stack = new ArrayDeque<>();
+        TreeNode root = new TreeNode(pre[0]);
+        stack.push(root);
+        int postIdx = 0;
+        
+        for (int i = 1; i < len; i++) {
+            TreeNode node = new TreeNode(pre[i]);
+            while (stack.peek().val == post[postIdx]) {
+                stack.pop();
+                postIdx++;
+            }
+            if (stack.peek().left == null) {
+                stack.peek().left = node;
+            } else {
+                stack.peek().right = node;
+            }
+            stack.push(node);
+        }
+        
+        return root;
+    }
+}
