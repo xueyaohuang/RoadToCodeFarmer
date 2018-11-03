a3b7b01c9dc9ef5ba030ad80eebfcf9b5f360482
CodeToFLAG <hxyqbsmile@gmail.com>
2018-11-03 18:40:51 -0500

    Update 204. Count Primes.java

--- 204. Count Primes.java
+++ 204. Count Primes.java
@@ -1,18 +1,34 @@
 class Solution {
     public int countPrimes(int n) {
-        if (n < 3) {
+        if (n <= 2) {
             return 0;
         }
         boolean[] notPrime = new boolean[n];
-        int count = 0;
+        int count = 1;
         for (int i = 3; i < n; i += 2) {
-            if (! notPrime[i]) {
+            if (!notPrime[i]) {
                 count++;
-            }
-            for (int j = 3; i * j < n; j += 2) {
-                notPrime[i * j] = true;
+                // 这个for j的循环放到if里面比外面快，因为放到外面会有重复的i * j被再次设置成true
+                for (int j = 3; i * j < n; j += 2) {
+                    notPrime[i * j] = true;
+                }
             }
         }
-        return count + 1; //i start from 3, missing 2, so plus 1.
+        return count;
     }
+    
+    // private boolean isPrime(int n) {
+    //     if (n <= 1) {
+    //         return false;
+    //     }
+    //     if (n % 2 == 0) {
+    //         return false;
+    //     }
+    //     for (int i = 3; i * i <= n; i += 2) {
+    //         if (n % i == 0) {
+    //             return false;
+    //         }
+    //     }
+    //     return true;
+    // }
 }
