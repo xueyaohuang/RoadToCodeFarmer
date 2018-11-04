6202fe01cab342ddfc31aa3a89f5afdc24f2679c
CodeToFLAG <hxyqbsmile@gmail.com>
2018-11-04 00:47:54 -0500

    Update 279. Perfect Squares.java

--- 279. Perfect Squares.java
+++ 279. Perfect Squares.java
@@ -1,7 +1,7 @@
 class Solution {
     public int numSquares(int n) {
         int[] dp = new int[n + 1];
-        Arrays.fill(dp, Integer.MAX_VALUE);
+        Arrays.fill(dp, n);
         dp[0] = 0;
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j * j <= i; j++) {
