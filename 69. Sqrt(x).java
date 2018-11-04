4552eddf1a9a7b2e68e01a23351e02e303288729
CodeToFLAG <hxyqbsmile@gmail.com>
2018-11-03 19:04:12 -0500

    Update 69. Sqrt(x).java

--- 69. Sqrt(x).java
+++ 69. Sqrt(x).java
@@ -10,18 +10,15 @@ class Solution {
 
 class Solution {
     public int mySqrt(int x) {
-        int start = 0;
-        int end = x;
+        int start = 0, end = x;
         while (start < end) {
-            int mid = (end + start + 1) / 2;
+            int mid = (start + end + 1) / 2;
             if (mid == x / mid) {
                 return mid;
-            }
-            else if (mid > x / mid) {
-                end = mid - 1;
-            }
-            else {
+            } else if (mid < x / mid) {
                 start = mid;
+            } else {
+                end = mid - 1;
             }
         }
         return start;
