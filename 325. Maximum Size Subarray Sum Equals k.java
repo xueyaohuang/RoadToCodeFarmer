e06b4724b5b43ae145616514c3bcdbc763c309e9
CodeToFLAG <hxyqbsmile@gmail.com>
2018-11-05 19:03:34 -0600

    Create 325. Maximum Size Subarray Sum Equals k.java

---
+++ 325. Maximum Size Subarray Sum Equals k.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int maxSubArrayLen(int[] nums, int k) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        int len = nums.length;
+        int maxLen = 0, sum = 0;
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(0, -1);
+        
+        for (int i = 0; i < len; i++) {
+            sum += nums[i];
+            // 因为是求最长的长度，所以要保留最先出现的sum的位置
+            if (!map.containsKey(sum)) {
+                map.put(sum, i);
+            } 
+            if (map.containsKey(sum - k)) {
+                maxLen = Math.max(maxLen, i - map.get(sum - k));
+            }
+        }
+        return maxLen;
+    }
+}
