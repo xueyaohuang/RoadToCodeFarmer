c7b7239570f1d26e59e9f4019de5209354bb95e7
CodeToFLAG <hxyqbsmile@gmail.com>
2018-11-05 21:07:45 -0600

    Update 462. Minimum Moves to Equal Array Elements II.java

--- 462. Minimum Moves to Equal Array Elements II.java
+++ 462. Minimum Moves to Equal Array Elements II.java
@@ -1,11 +1,45 @@
 // https://leetcode.com/problems/best-meeting-point/discuss/74209/strict-mathematics-proof-to-minimize-sum_i-absx-x_i-using-triangle-inequality
 class Solution {
     public int minMoves2(int[] nums) {
-        int sum = 0;
-        Arrays.sort(nums);
-        for (int i = 0; i < nums.length / 2; i++) {
-            sum += nums[nums.length - 1 - i] - nums[i];
+        int res = 0;
+        int len = nums.length;
+        int median = randomQuickSelect(nums, 0, len - 1, len / 2);
+        
+        for (int i = 0; i < len; i++) {
+            res += Math.abs(nums[i] - median);
         }
-        return sum;
+        
+        return res;
+    }
+    
+    private int randomQuickSelect(int[] nums, int start, int end, int k) {
+        Random rand = new Random();
+        int pivotIdx = rand.nextInt(end - start + 1) + start;
+        int pivot = nums[pivotIdx];
+        swap(nums, pivotIdx, end);
+        int left = start;
+        
+        for (int i = start; i < end; i++) {
+            if (nums[i] <= pivot) {
+                swap(nums, left, i);
+                left++;
+            }
+        }
+        
+        swap(nums, left, end);
+        
+        if (left == k) {
+            return nums[k];
+        } else if (left < k) {
+            return randomQuickSelect(nums, left + 1, end, k);
+        } else {
+            return randomQuickSelect(nums, start, left - 1, k);
+        }
+    }
+    
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
     }
 }
