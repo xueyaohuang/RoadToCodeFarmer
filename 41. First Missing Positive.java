4cb1ced73fffbf6a02b72e1de1d23943b482e178
CodeToFLAG <hxyqbsmile@gmail.com>
2018-11-13 12:23:11 -0600

    Update 41. First Missing Positive.java

--- 41. First Missing Positive.java
+++ 41. First Missing Positive.java
@@ -4,6 +4,7 @@ class Solution {
             return 1;
         }
         for (int i = 0; i < nums.length; i++) {
+            // 这里必须用while，因为可能当前不对的换走了，新来的还是不对
             while (nums[i] > 0 && nums[i] <= nums.length && nums[i] != nums[nums[i] - 1]) {
                 int temp = nums[nums[i] - 1];
                 nums[nums[i] - 1] = nums[i];
