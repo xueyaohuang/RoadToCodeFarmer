4b09bb9cafa20cd6c7cf61a8a801c015d685f906
CodeToFLAG <hxyqbsmile@gmail.com>
2018-11-13 01:12:43 -0600

    Update 240. Search a 2D Matrix II.java

--- 240. Search a 2D Matrix II.java
+++ 240. Search a 2D Matrix II.java
@@ -5,6 +5,7 @@ class Solution {
         }
         int row = matrix.length;
         int col = matrix[0].length;
+        // 从左下角开始，也可以从右上角开始
         int a = row -1;
         int b = 0;
         
@@ -13,7 +14,7 @@ class Solution {
                 return true;
             }
             else if (matrix[a][b] > target){
-                a--;
+                a--;  // 不需要b--,因为刚从左边一列较大一个值（比如x）移过来，如过要找的数是y，y肯定比x大，所以不需要b--
             }
             else {
                 b++;
