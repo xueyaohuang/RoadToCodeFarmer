c226f5a2fc07a232d7d8d9e6357d62545aa2e7ca
CodeToFLAG <hxyqbsmile@gmail.com>
2018-11-13 22:45:34 -0600

    Update 384. Shuffle an Array.java

--- 384. Shuffle an Array.java
+++ 384. Shuffle an Array.java
@@ -3,6 +3,46 @@
 Fisher–Yates shuffle Algorithm : O(n), assuming that the function rand() takes O(1) time.
 数学归纳法证明：某个数在ith position的概率是1/n = (n-1)/n * (n-2)/(n-1) * ... * (n-i)/(n-i+1) * (1)/(n-i)
 */
+/*
+比如1，2，3，4，5，6
+从最后的index 5开始，任意一个数最后在index 5的概率都是1/6。
+任意一个数最后在index 4的概率都是 5/6 * 1/5 (5/6是第一次没有被选中到最后的概率，1/5是这次被选中在4的概率)
+...
+某个数在ith position的概率是1/n = (n-1)/n * (n-2)/(n-1) * ... * (n-i)/(n-i+1) * (1)/(n-i)
+*/
+
+// 从后往前比较好解释
+class Solution {
+    
+    int[] copy;
+
+    public Solution(int[] nums) {
+        copy = nums.clone();
+    }
+    
+    /** Resets the array to its original configuration and return it. */
+    public int[] reset() {
+        return copy;
+    }
+    
+    /** Returns a random shuffling of the array. */
+    public int[] shuffle() {
+        int[] res = copy.clone();
+        int len = res.length;
+        Random rand = new Random();
+        for (int i = len - 1; i >= 0; i--) {
+            int j = rand.nextInt(i + 1);
+            swap(res, i, j);
+        }
+        return res;
+    }
+    
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+}
 
 class Solution {
     
