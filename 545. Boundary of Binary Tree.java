a52cb4d5522ddb5fc91dc35d3df66e901d0bd78e
CodeToFLAG <hxyqbsmile@gmail.com>
2018-11-18 17:56:49 -0600

    Update 545. Boundary of Binary Tree.java

--- 545. Boundary of Binary Tree.java
+++ 545. Boundary of Binary Tree.java
@@ -84,3 +84,21 @@ class Solution {
         getLeaves(root.right, leaves);
     }
 }
+
+// one pass
+class Solution {
+    public List<Integer> boundaryOfBinaryTree(TreeNode root) {
+        List<Integer> left = new LinkedList<>(), right = new LinkedList<>();
+        preorder(root, left, right, 0);
+        left.addAll(right);
+        return left;
+    }
+
+    public void preorder(TreeNode cur, List<Integer> left, List<Integer> right, int flag) {
+        if (cur == null) return;
+        if (flag == 2) right.add(0, cur.val);
+        else if (flag <= 1 || cur.left == null && cur.right == null) left.add(cur.val);
+        preorder(cur.left, left, right, flag <= 1 ? 1 : (flag == 2 && cur.right == null) ? 2 : 3);
+        preorder(cur.right, left, right, flag % 2 == 0 ? 2 : (flag == 1 && cur.left == null) ? 1 : 3);
+    }
+}
