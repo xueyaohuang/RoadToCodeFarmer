739926fc100c849661a569042103bafeafb7938d
CodeToFLAG <hxyqbsmile@gmail.com>
2018-11-21 16:55:03 -0600

    Update 148. Sort List.java

--- 148. Sort List.java
+++ 148. Sort List.java
@@ -50,4 +50,21 @@ class Solution {
         }
         return dummy.next;
     }
+    
+    // recursice way of merge two sorted list
+    private ListNode merge(ListNode l1, ListNode l2){
+        if (l1 == null) {
+            return l2;
+        }
+        if (l2 == null) {
+            return l1;
+        }
+        if (l1.val < l2.val) {
+            l1.next = merge(l1.next, l2);
+            return l1;
+        } else{
+            l2.next = merge(l2.next,l1);
+            return l2;
+        }
+    }
 }
