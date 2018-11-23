da923a049ac8f12dcb12641bc47c639a120dbcec
CodeToFLAG <hxyqbsmile@gmail.com>
2018-11-23 17:30:57 -0600

    Update 36. Valid Sudoku.java

--- 36. Valid Sudoku.java
+++ 36. Valid Sudoku.java
@@ -48,15 +48,15 @@ class Solution {
 
 class Solution {
     public boolean isValidSudoku(char[][] board) {
-        HashSet<String> set = new HashSet<>();
+        Set<String> set = new HashSet<>();
         for (int i = 0; i < 9; i++) {
             for (int j = 0; j < 9; j++) {
-                char num = board[i][j];
-                if (num != '.') {
-                    if (! set.add("row" + i + num) || ! set.add("col" + j + num) || ! set.add("cell" + i / 3 + j / 3 + num)) {
+                if (board[i][j] != '.') {
+                    int num = board[i][j] - '0';
+                    if (!set.add("r " + i + " " + num) || !set.add("c " + j + " " + num) || !set.add("b " + i / 3 + j / 3 + num)) {
                         return false;
                     }
-                }
+                } 
             }
         }
         return true;
