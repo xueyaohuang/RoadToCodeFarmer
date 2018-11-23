d780c13c1c0a98e5fe86e5bdd66a41a1bc5cc9ec
CodeToFLAG <hxyqbsmile@gmail.com>
2018-11-22 18:29:39 -0600

    Create 636. Exclusive Time of Functions.java

---
+++ 636. Exclusive Time of Functions.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int[] exclusiveTime(int n, List<String> logs) {
+        if (n == 0 || logs == null || logs.size() == 0) {
+            return new int[0];
+        }
+        Deque<Integer> stack = new ArrayDeque<>();
+        int[] res = new int[n];
+        int prevTime = 0;
+
+        for (String log : logs) {
+            String[] info = log.split(":");
+            int id = Integer.valueOf(info[0]);
+            int time = Integer.valueOf(info[2]);
+            
+            if (info[1].equals("start")) {
+                if (!stack.isEmpty()) {
+                    res[stack.peek()] += time - prevTime;
+                }
+                stack.push(id);
+                prevTime = time;
+            } else {
+                res[stack.peek()] += time - prevTime + 1;
+                stack.pop();
+                prevTime = time + 1; // 巧妙之处
+            }
+        }
+        return res;
+    }
+}
