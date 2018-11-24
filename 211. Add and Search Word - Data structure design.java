5c67a247ac5ed8f053ba3818e533373745374016
CodeToFLAG <hxyqbsmile@gmail.com>
2018-11-24 01:04:46 -0600

    Update 211. Add and Search Word - Data structure design.java

--- 211. Add and Search Word - Data structure design.java
+++ 211. Add and Search Word - Data structure design.java
@@ -1,4 +1,72 @@
 class WordDictionary {
+    
+    class TrieNode {
+        TrieNode[] children;
+        boolean isWord;
+        public TrieNode() {
+            this.children = new TrieNode[26];
+            this.isWord = false;
+        }
+    }
+    
+    private TrieNode root;
+    /** Initialize your data structure here. */
+    public WordDictionary() {
+        root = new TrieNode();
+    }
+    
+    /** Adds a word into the data structure. */
+    public void addWord(String word) {
+        if (word == null || word.length() == 0) {
+            return;
+        }
+        
+        TrieNode cur = root;
+        for (char c : word.toCharArray()) {
+            if (cur.children[c - 'a'] == null) {
+                cur.children[c - 'a'] = new TrieNode();
+            }
+           cur = cur.children[c - 'a']; 
+        }
+        cur.isWord = true;
+    }
+    
+    /** Returns if the word is in the data structure. A word could contain the dot character '.' to represent any one letter. */
+    public boolean search(String word) {
+        return searchHelper(word, root);
+    }
+    
+    private boolean searchHelper(String word, TrieNode root) {
+        TrieNode cur = root;
+        for (int i = 0; i < word.length(); i++) {
+            char c = word.charAt(i);
+            if (c == '.') {
+                String rest = word.substring(i + 1);
+                for (TrieNode newRoot : cur.children) {
+                    if (newRoot != null && searchHelper(rest, newRoot)) {
+                        return true;
+                    }
+                }
+                return false;
+            } else {
+                if (cur.children[c - 'a'] == null) {
+                    return false;
+                }
+               cur = cur.children[c - 'a'];
+            }
+        }
+        return cur.isWord;
+    }
+}
+
+/**
+ * Your WordDictionary object will be instantiated and called as such:
+ * WordDictionary obj = new WordDictionary();
+ * obj.addWord(word);
+ * boolean param_2 = obj.search(word);
+ */
+
+class WordDictionary {
 
     private TrieNode root;
     /** Initialize your data structure here. */
