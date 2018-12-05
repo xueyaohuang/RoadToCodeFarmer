695d9aba1596b0277a0d9ed6eaf8422d980eabc8
CodeToFLAG <hxyqbsmile@gmail.com>
2018-12-05 00:39:22 -0600

    Update 582. Kill Process.java

--- 582. Kill Process.java
+++ 582. Kill Process.java
@@ -20,3 +20,26 @@ class Solution {
         return res;
     }
 }
+
+class Solution {
+    public List<Integer> killProcess(List<Integer> pid, List<Integer> ppid, int kill) {
+        Map<Integer, List<Integer>> adj = new HashMap<>();
+        List<Integer> res = new ArrayList<>();
+        // 构建adj list的graph
+        for (int i = 0; i < pid.size(); i++) {
+            adj.putIfAbsent(ppid.get(i), new ArrayList<>());
+            adj.get(ppid.get(i)).add(pid.get(i));
+        }
+        dfs(adj, res, kill);
+        return res;
+    }
+    
+    private void dfs(Map<Integer, List<Integer>> adj, List<Integer> res, int cur) {
+        res.add(cur);
+        if (adj.containsKey(cur)) {
+            for (int next : adj.get(cur)) {
+                dfs(adj, res, next);
+            }
+        }
+    }
+}
