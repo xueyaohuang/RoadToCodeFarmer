86364683f2b58e1267e834a9e6b61a5584fc8336
CodeToFLAG <hxyqbsmile@gmail.com>
2018-12-05 22:00:06 -0800

    Update 210. Course Schedule II.java

--- 210. Course Schedule II.java
+++ 210. Course Schedule II.java
@@ -2,39 +2,43 @@
 // 这就是topological sort，当完成对一个点的访问后，加入list中，dfs是逆序，所以要用stack。
 class Solution {
     public int[] findOrder(int numCourses, int[][] prerequisites) {
+        if (numCourses == 0) {
+            return new int[0];
+        }
         List<Integer>[] adj = new ArrayList[numCourses];
-        int[] visited = new int[numCourses];
+        for (int i = 0; i < numCourses; i++) {
+            adj[i] = new ArrayList<>();
+        }
+        for (int[] prerequisite : prerequisites) {
+            adj[prerequisite[1]].add(prerequisite[0]);
+        }
         Deque<Integer> stack = new ArrayDeque<>();
+        int[] visited = new int[numCourses];
         for (int i = 0; i < numCourses; i++) {
-            adj[i] = new ArrayList<Integer>();
-        }
-        for (int i = 0; i < prerequisites.length; i++) {
-            adj[prerequisites[i][1]].add(prerequisites[i][0]);
-        }
-        for (int i = 0; i < numCourses; i++) {
-            if (hasCycle(adj, i, visited, stack)) {
-                return new int[0];
+            if (visited[i] == 0) {
+                if (hasCycle(adj, i, visited, stack)) {
+                    return new int[0];
+                }
             }
         }
-        int i = 0;
         int[] res = new int[numCourses];
+        int idx = 0;
         while (!stack.isEmpty()) {
-            res[i++] = stack.pop();
+            res[idx++] = stack.pop();
         }
         return res;
     }
+    
     private boolean hasCycle(List<Integer>[] adj, int i, int[] visited, Deque<Integer> stack) {
-        // visit[i] == 1 means is visiting
-        if (visited[i] == 1) {
-            return true;
-        }    
-        // visit[i] == 2 means has been visited
         if (visited[i] == 2) {
             return false;
-        }      
+        }
+        if (visited[i] == 1) {
+            return true;
+        }
         visited[i] = 1;
-        for (int j = 0; j < adj[i].size(); j++) {
-            if (hasCycle(adj, adj[i].get(j), visited, stack)) {
+        for (int j : adj[i]) {
+            if (hasCycle(adj, j, visited, stack)) {
                 return true;
             }
         }
