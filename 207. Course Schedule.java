72901d48bc5ccd70a1a200092eb2f238c3312dc6
CodeToFLAG <hxyqbsmile@gmail.com>
2018-12-05 21:57:20 -0800

    Update 207. Course Schedule.java

--- 207. Course Schedule.java
+++ 207. Course Schedule.java
@@ -1,22 +1,27 @@
 // DFS detect cycle solution.
 class Solution {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
-        List<Integer>[] adj = new ArrayList[numCourses];
         int[] visited = new int[numCourses];
+        List<Integer>[] adjList = new ArrayList[numCourses];
         for (int i = 0; i < numCourses; i++) {
-            adj[i] = new ArrayList<Integer>();
+            adjList[i] = new ArrayList<>();
         }
         for (int i = 0; i < prerequisites.length; i++) {
-            adj[prerequisites[i][1]].add(prerequisites[i][0]);
+            adjList[prerequisites[i][1]].add(prerequisites[i][0]);
         }
+        
         for (int i = 0; i < numCourses; i++) {
-            if (hasCycle(adj, i, visited)) {
-                return false;
+            if (visited[i] == 0) {
+                if (hasCycle(adjList, i, visited)) {
+                    return false;
+                }
             }
         }
         return true;
     }
-    private boolean hasCycle(List<Integer>[] adj, int i, int[] visited) {
+    
+    private boolean hasCycle(List<Integer>[] adjList, int i, int[] visited) {
+        
         if (visited[i] == 1) {
             return true;
         }
@@ -24,8 +29,8 @@ class Solution {
             return false;
         }
         visited[i] = 1;
-        for (int j = 0; j < adj[i].size(); j++) {
-            if (hasCycle(adj, adj[i].get(j), visited)) {
+        for (int j = 0; j < adjList[i].size(); j++) {
+            if (hasCycle(adjList, adjList[i].get(j), visited)) {
                 return true;
             }
         }
