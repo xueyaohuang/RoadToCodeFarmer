cab1b4a91a81160f1f203c129b0ca43d1d6cc9e1
CodeToFLAG <hxyqbsmile@gmail.com>
2018-12-20 00:18:31 -0600

    Update 1. Two Sum.java

--- 1. Two Sum.java
+++ 1. Two Sum.java
@@ -1,17 +1,15 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        Map<Integer, Integer> map = new HashMap<>();
         int len = nums.length;
-        int[] res = new int[2];
+        Map<Integer, Integer> map = new HashMap<>();
+        
         for (int i = 0; i < len; i++) {
             if (map.containsKey(target - nums[i])) {
-                res[0] = map.get(target - nums[i]);
-                res[1] = i;
-                return res; // return early
+                return new int[]{map.get(target - nums[i]), i};
             } else {
                 map.put(nums[i], i);
             }
         }
-        return res;
+        return new int[]{-1, -1};
     }
 }
