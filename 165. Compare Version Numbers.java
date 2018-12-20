9f46d5edce5117b8b9f7f5de8a1ee79f5cb9fb27
CodeToFLAG <hxyqbsmile@gmail.com>
2018-12-20 00:35:10 -0600

    Update 165. Compare Version Numbers.java

--- 165. Compare Version Numbers.java
+++ 165. Compare Version Numbers.java
@@ -2,39 +2,36 @@ class Solution {
     public int compareVersion(String version1, String version2) {
         String[] v1 = version1.split("\\.");
         String[] v2 = version2.split("\\.");
+        int i = 0;
+        int len1 = v1.length, len2 = v2.length;
         
-        int len1 = v1.length;
-        int len2 = v2.length;
-        
-        for (int i = 0; i < Math.min(len1, len2); i++) {
+        while (i < len1 && i < len2) {
             int num1 = Integer.parseInt(v1[i]);
             int num2 = Integer.parseInt(v2[i]);
-            if (num1 == num2) {
-                System.out.println("=");
-                continue;
-            } else if (num1 < num2) {
+            if (num1 < num2) {
                 return -1;
-                
-            } else {
+            } else if (num1 > num2) {
                 return 1;
+            } else {
+                i++;
             }
         }
         if (len1 == len2) {
             return 0;
-        } else if (len1 < len2) {
-            for (int i = len1; i < len2; i++) {
-                int num = Integer.parseInt(v2[i]);
-                if (num > 0) {
-                    return -1;
+        } else if (len1 > len2) {
+            while (i < len1) {
+                if (Integer.parseInt(v1[i]) != 0) {
+                    return 1;
                 }
+                i++;
             }
             return 0;
         } else {
-            for (int i = len2; i < len1; i++) {
-                int num = Integer.parseInt(v1[i]);
-                if (num > 0) {
-                    return 1;
+            while (i < len2) {
+                if (Integer.parseInt(v2[i]) != 0) {
+                    return -1;
                 }
+                i++;
             }
             return 0;
         }
