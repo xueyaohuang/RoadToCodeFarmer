2b626d2f45aa8ba0718b36e6c567a493586ec11b
CodeToFLAG <hxyqbsmile@gmail.com>
2018-12-24 14:25:21 -0600

    Create 398. Random Pick Index.java

---
+++ 398. Random Pick Index.java
@@ -0,0 +1,57 @@
+//  O(N) memory, O(N) init, O(1) pick
+class Solution {
+    
+    Map<Integer, List<Integer>> map;
+    Random rand;
+
+    public Solution(int[] nums) {
+        map = new HashMap<>();
+        rand = new Random();
+        for (int i = 0; i < nums.length; i++) {
+            if (!map.containsKey(nums[i])) {
+                map.put(nums[i], new ArrayList<>());
+            }
+            map.get(nums[i]).add(i);
+        }
+    }
+    
+    public int pick(int target) {
+        List<Integer> list = map.get(target);
+        int size = list.size();
+        int idx = rand.nextInt(size);
+        return list.get(idx);
+    }
+}
+
+// O(N) memory, O(1) init, O(N) pick
+// reservoir sampling
+class Solution {
+    
+    int[] nums;
+    Random rand;
+
+    public Solution(int[] nums) {
+        this.nums = nums;
+        rand = new Random();
+    }
+    
+    public int pick(int target) {
+        int result = 0;
+        int count = 1;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != target) {
+                continue;
+            }
+            if (rand.nextInt(count++) == 0) {
+                result = i;
+            }
+        }
+        return result;
+    }
+}
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution obj = new Solution(nums);
+ * int param_1 = obj.pick(target);
+ */
