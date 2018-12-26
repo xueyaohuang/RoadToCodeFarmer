1d2d575740562ad2d0e0d9a188fc687f5e34d96e
CodeToFLAG <hxyqbsmile@gmail.com>
2018-12-26 03:13:02 -0600

    Update 75. Sort Colors.java

--- 75. Sort Colors.java
+++ 75. Sort Colors.java
@@ -53,3 +53,30 @@ class Solution {
         }
     }
 }
+
+// counting sort
+class Solution {
+    public void sortColors(int[] nums) {
+        int a = 0;
+        int b = 0;
+        int c = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 0) {
+                a++;
+            } else if (nums[i] == 1) {
+                b++;
+            } else {
+                c++;
+            }
+        }
+        for (int i = 0; i < a; i++) {
+            nums[i] = 0;
+        }
+        for (int i = a; i < a + b; i++) {
+            nums[i] = 1;
+        }
+        for (int i = a + b; i < nums.length; i++) {
+            nums[i] = 2;
+        }
+    }
+}
