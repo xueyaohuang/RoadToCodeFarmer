32cf2e2d8b9faac01f5abc2d76af79af6be04b1a
CodeToFLAG <hxyqbsmile@gmail.com>
2018-12-26 03:08:28 -0600

    Create 24. Swap Nodes in Pairs.java

---
+++ 24. Swap Nodes in Pairs.java
@@ -0,0 +1,19 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode swapPairs(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode node = head.next;
+        head.next = swapPairs(head.next.next);
+        node.next = head;
+        return node;
+    }
+}
