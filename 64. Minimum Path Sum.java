ef2be1e995ac756c75f0c77b1cf982e5eaf1f265
CodeToFLAG <hxyqbsmile@gmail.com>
2018-12-31 17:50:11 -0600

    Update 64. Minimum Path Sum.java

--- 64. Minimum Path Sum.java
+++ 64. Minimum Path Sum.java
@@ -1,5 +1,3 @@
-// O(m*n) extra space
-
 // no extra space. Modify grid.
 class Solution {
     public int minPathSum(int[][] grid) {
@@ -20,6 +18,31 @@ class Solution {
             }
         }
         return grid[row - 1][col - 1];
+    }
+}
+
+// O(m*n) extra space
+class Solution {
+    public int minPathSum(int[][] grid) {
+        if (grid == null || grid.length == 0) {
+            return 0;
+        }
+        int m = grid.length;
+        int n = grid[0].length;
+        int[][] res = new int[m][n];
+        res[0][0] = grid[0][0];
+        for (int i = 1; i < m; i++) {
+            res[i][0] = res[i - 1][0] + grid[i][0];
+        }
+        for (int j = 1; j < n; j++) {
+            res[0][j] = res[0][j - 1] + grid[0][j];
+        }
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                res[i][j] = grid[i][j] + Math.min(res[i][j - 1], res[i - 1][j]);
+            }
+        }
+        return res[m - 1][n - 1];
     }
 }
 
@@ -45,5 +68,3 @@ class Solution {
         return res[n - 1];
     }
 }
-
-
