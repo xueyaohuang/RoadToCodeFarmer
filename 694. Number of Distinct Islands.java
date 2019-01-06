882ee36e10c3130bb3f81ee336d60b3609f962f6
CodeToFLAG <hxyqbsmile@gmail.com>
2019-01-06 15:18:05 -0600

    Update 694. Number of Distinct Islands.java

--- 694. Number of Distinct Islands.java
+++ 694. Number of Distinct Islands.java
@@ -4,30 +4,32 @@ class Solution {
             return 0;
         }
         Set<String> set = new HashSet<>();
-        int m = grid.length;
-        int n = grid[0].length;
+        int m = grid.length, n = grid[0].length;
+        boolean[][] visited = new boolean[m][n];
+        
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                if (grid[i][j] == 1) {
+                if (grid[i][j] == 1 && !visited[i][j]) {
                     StringBuilder sb = new StringBuilder();
-                    encodeIsland(grid, i, j, sb, 's'); // s代表start
+                    encodeIsland(grid, i, j, sb, visited, 's');
                     set.add(sb.toString());
                 }
             }
         }
+        
         return set.size();
     }
     
-    private void encodeIsland(int[][] grid, int i, int j, StringBuilder sb, char direction) {
-        if (i < 0 || i >= grid.length || j < 0 || j >= grid[0].length || grid[i][j] == 0) {
+    private void encodeIsland(int[][] grid, int i, int j, StringBuilder sb, boolean[][] visited, char direction) {
+        if (i < 0 || i >= grid.length || j < 0 || j >= grid[0].length || grid[i][j] == 0 || visited[i][j]) {
             return;
         }
-        grid[i][j] = 0;
+        visited[i][j] = true;
         sb.append(direction);
-        encodeIsland(grid, i - 1, j, sb, 'u');
-        encodeIsland(grid, i + 1, j, sb, 'd');
-        encodeIsland(grid, i, j - 1, sb, 'l');
-        encodeIsland(grid, i, j + 1, sb, 'r');
+        encodeIsland(grid, i - 1, j, sb, visited, 'u');
+        encodeIsland(grid, i + 1, j, sb, visited, 'd');
+        encodeIsland(grid, i, j - 1, sb, visited, 'l');
+        encodeIsland(grid, i, j + 1, sb, visited, 'r');
         sb.append('e');
     }
 }
