95efb06887dffe9c6aced6f10417ef2553f0eaa0
CodeToFLAG <hxyqbsmile@gmail.com>
2019-01-06 16:06:14 -0600

    Update 171. Excel Sheet Column Number.java

--- 171. Excel Sheet Column Number.java
+++ 171. Excel Sheet Column Number.java
@@ -2,20 +2,6 @@
 class Solution {
     public int titleToNumber(String s) {
         if (s == null || s.length() == 0) {
-             return 0;
-        }
-        int len = s.length();
-        int num = 0;
-        for (int i = 0; i < len; i++) {
-            num += Math.pow(26, len - 1 - i) * (s.charAt(i) - 'A' + 1);
-        }
-        return num;
-    }
-}
-
-class Solution {
-    public int titleToNumber(String s) {
-        if (s == null || s.length() == 0) {
             return 0;
         }
         int res = 0;
