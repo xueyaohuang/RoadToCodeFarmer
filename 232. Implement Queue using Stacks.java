949473fd274a0292de482b4db4c74de6de3c98c0
CodeToFLAG <hxyqbsmile@gmail.com>
2019-01-07 18:46:49 -0600

    Update 232. Implement Queue using Stacks.java

--- 232. Implement Queue using Stacks.java
+++ 232. Implement Queue using Stacks.java
@@ -1,3 +1,13 @@
+
+/*
+The application for this implementation is to separate read & write of a queue in multi-processing.
+One of the stack is for read, and another is for write. They only interfere each other when the former
+one is full or latter is empty. When there's only one thread doing the read/write operation to the stack,
+there will always one stack empty. However, in a multi-thread application, if we have only one queue,
+for thread-safety, either read or write will lock the whole queue. In the two stack implementation, as
+long as the second stack is not empty, push operation will not lock the stack for pop.
+*/
+
 class MyQueue {
     
     Stack<Integer> sIn;
