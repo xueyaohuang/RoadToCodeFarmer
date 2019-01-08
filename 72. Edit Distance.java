52bd8363b1ac097bbe82c16a402a7fcd7868793a
CodeToFLAG <hxyqbsmile@gmail.com>
2019-01-07 18:05:59 -0600

    Update 72. Edit Distance.java

--- 72. Edit Distance.java
+++ 72. Edit Distance.java
@@ -4,31 +4,25 @@ class Solution {
             return 0;
         }
         if (word1 == null || word2 == null) {
-            return word2 == null ? word1.length() : word2.length();
+            return word1 == null ? word2.length() : word1.length();
         }
         int len1 = word1.length();
         int len2 = word2.length();
         int[][] dp = new int[len1 + 1][len2 + 1];
+        
         for (int i = 0; i <= len1; i++) {
             for (int j = 0; j <= len2; j++) {
-                if (i == 0 && j == 0) {
-                    dp[i][j] = 0;
-                } else if (i == 0) {
+                if (i == 0) {
                     dp[i][j] = j;
                 } else if (j == 0) {
                     dp[i][j] = i;
-                } else if (word1.charAt(i - 1) != word2.charAt(j - 1)) {
-                    dp[i][j] = Math.min(dp[i - 1][j - 1], Math.min(dp[i - 1][j], dp[i][j - 1])) + 1;
-                } else {
+                } else if (word1.charAt(i - 1) == word2.charAt(j - 1)) {
                     dp[i][j] = dp[i - 1][j - 1];
-                }       
+                } else {
+                    dp[i][j] = Math.min(dp[i - 1][j - 1], Math.min(dp[i - 1][j], dp[i][j - 1])) + 1;
+                }
             }
         }
         return dp[len1][len2];
     }
 }
-/*
-dp[i - 1][j - 1] + 1: replace
-dp[i - 1][j] + 1: delete
-dp[i][j - 1] + 1: insert
-*/
