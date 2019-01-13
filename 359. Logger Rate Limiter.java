6121e3b2bb234703f55534ffd1c392411a0ddbb7
CodeToFLAG <hxyqbsmile@gmail.com>
2019-01-13 17:52:01 -0600

    Update 359. Logger Rate Limiter.java

--- 359. Logger Rate Limiter.java
+++ 359. Logger Rate Limiter.java
@@ -35,6 +35,45 @@ class Logger {
     }
 }
 
+
+// map/set with experiation
+class Log {
+    int time;
+    String msg;
+    public Log(int time, String msg) {
+        this.time = time;
+        this.msg = msg;
+    }
+}
+
+class Logger {
+
+    Queue<Log> queue;
+    Set<String> set;
+    /** Initialize your data structure here. */
+    public Logger() {
+        queue = new LinkedList<>();
+        set = new HashSet<>();
+    }
+    
+    /** Returns true if the message should be printed in the given timestamp, otherwise returns false.
+        If this method returns false, the message will not be printed.
+        The timestamp is in seconds granularity. */
+    public boolean shouldPrintMessage(int timestamp, String message) {
+        while (!queue.isEmpty() && timestamp - 10 >= queue.peek().time) {
+            Log timeExceedLog = queue.poll();
+            set.remove(timeExceedLog.msg);
+        }
+        if (set.contains(message)) {
+            return false;
+        } else {
+            queue.offer(new Log(timestamp, message));
+            set.add(message);
+            return true;
+        }
+    }
+}
+
 /**
  * Your Logger object will be instantiated and called as such:
  * Logger obj = new Logger();
