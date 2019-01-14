457534bc98c564b7b4c656714a86280a967d1efb
CodeToFLAG <hxyqbsmile@gmail.com>
2019-01-14 01:50:36 -0600

    Update 7. Reverse Integer.java

--- 7. Reverse Integer.java
+++ 7. Reverse Integer.java
@@ -1,13 +1,14 @@
 class Solution {
     public int reverse(int x) {
-        long result = 0;
-        while (x != 0) {
-            result = result * 10 + x % 10;
-            x /= 10;
-            if (result > Integer.MAX_VALUE || result < Integer.MIN_VALUE) {
+        int n = x;
+        long res = 0;
+        while (n != 0) {
+            res = res * 10 + n % 10;
+            if (res >= Integer.MAX_VALUE || res <= Integer.MIN_VALUE) {
                 return 0;
             }
+            n /= 10;
         }
-        return (int)result;
+        return (int)res;
     }
 }
