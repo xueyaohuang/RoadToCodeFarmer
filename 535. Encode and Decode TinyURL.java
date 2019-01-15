94ecd1f017f1dff6214b25322a9cafc9dc6a3484
CodeToFLAG <hxyqbsmile@gmail.com>
2019-01-14 18:56:00 -0600

    Update 535. Encode and Decode TinyURL.java

--- 535. Encode and Decode TinyURL.java
+++ 535. Encode and Decode TinyURL.java
@@ -2,30 +2,35 @@ public class Codec {
     
     // key:shortUrl, value:longUrl
     private HashMap<String, String> map = new HashMap<>();
-    String dict = "0123456789qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM";
+    private String dict = "0123456789qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM";
+    private Random rand = new Random();
+    
     // Encodes a URL to a shortened URL.
-    public String encode(String longUrl) {      
-        Random random = new Random();
-        String url = "";
-        String shortUrl = "http://tinyurl.com/";
-        int dictLen = dict.length();
-        // It's possible that a randomly generated code has already been generated before. In that case, another random code is generated instead. Repeat until we have a code that's not already in use.
-        while (!map.containsValue(longUrl)) {
-            for (int i = 0; i < 6; i++) {
-                int idx = random.nextInt(dictLen);
-                url += dict.charAt(idx);
-            }
-            shortUrl = "http://tinyurl.com/" + url;
-            if (!map.containsKey(shortUrl)) {
-                map.put(shortUrl, longUrl);
-            }      
+    public String encode(String longUrl) {    
+        int len = dict.length();
+        String shortUrl = getEncodedUrl(len);
+        // It's possible that a randomly generated code has already been generated before. 
+        // In that case, another random code is generated instead. 
+        // Repeat until we have a code that's not already in use.
+        while (map.containsKey(shortUrl)) {
+            shortUrl = getEncodedUrl(len);
         }
+        map.put(shortUrl, longUrl);
         return shortUrl;
     }
 
     // Decodes a shortened URL to its original URL.
     public String decode(String shortUrl) {
         return map.get(shortUrl);
+    }
+    
+    private String getEncodedUrl(int len) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < 6; i++) {
+            int idx = rand.nextInt(len);
+            sb.append(dict.charAt(idx));
+        }
+        return "http://tinyurl.com/" + sb.toString();
     }
 }
 
