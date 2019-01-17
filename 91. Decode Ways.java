8cf61f3e4d35004083e89deb92e78ee7d5525d9c
CodeToFLAG <hxyqbsmile@gmail.com>
2019-01-17 03:42:21 -0600

    Update 91. Decode Ways.java

--- 91. Decode Ways.java
+++ 91. Decode Ways.java
@@ -6,7 +6,8 @@ class Solution {
         }
         int[] dp = new int[s.length() + 1];
         dp[0] = 1;
-        dp[1] = s.charAt(0) == '0' ? 0 : 1;
+        dp[1] = 1; // s是valid，第一位必定不是0
+        // dp[1] = s.charAt(0) == '0' ? 0 : 1;
         for (int i = 2; i <= s.length(); i++) {
             int a = Integer.valueOf(s.substring(i - 1, i));
             int b = Integer.valueOf(s.substring(i - 2, i));
