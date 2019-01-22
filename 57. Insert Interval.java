3a427519a85ade24c26433b452967e11574cad2c
CodeToFLAG <hxyqbsmile@gmail.com>
2019-01-22 02:03:11 -0600

    Update 57. Insert Interval.java

--- 57. Insert Interval.java
+++ 57. Insert Interval.java
@@ -10,15 +10,20 @@
 class Solution {
     public List<Interval> insert(List<Interval> intervals, Interval newInterval) {
         List<Interval> res = new ArrayList<>();
-        int start, end, i = 0;
-        while (i < intervals.size() && intervals.get(i).end < newInterval.start) res.add(intervals.get(i++));
-        if (i == intervals.size()) res.add(newInterval);
-        else {
-            start = Math.min(newInterval.start, intervals.get(i).start);
-            while (i < intervals.size() && intervals.get(i).start <= newInterval.end) i++;
-            end = i == 0 ? newInterval.end : Math.max(newInterval.end, intervals.get(i - 1).end);
-            res.add(new Interval(start, end));
-            while (i < intervals.size()) res.add(intervals.get(i++));
+        for (Interval itv : intervals) {
+            if (newInterval == null || itv.end < newInterval.start) {
+                res.add(itv);
+            } else if (itv.start > newInterval.end) {
+                res.add(newInterval);
+                res.add(itv);
+                newInterval = null;
+            } else {
+                newInterval.start = Math.min(itv.start, newInterval.start);
+                newInterval.end = Math.max(itv.end, newInterval.end);
+            }
+        }
+        if (newInterval != null) {
+            res.add(newInterval);
         }
         return res;
     }
