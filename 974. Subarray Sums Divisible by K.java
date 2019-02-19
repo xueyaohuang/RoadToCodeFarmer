4c490c2b91dd30b362ce72e2395eab2dd93eae92
CodeToFLAG <hxyqbsmile@gmail.com>
2019-02-18 22:54:07 -0800

    Update 974. Subarray Sums Divisible by K.java

--- 974. Subarray Sums Divisible by K.java
+++ 974. Subarray Sums Divisible by K.java
@@ -18,3 +18,28 @@ class Solution {
         return count;
     }
 }
+
+class Solution {
+    public int subarraysDivByK(int[] A, int K) {
+        if (A == null || A.length == 0) {
+            return 0;
+        }
+        int count = 0;
+        int sum = 0;
+        int[] mod = new int[K];
+        
+        for (int i = 0; i < A.length; i++) {
+            sum += A[i];
+            mod[(sum % K + K) % K]++;
+        }
+        
+        for (int i = 0; i < K; i++) {
+            if (mod[i] > 1) {
+                count += mod[i] * (mod[i] - 1) / 2;
+            }
+        }
+        // add the elements which are divisible by k itself 
+        count += mod[0]; 
+        return count;
+    }
+}
