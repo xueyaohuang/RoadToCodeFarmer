7401be0549ba05705c32f0b91367d54ebe589e2e
CodeToFLAG <hxyqbsmile@gmail.com>
2019-03-20 23:21:06 -0700

    Create 496. Next Greater Element I.java

---
+++ 496. Next Greater Element I.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        Map<Integer, Integer> map = new HashMap<>();
+        Deque<Integer> stack = new ArrayDeque<>();
+        for (int i = 0; i < nums2.length; i++) {
+            while (!stack.isEmpty() && nums2[i] > nums2[stack.peek()]) {
+                int idx = stack.pop();
+                map.put(nums2[idx], nums2[i]);
+            }
+            stack.push(i);
+        }
+        int[] res = new int[nums1.length];
+        for (int i = 0; i < nums1.length; i++) {
+            res[i] = map.getOrDefault(nums1[i], -1);
+        }
+        return res;
+    }
+}
