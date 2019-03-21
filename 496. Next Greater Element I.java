ee6e2711289b53f40643c1e636acf2f2228514c2
CodeToFLAG <hxyqbsmile@gmail.com>
2019-03-20 23:24:31 -0700

    Update 496. Next Greater Element I.java

--- 496. Next Greater Element I.java
+++ 496. Next Greater Element I.java
@@ -1,3 +1,4 @@
+// stack 存index
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
         Map<Integer, Integer> map = new HashMap<>();
@@ -8,6 +9,25 @@ class Solution {
                 map.put(nums2[idx], nums2[i]);
             }
             stack.push(i);
+        }
+        int[] res = new int[nums1.length];
+        for (int i = 0; i < nums1.length; i++) {
+            res[i] = map.getOrDefault(nums1[i], -1);
+        }
+        return res;
+    }
+}
+
+// stack也可以直接存数
+class Solution {
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        Map<Integer, Integer> map = new HashMap<>();
+        Deque<Integer> stack = new ArrayDeque<>();
+        for (int i = 0; i < nums2.length; i++) {
+            while (!stack.isEmpty() && nums2[i] > stack.peek()) {
+                map.put(stack.pop(), nums2[i]);
+            }
+            stack.push(nums2[i]);
         }
         int[] res = new int[nums1.length];
         for (int i = 0; i < nums1.length; i++) {
