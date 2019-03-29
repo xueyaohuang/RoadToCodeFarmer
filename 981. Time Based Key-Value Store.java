bb10ab016c683568335f9a51dc9355c3fc28fc42
CodeToFLAG <hxyqbsmile@gmail.com>
2019-03-28 23:36:53 -0700

    Create 981. Time Based Key-Value Store.java

---
+++ 981. Time Based Key-Value Store.java
@@ -0,0 +1,24 @@
+
+class TimeMap {
+    
+    Map<String, TreeMap<Integer, String>> map;
+
+    /** Initialize your data structure here. */
+    public TimeMap() {
+        map = new HashMap<>();
+    }
+    
+    public void set(String key, String value, int timestamp) {
+        map.putIfAbsent(key, new TreeMap<>());
+        map.get(key).put(timestamp, value);
+    }
+    
+    public String get(String key, int timestamp) {
+        if (!map.containsKey(key)) {
+            return "";
+        }
+        TreeMap<Integer, String> cur = map.get(key);
+        Integer ceilKey = cur.floorKey(timestamp);
+        return ceilKey == null ? "" : cur.get(ceilKey);
+    }
+}
