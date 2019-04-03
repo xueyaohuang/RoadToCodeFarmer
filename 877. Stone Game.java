1e473a4d11227e7ff114da93f13cfa5e0e661ec4
CodeToFLAG <hxyqbsmile@gmail.com>
2019-04-02 21:08:40 -0700

    Update 877. Stone Game.java

--- 877. Stone Game.java
+++ 877. Stone Game.java
@@ -8,3 +8,30 @@ class Solution {
     }
 }
 
+
+/*
+dp[i][j] means the biggest number of stones you can get more than opponent picking piles in piles[i] ~ piles[j].
+You can first pick piles[i] or piles[j].
+
+If you pick piles[i], your result will be piles[i] - dp[i + 1][j]
+If you pick piles[j], your result will be piles[j] - dp[i][j - 1]
+So we get:
+dp[i][j] = max(piles[i] - dp[i + 1][j], piles[j] - dp[i][j - 1])
+We start from smaller subarray and then we use that to calculate bigger subarray.
+*/
+class Solution {
+    public boolean stoneGame(int[] piles) {
+        int len = piles.length;
+        int[][] dp = new int[len][len];
+        for (int i = 0; i < len; i++) {
+            dp[i][i] = piles[i];
+        }
+        for (int l = 1; l < len; l++) { // 区间长度
+            for (int i = 0; i + l < len; i++) {
+                dp[i][i + l] = Math.max(piles[i] - dp[i + 1][i + l], piles[i + l] - dp[i][i + l - 1]);
+            }
+        }
+        return dp[0][len - 1] > 0;
+    }
+}
+
