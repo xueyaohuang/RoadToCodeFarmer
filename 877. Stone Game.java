8e5f69578f36aabd829e14e3f28149da8f2693e8
CodeToFLAG <hxyqbsmile@gmail.com>
2019-04-02 18:30:10 -0700

    Create 877. Stone Game.java

---
+++ 877. Stone Game.java
@@ -0,0 +1,10 @@
+// Since there are even numbers, and sum is odd. You may decide to pick either all the odd or all the even terms,
+// and the other person can only pick the other type of position. So if sum of all even terms is larger,
+// you start with last one, otherwise, you start with the first one.
+
+class Solution {
+    public boolean stoneGame(int[] piles) {
+        return true;
+    }
+}
+
