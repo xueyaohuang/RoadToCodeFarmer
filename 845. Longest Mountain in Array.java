c6512de6092a8ab115e29b70d900429580dd2fa9
CodeToFLAG <hxyqbsmile@gmail.com>
2019-04-04 22:29:27 -0700

    Update 845. Longest Mountain in Array.java

--- 845. Longest Mountain in Array.java
+++ 845. Longest Mountain in Array.java
@@ -26,3 +26,62 @@ class Solution {
         return j - i + 1;
     }
 }
+
+class Solution {
+    public int longestMountain(int[] A) {
+        if (A.length < 3) {
+            return 0;
+        }
+        int len = A.length;
+        int[] forward = new int[len];
+        int[] backward = new int[len];
+        for (int i = 1; i < len; i++) {
+            if (A[i] > A[i - 1]) {
+                forward[i] = forward[i - 1] + 1;
+            } else {
+                forward[i] = 0;
+            }
+        }
+        for (int i = len - 2; i >= 0; i--) {
+            if (A[i] > A[i + 1]) {
+                backward[i] = backward[i + 1] + 1;
+            } else {
+                backward[i] = 0;
+            }
+        }
+        int res = 0;
+        for (int i = 1; i < len - 1; i++) {
+            if (forward[i] == 0 || backward[i] == 0) {
+                continue;
+            }
+            if (forward[i] + backward[i] + 1 >= 3 && forward[i] + backward[i] + 1 > res) {
+                res = forward[i] + backward[i] + 1;
+            }
+        }
+        return res;
+    }
+}
+
+class Solution {
+    public int longestMountain(int[] A) {
+        int len = A.length;
+        int start = 0;
+        int end = 0;
+        int res = 0;
+        while (start < len - 2) {
+            while (start < len - 1 && A[start] >= A[start + 1]) {
+                start++;
+            }
+            end = start + 1;
+            while (end < len - 1 && A[end] < A[end + 1]) {
+                end++;
+            }
+            while (end < len - 1 && A[end] > A[end + 1]) {
+                end++;
+                res = Math.max(res, end - start + 1);
+            }
+            start = end;
+        }
+        return res;
+    }
+}
