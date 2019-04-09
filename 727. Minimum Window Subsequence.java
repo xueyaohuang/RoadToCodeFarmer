43dceb3b932bdf9a1270647a0820e9b6b9074fd2
CodeToFLAG <hxyqbsmile@gmail.com>
2019-04-08 21:00:39 -0700

    Update 727. Minimum Window Subsequence.java

--- 727. Minimum Window Subsequence.java
+++ 727. Minimum Window Subsequence.java
@@ -33,3 +33,40 @@ class Solution {
         return len == Integer.MAX_VALUE ? "" : S.substring(start, start + len);
     }
 }
+
+class Solution {
+    
+    public String minWindow(String S, String T) {
+        int m = T.length();
+        int n = S.length();
+        // M[i][j] = k means: Substring S[k~j] covers substring T[0~i] in the right order.
+        // That is to say, M[i][j] is the start index of the substring.
+        int[][] M = new int[m][n];
+        
+        for (int i = 0; i < m; i++) {
+            for (int j = i; j < n; j++) {
+                // if T[i] == S[j], S[k~j-1][j] covers T[0~i-1][i] => S[k~j-1] covers T[0~i-1]
+                // if T[i] != S[j], S[k~j-1][j] covers T[0~i] => S[k~j-1] covers T[0~i]
+                if (T.charAt(i) == S.charAt(j)) {
+                    M[i][j] = (i == 0 ? j : M[i - 1][j - 1]);
+                } else {
+                    M[i][j] = (j == i ? -1 : M[i][j - 1]); // Since 0 is a valid start, we need to mark invalid start as -1
+                }
+            }
+        }
+        
+        int min = n + 1;
+        int start = 0, end = -1;
+        for (int i = m - 1; i < n; i++) {
+            if (M[m - 1][i] == -1) {
+                continue;
+            }
+            if (i - M[m - 1][i] + 1 < min) {
+                start = M[m - 1][i];
+                end = i;
+                min = i - M[m - 1][i] + 1;
+            }
+        }
+        return S.substring(start, end + 1);
+    }
+}
