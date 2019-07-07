e4db40749157e2eac3059be9598b1494cccef2af
CodeToFLAG <hxyqbsmile@gmail.com>
2019-07-06 17:19:15 -0700

    Create 938. Range Sum of BST.go

---
+++ Go/938. Range Sum of BST.go
@@ -0,0 +1,20 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func rangeSumBST(root *TreeNode, L int, R int) int {
+    if root == nil {
+        return 0
+    }
+    if root.Val < L {
+        return rangeSumBST(root.Right, L, R)
+    }
+    if root.Val > R {
+        return rangeSumBST(root.Left, L, R)
+    }
+    return root.Val + rangeSumBST(root.Right, L, R) + rangeSumBST(root.Left, L, R)
+}
