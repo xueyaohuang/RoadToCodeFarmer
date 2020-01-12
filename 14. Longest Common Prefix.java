ef2302b4c61366c7cb062c4c54a396dd56008d11
CodeToFLAG <hxyqbsmile@gmail.com>
2020-01-11 21:45:09 -0800

    Update 14. Longest Common Prefix.java

--- 14. Longest Common Prefix.java
+++ 14. Longest Common Prefix.java
@@ -15,3 +15,21 @@ class Solution {
         return strs[0];
     }
 }
+
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        if (strs == null || strs.length == 0) {
+            return "";
+        }
+        String s = strs[0];
+        for (int i = 1; i < strs.length; i++) {
+            while (strs[i].indexOf(s) != 0) {
+                s = s.substring(0, s.length() - 1);
+            }
+            if (s.equals("")) {
+                return "";
+            }
+        }
+        return s;
+    }
+}
