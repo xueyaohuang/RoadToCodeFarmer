33ac1ef23256e748cd7d5c6aa8593d03fb0df8d6
CodeToFLAG <hxyqbsmile@gmail.com>
2020-04-05 00:11:34 -0700

    Update 392. Is Subsequence.java

--- 392. Is Subsequence.java
+++ 392. Is Subsequence.java
@@ -24,3 +24,40 @@ class Solution {
 }
 
 // follow up question
+	class Solution {
+    public boolean isSubsequence(String s, String t) {
+        Map<Character, List<Integer>> map = new HashMap<>();
+        for (int i = 0; i < t.length(); i++) {
+            char c = t.charAt(i);
+            if (!map.containsKey(c)) {
+                map.put(c, new ArrayList<>());
+            }
+            map.get(c).add(i);
+        }
+        int cur = -1;
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (!map.containsKey(c)) {
+                return false;
+            }
+            cur = searchIndex(cur, map.get(c));
+            if (cur == -1) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private int searchIndex(int cur, List<Integer> list) {
+        int start = 0, end = list.size() - 1;
+        while (start < end) {
+            int mid = start + (end - start) / 2;
+            if (list.get(mid) <= cur) {
+                start = mid + 1;
+            } else {
+                end = mid;
+            }
+        }
+        return list.get(start) > cur ? list.get(start) : -1;
+    }
+}
