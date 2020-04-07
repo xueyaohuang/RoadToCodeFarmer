1c1176140cd5bc09a3f9d40f886dd19492646ed2
CodeToFLAG <hxyqbsmile@gmail.com>
2020-04-07 16:49:29 -0700

    Update 49. Group Anagrams.java

--- 49. Group Anagrams.java
+++ 49. Group Anagrams.java
@@ -48,3 +48,40 @@ class Solution {
         return new ArrayList<>(map.values());
     }
 }
+
+//
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+         if (strs == null || strs.length == 0) {
+            return new ArrayList<>();
+        }
+        Map<String, List<String>> map = new HashMap<>();
+        for (String s : strs) {
+            String flag = createFlag1(s);
+            // String flag = createFlag2(s);
+            map.putIfAbsent(flag, new ArrayList<>());
+            map.get(flag).add(s);
+        }
+        return new ArrayList<>(map.values());
+    }
+    
+    private String createFlag1(String s) {
+        char[] ca = s.toCharArray();
+        Arrays.sort(ca);
+        return new String(ca);
+    }
+    
+    private String createFlag2(String s) {
+        int[] map = new int[26];
+        StringBuilder sb = new StringBuilder();
+        for (char c : s.toCharArray()) {
+            map[c - 'a']++;
+        }
+        for (int i = 0; i < map.length; i++) {
+            if (map[i] != 0) {
+                sb.append(map[i]).append((char)('a' + i));
+            }
+        }
+        return sb.toString();
+    }
+}
