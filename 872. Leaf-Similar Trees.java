a32f53b001d0a0dcd6520c794a9ed788164db57c
CodeToFLAG <hxyqbsmile@gmail.com>
2020-04-09 16:47:00 -0700

    Update 872. Leaf-Similar Trees.java

--- 872. Leaf-Similar Trees.java
+++ 872. Leaf-Similar Trees.java
@@ -16,7 +16,7 @@ class Solution {
         if (l1.size() != l2.size()) {
             return false;
         }
-        for (int i = 0; i< l1.size(); i++) {
+        for (int i = 0; i < l1.size(); i++) {
             if (l1.get(i) != l2.get(i)) {
                 return false;
             }
