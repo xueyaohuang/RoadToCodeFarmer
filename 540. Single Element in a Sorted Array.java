ec7605f21738dfe8dd2e2f088ece8b5a9088f6d4
CodeToFLAG <hxyqbsmile@gmail.com>
2020-04-16 21:49:49 -0700

    Update 540. Single Element in a Sorted Array.java

--- 540. Single Element in a Sorted Array.java
+++ 540. Single Element in a Sorted Array.java
@@ -4,6 +4,30 @@ class Solution {
         int end = nums.length - 1;
         while (start < end) {
             int mid = start + (end - start) / 2;
+            if (mid % 2 == 0) {
+                if (nums[mid] < nums[mid + 1]) {
+                    end = mid;
+                } else {
+                    start = mid + 1;
+                }
+            } else {
+                if (nums[mid] < nums[mid + 1]) {
+                    start = mid + 1;
+                } else {
+                    end = mid;
+                }
+            }
+        }
+        return nums[start];
+    }
+}
+
+class Solution {
+    public int singleNonDuplicate(int[] nums) {
+        int start = 0;
+        int end = nums.length - 1;
+        while (start < end) {
+            int mid = start + (end - start) / 2;
             if (nums[mid] != nums[mid + 1] && nums[mid] != nums[mid - 1]) {
                 return mid;
             }
