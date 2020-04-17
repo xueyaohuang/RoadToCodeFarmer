917b074a0daf3193494fa31a4ae72b0d90e38c16
CodeToFLAG <hxyqbsmile@gmail.com>
2020-04-16 23:10:08 -0700

    Update 637. Average of Levels in Binary Tree.java

--- 637. Average of Levels in Binary Tree.java
+++ 637. Average of Levels in Binary Tree.java
@@ -31,3 +31,32 @@ class Solution {
         return 1 + Math.max(getHeight(root.left, curHeight), getHeight(root.right, curHeight));
     }
 }
+
+class Solution {
+    public List<Double> averageOfLevels(TreeNode root) {
+        if (root == null) {
+            return new ArrayList<Double>();
+        }
+        List<Double> res = new ArrayList<>();
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            long sum = 0;
+            int count = 0;
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+                if (node.left != null) {
+                    queue.offer(node.left);
+                }
+                if (node.right != null) {
+                    queue.offer(node.right);
+                }
+                sum += node.val;
+                count++;
+            }
+            res.add((double)sum / count);
+        }
+        return res;
+    }
+}
