90894cc561c5642880564dc257b502013ba5ddc3
CodeToFLAG <hxyqbsmile@gmail.com>
2020-04-18 16:49:41 -0700

    Update 198. House Robber.java

--- 198. House Robber.java
+++ 198. House Robber.java
@@ -14,25 +14,6 @@ class Solution {
     }
 }
 
-class Solution {
-    public int rob(int[] nums) {
-        if (nums == null || nums.length == 0) {
-            return 0;
-        }
-        int rob = 0;
-        int notRob = 0;
-        int res = 0;
-        for (int i = 0; i < nums.length; i++) {
-            int preRob = rob;
-            // rob = Math.max(preRob, notRob + nums[i]); //if rob current value, previous house must not be robbed
-            rob = notRob + nums[i];
-            notRob = Math.max(preRob, notRob); //if not rob ith house, take the max value of robbed (i-1)th house and not rob (i-1)th house
-            res = Math.max(rob, notRob);
-        }
-        return res;
-    }
-}
-
 
 // 变种：给定一个int array，元素有正有负。求一个sum最大的subsequence，保证每次跳过的元素最多为一个。
 // 注意是subsequence，并且每次最多跳过一个元素，所以subsequence长度最少是len/2-1
