73d0ca420327895d0479a301a04ae988a55fdd2b
CodeToFLAG <hxyqbsmile@gmail.com>
2020-04-19 16:43:15 -0700

    Update 337. House Robber III.java

--- 337. House Robber III.java
+++ 337. House Robber III.java
@@ -56,13 +56,14 @@ class Solution {
         if (root == null) {
             return 0;
         }
-        int val = 0;
+        int robRoot = root.val;
         if (root.left != null) {
-            val += rob(root.left.left) + rob(root.left.right);
+            robRoot += rob(root.left.left) + rob(root.left.right);
         }
         if (root.right != null) {
-            val += rob(root.right.left) + rob(root.right.right);
+            robRoot += rob(root.right.left) + rob(root.right.right);
         }
-        return Math.max(val + root.val, rob(root.left) + rob(root.right));
+        // max of rob root vs not rob root
+        return Math.max(robRoot, rob(root.left) + rob(root.right));
     }
 }
