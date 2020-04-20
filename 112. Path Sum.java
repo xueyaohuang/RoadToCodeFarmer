820e6178e486d2610633a47ce4061d2764497caa
CodeToFLAG <hxyqbsmile@gmail.com>
2020-04-19 20:38:38 -0700

    Update 112. Path Sum.java

--- 112. Path Sum.java
+++ 112. Path Sum.java
@@ -12,3 +12,28 @@ class Solution {
         return hasPathSum(root.left, sum - root.val) || hasPathSum(root.right, sum - root.val);
     }
 }
+
+class Solution {
+    boolean hasSum = false;
+    public boolean hasPathSum(TreeNode root, int sum) {
+        if (root == null) {
+            return false;
+        }
+        dfs(root, sum, 0);
+        return hasSum;
+    }
+    
+    private void dfs(TreeNode root, int sum, int cur) {
+        if (root == null) {
+            return;
+        }
+        cur += root.val;
+        if (root.left == null && root.right == null) {
+            if (cur == sum) {
+                hasSum = true;
+            }
+        }
+        dfs(root.left, sum, cur);
+        dfs(root.right, sum, cur);
+    }
+}
