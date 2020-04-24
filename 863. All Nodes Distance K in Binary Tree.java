a6d4857431ee3f0567d922384ce4e8c75ee81342
CodeToFLAG <hxyqbsmile@gmail.com>
2020-04-23 20:13:26 -0700

    Update 863. All Nodes Distance K in Binary Tree.java

--- 863. All Nodes Distance K in Binary Tree.java
+++ 863. All Nodes Distance K in Binary Tree.java
@@ -1,30 +1,3 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode(int x) { val = x; }
- * }
- */
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode(int x) { val = x; }
- * }
- */
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode(int x) { val = x; }
- * }
- */
 class Solution {
     public List<Integer> distanceK(TreeNode root, TreeNode target, int K) {
         if (root == null) {
