41b361cccde30329c2a25d52b9776962479aaeb0
CodeToFLAG <hxyqbsmile@gmail.com>
2020-04-25 16:26:49 -0700

    Update 413. Arithmetic Slices.java

--- 413. Arithmetic Slices.java
+++ 413. Arithmetic Slices.java
@@ -3,16 +3,17 @@ class Solution {
         if (A == null || A.length < 3) {
             return 0;
         }
-        int sum = 0;
         int cur = 0;
+        int res = 0;
         for (int i = 2; i < A.length; i++) {
             if (2 * A[i - 1] == A[i] + A[i - 2]) {
+                // cur代表以A[i]结尾的等差数列的个数
                 cur++;
-                sum += cur;
+                res += cur;
             } else {
                 cur = 0;
             }
         }
-        return sum;
+        return res;
     }
 }
