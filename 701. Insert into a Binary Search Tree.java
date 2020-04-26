f968a3ad9989526bfe363b40f8f1cb2b82021830
CodeToFLAG <hxyqbsmile@gmail.com>
2020-04-25 20:46:12 -0700

    Update 701. Insert into a Binary Search Tree.java

--- 701. Insert into a Binary Search Tree.java
+++ 701. Insert into a Binary Search Tree.java
@@ -48,3 +48,27 @@ class Solution {
         return root;
     }
 }
+
+class Solution {
+    public TreeNode insertIntoBST(TreeNode root, int val) {
+        if (root == null) {
+            return new TreeNode(val);
+        }
+        TreeNode cur = root;
+        TreeNode prev = null;
+        while (cur != null) {
+            prev = cur;
+            if (cur.val > val) {
+                cur = cur.left;
+            } else {
+                cur = cur.right;
+            }
+        }
+        if (prev.val > val) {
+            prev.left = new TreeNode(val);
+        } else {
+            prev.right = new TreeNode(val);
+        }
+        return root;
+    }
+}
