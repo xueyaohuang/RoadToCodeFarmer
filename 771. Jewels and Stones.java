4884efda7e2fcbf035549f36230302aaf09a2b03
CodeToFLAG <hxyqbsmile@gmail.com>
2020-05-03 16:08:58 -0700

    Update 771. Jewels and Stones.java

--- 771. Jewels and Stones.java
+++ 771. Jewels and Stones.java
@@ -1,6 +1,22 @@
 class Solution {
     public int numJewelsInStones(String J, String S) {
         int count = 0;
+        Set<Character> set = new HashSet<>();
+        for (char c : J.toCharArray()) {
+            set.add(c);
+        }
+        for (char c : S.toCharArray()) {
+            if (set.contains(c)) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
+
+class Solution {
+    public int numJewelsInStones(String J, String S) {
+        int count = 0;
         for (char c : S.toCharArray()) {
             if (J.indexOf(c) != -1) {
                 count++;
