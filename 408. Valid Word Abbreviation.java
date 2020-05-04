e04000a01f5d9957e2dfe58a9b9cb8cb70bdbb82
CodeToFLAG <hxyqbsmile@gmail.com>
2020-05-03 23:13:24 -0700

    Update 408. Valid Word Abbreviation.java

--- 408. Valid Word Abbreviation.java
+++ 408. Valid Word Abbreviation.java
@@ -23,3 +23,47 @@ class Solution {
         return idx1 == word.length() && idx2 == abbr.length();
     }
 }
+public class Solution {
+    /**
+     * @param word: a non-empty string
+     * @param abbr: an abbreviation
+     * @return: true if string matches with the given abbr or false
+     */
+    public boolean validWordAbbreviation(String word, String abbr) {
+        // write your code here
+        if (abbr == null) {
+            return false;
+        }
+        int idx = 0;
+        int count = 0;
+        int len1 = word.length();
+        int len2 = abbr.length();
+        for (int i = 0; i < len2;) {
+            if (Character.isDigit(abbr.charAt(i))) {
+                if (abbr.charAt(i) == '0') {
+                    return false;
+                }
+                count = 0;
+                while (i < len2 && Character.isDigit(abbr.charAt(i))) {
+                    count *= 10;
+                    count += abbr.charAt(i) - '0';
+                    i++;
+                }
+                idx += count;
+                if (idx > len1) {
+                    return false;
+                }
+            } else {
+                if (abbr.charAt(i) != word.charAt(idx)) {
+                    return false;
+                }
+                idx++;
+                i++;
+                if (idx > len1) {
+                    return false;
+                }
+            }
+        }
+        return idx == len1;
+    }
+}
