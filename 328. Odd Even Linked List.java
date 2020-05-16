919a44e9ce3587bd1589cc44daee44f77983d595
CodeToFLAG <hxyqbsmile@gmail.com>
2020-05-16 15:54:38 -0700

    Update 328. Odd Even Linked List.java

--- 328. Odd Even Linked List.java
+++ 328. Odd Even Linked List.java
@@ -3,24 +3,27 @@
  * public class ListNode {
  *     int val;
  *     ListNode next;
- *     ListNode(int x) { val = x; }
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
 class Solution {
     public ListNode oddEvenList(ListNode head) {
-        if (head == null || head.next == null || head.next.next == null) {
+        if (head == null || head.next == null) {
             return head;
         }
-        ListNode even = head;
-        ListNode odd = head.next;
-        ListNode oddNode = odd;
-        while (odd != null && odd.next != null) {
-            even.next = even.next.next;
+        ListNode odd = head;
+        ListNode even = head.next;
+        ListNode evenHead = even;
+        while (even != null && even.next != null) {
             odd.next = odd.next.next;
+            even.next = even.next.next;
+            odd = odd.next;
             even = even.next;
-            odd = odd.next;  
         }
-        even.next = oddNode;
+        // 把even接到odd后面
+        odd.next = evenHead;
         return head;
     }
 }
