1798a3d3b87598e6070010809511f5cd618f281d
CodeToFLAG <hxyqbsmile@gmail.com>
2020-05-16 15:57:44 -0700

    Update 326. Power of Three.java

--- 326. Power of Three.java
+++ 326. Power of Three.java
@@ -6,3 +6,18 @@ class Solution {
         return 1162261467 % n == 0;
     }
 }
+
+class Solution {
+    public boolean isPowerOfThree(int n) {
+        if (n <= 0) {
+            return false;
+        }
+        while (n > 1) {
+            if (n % 3 != 0) {
+                return false;
+            }
+            n /= 3;
+        }
+        return true;
+    }
+}
