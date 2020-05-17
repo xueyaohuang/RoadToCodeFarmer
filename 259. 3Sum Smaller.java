857121d5fcfb113c6166aa75e6c1906d61b39689
CodeToFLAG <hxyqbsmile@gmail.com>
2020-05-16 21:15:18 -0700

    Update 259. 3Sum Smaller.java

--- 259. 3Sum Smaller.java
+++ 259. 3Sum Smaller.java
@@ -1,11 +1,11 @@
 class Solution {
     public int threeSumSmaller(int[] nums, int target) {
-        if (nums == null || nums.length == 0) {
+        if (nums == null || nums.length < 3) {
             return 0;
         }
         int count = 0;
         Arrays.sort(nums);
-        for (int i = 0; i + 2 < nums.length; i++) {
+        for (int i = 0; i < nums.length - 2; i++) {
             int temp = target - nums[i];
             int j = i + 1;
             int k = nums.length - 1;
@@ -13,8 +13,7 @@ class Solution {
                 if (nums[j] + nums[k] < temp) {
                     count += k - j;
                     j++;
-                }
-                else {
+                } else {
                     k--;
                 }
             }
