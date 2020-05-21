1a223e2857f7d43db403423cd402f94b92f6530d
CodeToFLAG <hxyqbsmile@gmail.com>
2020-05-20 21:53:58 -0700

    Update 228. Summary Ranges.java

--- 228. Summary Ranges.java
+++ 228. Summary Ranges.java
@@ -15,11 +15,43 @@ class Solution {
                 start = i + 1;
             }
             if (i == nums.length - 1) {
-                if (start == nums.length - 1) {
+                if (start == i) {
                     res.add(String.valueOf(nums[start]));
                 } else {
                     res.add(nums[start] + "->" + nums[nums.length - 1]);
                 }
+            }
+        }
+        return res;
+    }
+}
+
+class Solution {
+    public List<String> summaryRanges(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return new ArrayList<>();
+        }
+        List<String> res = new ArrayList<>();
+        int len = nums.length;
+        int start = 0;
+        int end = 1;
+        while (end < len) {
+            while (end < len && nums[end - 1] + 1 == nums[end]) {
+                end++;
+            }
+            if (start == end - 1) {
+                res.add(String.valueOf(nums[start]));
+            } else {
+                res.add(nums[start] + "->" + nums[end - 1]);
+            }
+            start = end;
+            end++;
+        }
+        if (start < len) {
+            if (start == end - 1) {
+                res.add(String.valueOf(nums[start]));
+            } else {
+                res.add(nums[start] + "->" + nums[end - 1]);
             }
         }
         return res;
