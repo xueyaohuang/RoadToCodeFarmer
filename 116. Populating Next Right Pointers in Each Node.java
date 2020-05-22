2c7055b724f676bad1011210f6536996760af7cc
CodeToFLAG <hxyqbsmile@gmail.com>
2020-05-21 18:05:53 -0700

    Update 116. Populating Next Right Pointers in Each Node.java

--- 116. Populating Next Right Pointers in Each Node.java
+++ 116. Populating Next Right Pointers in Each Node.java
@@ -43,3 +43,32 @@ public class Solution {
         connect(root.right);
     }
 }
+
+class Solution {
+    public Node connect(Node root) {
+        if (root == null) {
+            return root;
+        }
+        Queue<Node> queue = new LinkedList<>();
+        queue.offer(root);
+        Node prev = null;
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                Node cur = queue.poll();
+                if (prev != null) {
+                    prev.next = cur;
+                }
+                prev = cur;
+                if (cur.left != null) {
+                    queue.offer(cur.left);
+                }
+                if (cur.right != null) {
+                    queue.offer(cur.right);
+                }
+            }
+            prev = null;
+        }
+        return root;
+    }
+}
