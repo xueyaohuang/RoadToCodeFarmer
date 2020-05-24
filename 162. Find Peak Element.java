44b32561ef7d8f4050cd4466f64564011711e5df
CodeToFLAG <hxyqbsmile@gmail.com>
2020-05-23 23:52:04 -0700

    Update 162. Find Peak Element.java

--- 162. Find Peak Element.java
+++ 162. Find Peak Element.java
@@ -1,25 +1,3 @@
-class Solution {
-    public int findPeakElement(int[] nums) {
-        int start = 0;
-        int end = nums.length - 1;
-        while (start + 1 < end) {
-            int mid = start + (end - start) / 2;
-            if (nums[mid] > nums[mid - 1]) {
-                start = mid;
-            }
-            else {
-                end = mid;
-            }
-        }
-        if (nums[start] > nums[end]) {
-            return start;
-        }
-        else {
-            return end;
-        }
-    }
-}
-
 // 跟mid+1比
 class Solution {
     public int findPeakElement(int[] nums) {
