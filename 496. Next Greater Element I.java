853b09607fbf7544fce32ff76413827736ce4f60
CodeToFLAG <hxyqbsmile@gmail.com>
2020-05-24 17:27:15 -0700

    Update 496. Next Greater Element I.java

--- 496. Next Greater Element I.java
+++ 496. Next Greater Element I.java
@@ -1,3 +1,29 @@
+// stack存数
+// Key observation:
+// Suppose we have a decreasing sequence followed by a greater number
+// For example [5, 4, 3, 2, 1, 6] then the greater number 6 is the next greater element for all previous numbers in the sequence
+
+// We use a stack to keep a decreasing sub-sequence, whenever we see a number x greater than stack.peek() we pop all elements less than x and for all the popped ones, their next greater element is x
+// For example [9, 8, 7, 3, 2, 1, 6]
+// The stack will first contain [9, 8, 7, 3, 2, 1] and then we see 6 which is greater than 1 so we pop 1 2 3 whose next greater element should be 6
+class Solution {
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        Map<Integer, Integer> map = new HashMap<>();
+        Deque<Integer> stack = new ArrayDeque<>();
+        for (int i = 0; i < nums2.length; i++) {
+            while (!stack.isEmpty() && nums2[i] > stack.peek()) {
+                map.put(stack.pop(), nums2[i]);
+            }
+            stack.push(nums2[i]);
+        }
+        int[] res = new int[nums1.length];
+        for (int i = 0; i < nums1.length; i++) {
+            res[i] = map.getOrDefault(nums1[i], -1);
+        }
+        return res;
+    }
+}
+
 // stack 存index
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
@@ -9,25 +35,6 @@ class Solution {
                 map.put(nums2[idx], nums2[i]);
             }
             stack.push(i);
-        }
-        int[] res = new int[nums1.length];
-        for (int i = 0; i < nums1.length; i++) {
-            res[i] = map.getOrDefault(nums1[i], -1);
-        }
-        return res;
-    }
-}
-
-// stack也可以直接存数
-class Solution {
-    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-        Map<Integer, Integer> map = new HashMap<>();
-        Deque<Integer> stack = new ArrayDeque<>();
-        for (int i = 0; i < nums2.length; i++) {
-            while (!stack.isEmpty() && nums2[i] > stack.peek()) {
-                map.put(stack.pop(), nums2[i]);
-            }
-            stack.push(nums2[i]);
         }
         int[] res = new int[nums1.length];
         for (int i = 0; i < nums1.length; i++) {
