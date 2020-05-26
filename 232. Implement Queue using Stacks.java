d7393bfc42cc9d49880ba569d8a8b7113831cd01
CodeToFLAG <hxyqbsmile@gmail.com>
2020-05-25 20:58:43 -0700

    Update 232. Implement Queue using Stacks.java

--- 232. Implement Queue using Stacks.java
+++ 232. Implement Queue using Stacks.java
@@ -51,6 +51,44 @@ class MyQueue {
     }
 }
 
+class MyQueue {
+    
+    Deque<Integer> stack1;
+    Deque<Integer> stack2;
+
+    /** Initialize your data structure here. */
+    public MyQueue() {
+        stack1 = new ArrayDeque<>();
+        stack2 = new ArrayDeque<>();
+    }
+    
+    /** Push element x to the back of queue. */
+    public void push(int x) {
+        while (!stack2.isEmpty()) {
+            stack1.push(stack2.pop());
+        }
+        stack1.push(x);
+        while (!stack1.isEmpty()) {
+            stack2.push(stack1.pop());
+        }
+    }
+    
+    /** Removes the element from in front of queue and returns that element. */
+    public int pop() {
+        return stack2.pop();
+    }
+    
+    /** Get the front element. */
+    public int peek() {
+        return stack2.peek();
+    }
+    
+    /** Returns whether the queue is empty. */
+    public boolean empty() {
+        return stack2.isEmpty();
+    }
+}
+
 /**
  * Your MyQueue object will be instantiated and called as such:
  * MyQueue obj = new MyQueue();
