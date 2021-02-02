d31b078c0a5b1f86c159ca047e34cf077c8b7082
CodeToFLAG <hxyqbsmile@gmail.com>
2021-02-01 21:23:07 -0800

    Update 13. Roman to Integer.java

--- 13. Roman to Integer.java
+++ 13. Roman to Integer.java
@@ -1,5 +1,30 @@
 class Solution {
     public int romanToInt(String s) {
+        Map<Character, Integer> map = new HashMap<>();
+        map.put('I', 1);
+        map.put('V', 5);
+        map.put('X', 10);
+        map.put('L', 50);
+        map.put('C', 100);
+        map.put('D', 500);
+        map.put('M', 1000);
+        int res = 0;
+        for (int i = 0; i < s.length() - 1; i++) {
+            int cur = map.get(s.charAt(i));
+            int next = map.get(s.charAt(i + 1));
+            if (cur >= next) {
+                res += cur;
+            } else {
+                res -= cur;
+            }
+        }
+        res += map.get(s.charAt(s.length() - 1));
+        return res;
+    }
+}
+
+class Solution {
+    public int romanToInt(String s) {
         int[] nums = new int[s.length()];
         int sum = 0;
         for (int i = 0; i < s.length(); i++) {
