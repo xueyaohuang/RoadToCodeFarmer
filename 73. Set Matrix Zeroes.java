17c596c7bcac11fe9cc1195373110f51266b85a4
CodeToFLAG <hxyqbsmile@gmail.com>
2021-04-06 23:46:23 -0700

    Update 73. Set Matrix Zeroes.java

--- 73. Set Matrix Zeroes.java
+++ 73. Set Matrix Zeroes.java
@@ -41,3 +41,28 @@ class Solution {
         }
     }
 }
+
+class Solution {
+    public void setZeroes(int[][] matrix) {
+        List<Integer> row = new ArrayList<>();
+        List<Integer> col = new ArrayList<>();
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                if (matrix[i][j] == 0) {
+                    row.add(i);
+                    col.add(j);
+                }
+            }
+        }
+        for (Integer i : row) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                matrix[i][j] = 0;
+            }
+        }
+        for (Integer j : col) {
+            for (int i = 0; i < matrix.length; i++) {
+                matrix[i][j] = 0;
+            }
+        }
+    }
+}
