5994b54e9e0f811ede108e7e96d3da6b43fcf4e7
CodeToFLAG <hxyqbsmile@gmail.com>
2021-04-07 00:02:58 -0700

    Update 73. Set Matrix Zeroes.java

--- 73. Set Matrix Zeroes.java
+++ 73. Set Matrix Zeroes.java
@@ -22,16 +22,16 @@ class Solution {
                 }
             }
         }
-        for (int i = 1; i < matrix[0].length; i++) {
-            if (matrix[0][i] == 0) {
-                for (int j = 1; j < matrix.length; j++) {
-                    matrix[j][i] = 0;
+        for (int j = 1; j < matrix[0].length; j++) {
+            if (matrix[0][j] == 0) {
+                for (int i = 1; i < matrix.length; i++) {
+                    matrix[i][j] = 0;
                 }
             }
         }
         if (rowZero) {
-            for (int i = 0; i < matrix[0].length; i++) {
-                matrix[0][i] = 0;
+            for (int j = 0; j < matrix[0].length; j++) {
+                matrix[0][j] = 0;
             }
         }
         if (colZero) {
