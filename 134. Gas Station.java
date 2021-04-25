528e98b86e916f8d711830c7200dea953afb6747
CodeToFLAG <hxyqbsmile@gmail.com>
2021-04-24 17:08:54 -0700

    Update 134. Gas Station.java

--- 134. Gas Station.java
+++ 134. Gas Station.java
@@ -8,6 +8,28 @@
 
 class Solution {
     public int canCompleteCircuit(int[] gas, int[] cost) {
+        int total = 0;
+        for (int i = 0; i < gas.length; i++) {
+            total += gas[i] - cost[i];
+        }
+        if (total < 0) {
+            return -1;
+        }
+        int start = 0;
+        total = 0;
+        for (int i = 0; i < gas.length; i++) {
+            total += gas[i] - cost[i];
+            if (total < 0) {
+                start = i + 1;
+                total = 0;
+            }
+        }
+        return start;
+    }
+}
+
+class Solution {
+    public int canCompleteCircuit(int[] gas, int[] cost) {
         int tank = 0;
         int total = 0;
         int start = 0;
