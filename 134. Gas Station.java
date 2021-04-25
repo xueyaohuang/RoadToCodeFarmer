92625999a707d6457b912d7c8f176b9c3683c876
CodeToFLAG <hxyqbsmile@gmail.com>
2021-04-24 17:01:26 -0700

    Update 134. Gas Station.java

--- 134. Gas Station.java
+++ 134. Gas Station.java
@@ -2,6 +2,10 @@
 // 2. 所以更新tank为tank += gas[i] - cost[i]，每次在stationi检查到tank<0, station i+ 1就可能是那个唯一解，所以更新start到i + 1。
 // 如果总的gas比总的cost大，一定有解。反之一定无解。检查 rest + tank 是否比0大，大于零一定有解，就是最后更新的那个start。小于0一定无解。
 
+//If car starts at A and can not reach B. Any station between A and B
+// can not reach B.(B is the first station that A can not reach.)
+// If the total number of gas is bigger than the total number of cost. There must be a solution.
+
 class Solution {
     public int canCompleteCircuit(int[] gas, int[] cost) {
         int tank = 0;
