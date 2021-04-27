56ea0f2537292c369f4693acb18b4a3a0a183b9c
CodeToFLAG <hxyqbsmile@gmail.com>
2021-04-26 19:07:45 -0700

    Update 202. Happy Number.java

--- 202. Happy Number.java
+++ 202. Happy Number.java
@@ -2,6 +2,26 @@ class Solution {
     public boolean isHappy(int n) {
         Set<Integer> set = new HashSet<>();
         while (set.add(n)) {
+            n = getSquareSum(n);
+        }
+        return n == 1;
+    }
+    
+    private int getSquareSum(int n) {
+        int sum = 0;
+        while (n > 0) {
+            int cur = n % 10;
+            sum += cur * cur;
+            n /= 10;
+        }
+        return sum;
+    }
+}
+
+class Solution {
+    public boolean isHappy(int n) {
+        Set<Integer> set = new HashSet<>();
+        while (set.add(n)) {
             if (sumSquNum(n) == 1) {
                 return true;
             }
