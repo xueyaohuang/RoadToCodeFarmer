1482eb0ff2f68587cffe8fedacf9a11dcab0e510
CodeToFLAG <hxyqbsmile@gmail.com>
2021-04-27 00:49:50 -0700

    Update 454. 4Sum II.java

--- 454. 4Sum II.java
+++ 454. 4Sum II.java
@@ -1,17 +1,21 @@
 class Solution {
-    public int fourSumCount(int[] A, int[] B, int[] C, int[] D) {
+    public int fourSumCount(int[] nums1, int[] nums2, int[] nums3, int[] nums4) {
         Map<Integer, Integer> map = new HashMap<>();
-        for (int i = 0; i < A.length; i++) {
-            for (int j = 0; j < B.length; j++) {
-                map.put(A[i] + B[j], map.getOrDefault(A[i] + B[j], 0) + 1);
+        int count = 0;
+        for (int i = 0; i < nums1.length; i++) {
+            for (int j = 0; j < nums2.length; j++) {
+                int sum = nums1[i] + nums2[j];
+                map.put(sum, map.getOrDefault(sum, 0) + 1);
             }
         }
-        int res = 0;
-        for (int i = 0; i < C.length; i++) {
-            for (int j = 0; j <D.length; j++) {
-                res += map.getOrDefault(-1 * (C[i] + D[j]), 0);
+        for (int i = 0; i < nums3.length; i++) {
+            for (int j = 0; j < nums4.length; j++) {
+                int sum = nums3[i] + nums4[j];
+                if (map.containsKey(-sum)) {
+                    count += map.get(-sum);
+                }
             }
         }
-        return res;
+        return count;
     }
 }
