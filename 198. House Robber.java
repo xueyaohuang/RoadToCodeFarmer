514ec5cbf9d46fd8d165c28b433b3bbd3690b0b5
CodeToFLAG <hxyqbsmile@gmail.com>
2021-05-02 20:02:54 -0700

    Update 198. House Robber.java

--- 198. House Robber.java
+++ 198. House Robber.java
@@ -1,14 +1,10 @@
 class Solution {
     public int rob(int[] nums) {
-        if (nums == null || nums.length == 0) {
-            return 0;
-        }
-        int rob = 0;
-        int notRob = 0;
-        for (int i = 0; i < nums.length; i++) {
+        int rob = 0, notRob = 0;
+        for (int i : nums) {
             int preRob = rob;
-            rob = Math.max(preRob, notRob + nums[i]); //if rob current value, previous house must not be robbed
-            notRob = Math.max(preRob, notRob) //if not rob ith house, take the max value of robbed (i-1)th house and not rob (i-1)th house
+            rob = notRob + i; //if rob current value, previous house must not be robbed
+            notRob = Math.max(preRob, notRob); //if not rob ith house, take the max value of robbed (i-1)th house and not rob (i-1)th house
         }
         return Math.max(rob, notRob);
     }
