12468804197f972a1b31eb49d7f6bcd4362b8b8d
CodeToFLAG <hxyqbsmile@gmail.com>
2021-05-03 20:33:03 -0700

    Update 172. Factorial Trailing Zeroes.java

--- 172. Factorial Trailing Zeroes.java
+++ 172. Factorial Trailing Zeroes.java
@@ -1,12 +1,17 @@
 // 取决于有多少个因子5，
 // 比如126！其中5有一个因子5，10有一个因子5，25有2个因子5，。。。125有三个因子5.
+// 一个因子5的数字个数是 n/5, 
+// 二个因子5的数字个数是 n/25, 
+// 三个因子5的数字个数是 n/125,
+// ...
 class Solution {
     public int trailingZeroes(int n) {
-        int res = 0;
-        while (n > 0) {
-            res += n / 5;
-            n /= 5;
+        int cur = 1;
+        int count = 0;
+        while (cur < n) {
+            cur *=  5;
+            count += n / cur;
         }
-        return res;
+        return count;
     }
 }
