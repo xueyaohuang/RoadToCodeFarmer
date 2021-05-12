9605f2ca4f03f0756ce3fc0af034246fa83c35b9
CodeToFLAG <hxyqbsmile@gmail.com>
2021-05-11 21:42:52 -0700

    Update 289. Game of Life.java

--- 289. Game of Life.java
+++ 289. Game of Life.java
@@ -1,3 +1,63 @@
+/*
+   current  next
+0  die      die
+1  live     die
+2  die      live
+3  live     live
+*/
+class Solution {
+    public void gameOfLife(int[][] board) {
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                board[i][j] = tempState(board, i, j);
+            }
+        }
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                if ((board[i][j] & 2) > 0) {
+                    board[i][j] = 1;
+                } else {
+                    board[i][j] = 0;
+                }
+            }
+        }
+    }
+    
+    private int tempState(int[][] board, int x, int y) {
+        int currentState = board[x][y];
+        int count = 0;
+        int res = 0;
+        for (int i = x - 1; i <= x + 1; i++) {
+            for (int j = y - 1; j <= y + 1; j++) {
+                if (i < 0 || i >= board.length || j < 0 || j >= board[0].length) {
+                    continue;
+                }
+                if (i == x && j == y) {
+                    continue;
+                }
+                if ((board[i][j] & 1) == 1) {
+                    count++;
+                }
+            }
+        }
+        if (currentState == 1) {
+            if (count < 2) {
+                res = 1;
+            } else if (count > 3) {
+                res =  1;
+            } else {
+                res = 3;
+            }
+        }
+        if (currentState == 0) {
+            if (count == 3) {
+                res = 2;
+            }
+        }
+        return res;
+    }
+}
+
 class Solution {
     public void gameOfLife(int[][] board) {
         if (board == null || board.length == 0) {
