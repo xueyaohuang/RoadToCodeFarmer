b4e5116c76b0cc846d0eab225eea9eee8372421c
CodeToFLAG <hxyqbsmile@gmail.com>
2021-05-12 20:04:14 -0700

    Update 329. Longest Increasing Path in a Matrix.java

--- 329. Longest Increasing Path in a Matrix.java
+++ 329. Longest Increasing Path in a Matrix.java
@@ -1,25 +1,18 @@
 // DP + DFS
 class Solution {
     public int longestIncreasingPath(int[][] matrix) {
-        if (matrix == null || matrix.length == 0) {
-            return 0;
-        }
-        int m = matrix.length;
-        int n = matrix[0].length;
-        int max = 1;
+        int max = 0;
         // 这里的dp相当于其他dfs里的visited
-        int[][] dp = new int[m][n];
-        
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                max = Math.max(max, maxEndHere(matrix, i, j, dp, Integer.MIN_VALUE));
+        int[][] dp = new int[matrix.length][matrix[0].length];
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                max = Math.max(max, dfs(matrix, i, j, dp, Integer.MIN_VALUE));
             }
         }
-        
         return max;
     }
     
-    private int maxEndHere(int[][] matrix, int i, int j, int[][] dp, int prev) {
+    private int dfs(int[][] matrix, int i, int j, int[][] dp, int prev) {
         if (i < 0 || i >= matrix.length || j < 0 || j >= matrix[0].length || matrix[i][j] <= prev) {
             return 0;
         }
@@ -27,10 +20,10 @@ class Solution {
             return dp[i][j];
         }
         int max = 1;
-        max = Math.max(max, maxEndHere(matrix, i + 1, j, dp, matrix[i][j]) + 1);
-        max = Math.max(max, maxEndHere(matrix, i - 1, j, dp, matrix[i][j]) + 1);
-        max = Math.max(max, maxEndHere(matrix, i, j + 1, dp, matrix[i][j]) + 1);
-        max = Math.max(max, maxEndHere(matrix, i, j - 1, dp, matrix[i][j]) + 1);
+        max = Math.max(max, dfs(matrix, i + 1, j, dp, matrix[i][j]) + 1);
+        max = Math.max(max, dfs(matrix, i - 1, j, dp, matrix[i][j]) + 1);
+        max = Math.max(max, dfs(matrix, i, j + 1, dp, matrix[i][j]) + 1);
+        max = Math.max(max, dfs(matrix, i, j - 1, dp, matrix[i][j]) + 1);
         dp[i][j] = max;
         return max;
     }
