c706dbab17960e7f41eaecb092efa289f90b1efa
CodeToFLAG <hxyqbsmile@gmail.com>
2021-08-24 21:11:23 -0700

    Update 523. Continuous Subarray Sum.java

--- 523. Continuous Subarray Sum.java
+++ 523. Continuous Subarray Sum.java
@@ -1,23 +1,15 @@
 class Solution {
     public boolean checkSubarraySum(int[] nums, int k) {
-        if (nums == null || nums.length < 2) {
-            return false;
-        }
-        Map<Integer, Integer> reminder = new HashMap<>();
-        reminder.put(0, -1);//妙啊
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(0, -1); // 妙啊
         int sum = 0;
         for (int i = 0; i < nums.length; i++) {
             sum += nums[i];
-            if (k != 0) {
-                sum %= k;
+            int reminder = sum % k;
+            if (map.containsKey(reminder)  && i - map.get(reminder) > 1) {
+                return true;
             }
-            if (reminder.containsKey(sum)) {  // 不能写成if (reminder.containsKey(sum) && i - reminder.get(sum) > 1),否则else的执行会有问题
-                 if (i - reminder.get(sum) > 1) {
-                     return true;
-                 }             
-            } else {
-                reminder.put(sum, i);
-            }
+            map.putIfAbsent(reminder, i);
         }
         return false;
     }
