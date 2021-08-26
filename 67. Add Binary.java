a7ab1115c65a46389aaac639a3e7a0037d62afe4
CodeToFLAG <hxyqbsmile@gmail.com>
2021-08-25 21:52:26 -0700

    Update 67. Add Binary.java

--- 67. Add Binary.java
+++ 67. Add Binary.java
@@ -26,3 +26,38 @@ class Solution {
         return sb.reverse().toString();
     }
 }
+
+class Solution {
+    public String addBinary(String a, String b) {
+        if (a == null || b == null) {
+            return a == null ? b : a;
+        }
+        int lena = a.length();
+        int lenb = b.length();
+        StringBuilder sb = new StringBuilder();
+        int carry = 0;
+        int i = 0;
+        for (; i < Math.min(lena, lenb); i++) {
+            int numa = a.charAt(lena - 1 - i) - '0';
+            int numb = b.charAt(lenb - 1 - i) - '0';
+            sb.append(numa ^ numb ^ carry);
+            carry = numa + numb + carry >= 2 ? 1 : 0;
+        }
+        while (i < lena) {
+            int numa = a.charAt(lena - 1 - i) - '0';
+            sb.append(numa ^ carry);
+            carry = numa + carry >= 2 ? 1 : 0;
+            i++;
+        }
+        while (i < lenb) {
+            int numb = b.charAt(lenb - 1 - i) - '0';
+            sb.append(numb ^ carry);
+            carry = numb + carry >= 2 ? 1 : 0;
+            i++;
+        }
+        if (carry != 0) {
+            sb.append(carry);
+        }
+        return sb.reverse().toString();
+    }
+}
