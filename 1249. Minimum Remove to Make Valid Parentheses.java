d35d15f8d0a3f2c66797c4361bd86b743665c3b3
CodeToFLAG <hxyqbsmile@gmail.com>
2021-09-02 00:16:31 -0700

    Update 1249. Minimum Remove to Make Valid Parentheses.java

--- 1249. Minimum Remove to Make Valid Parentheses.java
+++ 1249. Minimum Remove to Make Valid Parentheses.java
@@ -1,3 +1,8 @@
+/*
+counting the number of invalid ( and removing the invalid ) in the first pass.
+If there are open number of invalid ( left, we just need to remove them from the end in the second pass.
+
+*/
 class Solution {
     public String minRemoveToMakeValid(String s) {
         int open = 0;
