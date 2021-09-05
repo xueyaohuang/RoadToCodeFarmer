4ee57e5b5bd3ec9b282f2fee04df8e5684507ff5
CodeToFLAG <hxyqbsmile@gmail.com>
2021-09-04 17:38:56 -0700

    Update 636. Exclusive Time of Functions.java

--- 636. Exclusive Time of Functions.java
+++ 636. Exclusive Time of Functions.java
@@ -1,5 +1,33 @@
 class Solution {
     public int[] exclusiveTime(int n, List<String> logs) {
+        int[] res = new int[n];
+        // stack 里是[function_id, start_time]
+        Deque<int[]> stack = new ArrayDeque<>();
+        for (String s : logs) {
+            String[] log = s.split(":");
+            int func = Integer.parseInt(log[0]);
+            int time = Integer.parseInt(log[2]);
+            if (log[1].equals("start")) {
+                if (!stack.isEmpty()) {
+                    int[] prevFunc = stack.peek();
+                    res[prevFunc[0]] += time - prevFunc[1];
+                }
+                stack.push(new int[]{func, time});
+            } else {
+                int[] cur = stack.pop();
+                res[cur[0]] += time - cur[1] + 1;
+                if (!stack.isEmpty()) {
+                    // reset the start time of previous function
+                    stack.peek()[1] = time + 1;
+                }
+            }
+        }
+        return res;
+    }
+}
+
+class Solution {
+    public int[] exclusiveTime(int n, List<String> logs) {
         if (n == 0 || logs == null || logs.size() == 0) {
             return new int[0];
         }
