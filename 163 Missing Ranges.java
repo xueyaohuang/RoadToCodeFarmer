3dd5513694499cff7028965680dd077bc8ee762a
CodeToFLAG <hxyqbsmile@gmail.com>
2021-09-06 16:32:26 -0700

    Update 163 Missing Ranges.java

--- 163 Missing Ranges.java
+++ 163 Missing Ranges.java
@@ -4,6 +4,46 @@
 // 3. 每次衔接，相差一个数就只加一个数，相差超过一个数需要加一个范围 a->b
 // 4. nums中可能有重复的数要跳过
 // 5. nums是空或者null
+
+class Solution {
+    public List<String> findMissingRanges(int[] nums, int lower, int upper) {
+        List<String> res = new ArrayList<>();
+        if (lower == upper) {
+            if (nums.length == 0) {
+                res.add(String.valueOf(lower));
+                return res;
+            }
+            return res;
+        }
+        if (nums.length == 0) {
+            res.add(lower + "->" + upper);
+            return res;
+        }
+        if (nums[0] > lower) {
+            if (nums[0] - lower > 1) {
+                res.add(lower + "->" + (nums[0] - 1));
+            } else {
+                res.add(String.valueOf(lower));
+            }
+        }
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] - nums[i - 1] > 2) {
+                res.add((nums[i - 1] + 1) + "->" + (nums[i] - 1));
+            } else if (nums[i] - nums[i - 1] > 1) {
+                res.add(String.valueOf(nums[i - 1] + 1));
+            }
+        }
+        if (nums[nums.length - 1] < upper) {
+            if (upper - nums[nums.length - 1] > 1) {
+                res.add((nums[nums.length - 1] + 1) + "->" + upper);
+            } else {
+                res.add(String.valueOf(upper));
+            }
+        }
+        return res;
+    }
+}
+
 class Solution {
     public List<String> findMissingRanges(int[] nums, int lower, int upper) {
         // write your code here
