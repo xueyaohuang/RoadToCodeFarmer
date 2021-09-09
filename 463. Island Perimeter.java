f65dcc7099f26191dccd65ff3ed072b785d34188
CodeToFLAG <hxyqbsmile@gmail.com>
2021-09-09 01:12:06 -0700

    Update 463. Island Perimeter.java

--- 463. Island Perimeter.java
+++ 463. Island Perimeter.java
@@ -1,5 +1,36 @@
 class Solution {
     public int islandPerimeter(int[][] grid) {
+        int res = 0;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j] == 1) {
+                    res += 4 - getNumOfAdjcentIsland(grid, i, j);
+                }
+            }
+        }
+        return res;
+    }
+    
+    private int getNumOfAdjcentIsland(int[][] grid, int i, int j) {
+        int count = 0;
+        if (i > 0 && grid[i - 1][j] == 1) {
+            count++;
+        }
+        if (i < grid.length - 1 && grid[i + 1][j] == 1) {
+            count++;
+        }
+        if (j > 0 && grid[i][j - 1] == 1) {
+            count++;
+        }
+        if (j < grid[0].length - 1 && grid[i][j + 1] == 1) {
+            count++;
+        }
+        return count;
+    }
+}
+
+class Solution {
+    public int islandPerimeter(int[][] grid) {
         if (grid == null || grid.length == 0) {
             return 0;
         }
