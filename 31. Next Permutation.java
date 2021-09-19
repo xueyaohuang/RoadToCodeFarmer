fdfca546a9e61192cfc003452c1cda1882424be3
CodeToFLAG <hxyqbsmile@gmail.com>
2021-09-18 17:45:02 -0700

    Update 31. Next Permutation.java

--- 31. Next Permutation.java
+++ 31. Next Permutation.java
@@ -1,3 +1,57 @@
+/*
+I don't think any one can understand this solution without seeing an example, here is an example:
+2,3,6,5,4,1
+
+Solution:
+Step1, from right to left, find the first number which not increase in a ascending order. In this case which is 3.
+Step2, here we can have two situations:
+
+2a.We cannot find the number, all the numbers increasing in a ascending order. This means this permutation is the last permutation, we need to rotate back to the first permutation. So we reverse the whole array, for example, 6,5,4,3,2,1 we turn it to 1,2,3,4,5,6.
+
+2b.We can find the number, then the next step, we will start from right most to leftward, try to find the first number which is larger than 3, in this case it is 4.
+Then we swap 3 and 4, the list turn to 2,4,6,5,3,1.
+Last, we reverse numbers on the right of 4, we finally get 2,4,1,3,5,6.
+*/
+
+class Solution {
+    public void nextPermutation(int[] nums) {
+        if (nums.length < 2) {
+            return;
+        }
+        // step 1
+        int idx = nums.length - 2;
+        while  (idx >= 0 && nums[idx] >= nums[idx + 1]) {
+            idx--;
+        }
+        // step 2a
+        if (idx < 0) {
+            reverse(nums, 0, nums.length - 1);
+            return;
+        }
+        // step 2b
+        int idx1 = nums.length - 1;
+        while (nums[idx1] <= nums[idx]) {
+            idx1--;
+        }
+        swap(nums, idx, idx1);
+        reverse(nums, idx + 1, nums.length - 1);
+    }
+    
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+    
+    private void reverse(int[] nums, int i, int j) {
+        while (i < j) {
+            swap(nums, i, j);
+            i++;
+            j--;
+        }
+    }
+}
+
 class Solution {
     public void nextPermutation(int[] nums) {
         if (nums == null || nums.length <= 1) {
