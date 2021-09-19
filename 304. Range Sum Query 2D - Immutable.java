effdef684d18c534a57a679867216591bda31d12
CodeToFLAG <hxyqbsmile@gmail.com>
2021-09-19 15:37:48 -0700

    Update 304. Range Sum Query 2D - Immutable.java

--- 304. Range Sum Query 2D - Immutable.java
+++ 304. Range Sum Query 2D - Immutable.java
@@ -1,28 +1,21 @@
 class NumMatrix {
     
-    private int[][] sumEndHere;
-    private boolean isNull;
+    // sum[i][j] is the sum of elements inside the rectangle defined by its upper left corner (0, 0) and lower right corner (i, j)
+    int[][] sum;
 
     public NumMatrix(int[][] matrix) {
-        if (matrix == null || matrix.length == 0) {
-            isNull = true;
-        } else {
-            int m = matrix.length;
-            int n = matrix[0].length;
-            sumEndHere = new int[m + 1][n + 1];
-            for (int i = 1; i <= m; i++) {
-                for (int j = 1; j <= n; j++) {
-                    sumEndHere[i][j] = matrix[i - 1][j - 1] - sumEndHere[i - 1][j - 1] + sumEndHere[i - 1][j] + sumEndHere[i][j - 1];
-                }
+        int m = matrix.length;
+        int n = matrix[0].length;
+        sum = new int[m + 1][n + 1];
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                sum[i][j] = sum[i - 1][j] + sum[i][j - 1] - sum[i - 1][j - 1] + matrix[i - 1][j - 1];
             }
         }
     }
     
     public int sumRegion(int row1, int col1, int row2, int col2) {
-        if (isNull) {
-            return 0;
-        }
-        return sumEndHere[row2 + 1][col2 + 1] - sumEndHere[row1][col2 + 1] - sumEndHere[row2 + 1][col1] + sumEndHere[row1][col1];
+        return sum[row2 + 1][col2 + 1] - sum[row2 + 1][col1] - sum[row1][col2 + 1] + sum[row1][col1];
     }
 }
 
