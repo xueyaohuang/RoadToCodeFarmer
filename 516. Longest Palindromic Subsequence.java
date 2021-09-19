0377eea4b80ad366886eb49881afd8ae6e9c024a
CodeToFLAG <hxyqbsmile@gmail.com>
2021-09-18 19:02:32 -0700

    Update 516. Longest Palindromic Subsequence.java

--- 516. Longest Palindromic Subsequence.java
+++ 516. Longest Palindromic Subsequence.java
@@ -5,6 +5,7 @@ class Solution {
         }
         int len  = s.length();
         int[][] dp = new int[len][len];
+        // 从后往前
         for (int i = len - 1; i >= 0; i--) {
             dp[i][i] = 1;
             for (int j = i + 1; j < len; j++) {
@@ -15,6 +16,17 @@ class Solution {
                 }
             }
         }
+//         // 从前往后
+//         for (int j = 0; j < len; j++) {
+//             dp[j][j] = 1;
+//             for (int i = j - 1; i >= 0; i--) {
+//                 if (s.charAt(i) == s.charAt(j)) {
+//                     dp[i][j] = dp[i + 1][j - 1] + 2;
+//                 } else {
+//                     dp[i][j] = Math.max(dp[i + 1][j], dp[i][j - 1]);
+//                 }
+//             }
+//         }
         return dp[0][len - 1];
     }
 }
