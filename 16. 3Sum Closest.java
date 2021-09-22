853469794fb089055b01b07954ae0ab51857e10f
CodeToFLAG <hxyqbsmile@gmail.com>
2021-09-21 20:11:58 -0700

    Update 16. 3Sum Closest.java

--- 16. 3Sum Closest.java
+++ 16. 3Sum Closest.java
@@ -1,6 +1,34 @@
 class Solution {
     public int threeSumClosest(int[] nums, int target) {
         Arrays.sort(nums);
+        int sum = nums[0] + nums[1] + nums[nums.length - 1];
+        for (int i = 0; i < nums.length - 2; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            int j = i + 1, k = nums.length - 1;
+            while (j < k) {
+                int temp = nums[i] + nums[j] + nums[k];
+                if (temp == target) {
+                    return target;
+                }
+                if (Math.abs(sum - target) > Math.abs(temp - target)) {
+                    sum = temp;
+                }
+                if (temp > target) {
+                    k--;
+                }  else {
+                    j++;
+                }
+            }
+        }
+        return sum;
+    }
+}
+
+class Solution {
+    public int threeSumClosest(int[] nums, int target) {
+        Arrays.sort(nums);
         int len = nums.length;
         int sum = nums[0] + nums[1] + nums[nums.length - 1];
         for (int i = 0; i < len; i++) {
