51efa83bcc041f04aebc311a27db443f68443570
CodeToFLAG <hxyqbsmile@gmail.com>
2021-09-23 19:39:35 -0700

    Update 1011. Capacity To Ship Packages Within D Days.java

--- 1011. Capacity To Ship Packages Within D Days.java
+++ 1011. Capacity To Ship Packages Within D Days.java
@@ -1,4 +1,53 @@
 class Solution {
+    public int shipWithinDays(int[] weights, int days) {
+        int start = getMax(weights);
+        int end = getSum(weights);
+        while (start < end) {
+            int mid = (start + end) / 2;
+            if (canShip(weights, days, mid)) {
+                end = mid;
+            } else {
+                start = mid + 1;
+            }
+        }
+        return start;
+    }
+    
+    private int getSum(int[] nums) {
+        int res = 0;
+        for (int n : nums) {
+            res += n;
+        }
+        return res;
+    }
+    
+    private int getMax(int[] nums) {
+        int res = Integer.MIN_VALUE;
+        for (int n : nums) {
+            res = Math.max(res, n);
+        }
+        return res;
+    }
+    
+    private boolean canShip(int[] weights, int days, int cap) {
+        int count = 1;
+        int cur = 0;
+        for (int w : weights) {
+            if (cur + w > cap) {
+                cur = w;
+                count++;
+            } else {
+                cur += w;
+            }
+            if (count > days) {
+                return false;
+            }
+        }
+        return count <= days;
+    }
+}
+
+class Solution {
     public int shipWithinDays(int[] weights, int D) {
         int sum = 0;
         for (int w : weights) {
