f0b3b1d833516044b6fcfb3bcce09d2e26b4f44d
CodeToFLAG <hxyqbsmile@gmail.com>
2021-09-23 19:13:30 -0700

    Update 605. Can Place Flowers.java

--- 605. Can Place Flowers.java
+++ 605. Can Place Flowers.java
@@ -30,6 +30,38 @@
 //     }
 // }
 
+// greedy
+class Solution {
+    public boolean canPlaceFlowers(int[] flowerbed, int n) {
+        int count = 0;
+        for (int i = 0; i < flowerbed.length; i++) {
+            if (canPlaceHere(flowerbed, i)) {
+                count++;
+                flowerbed[i] = 1;
+            }
+            if (count >= n) {
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    private boolean canPlaceHere(int[] flowerbed, int i) {
+        if (flowerbed[i] == 1) {
+            return false;
+        }
+        // 左边有花
+        if (i > 0 && flowerbed[i - 1] == 1) {
+            return false;
+        }
+        // 右边有花
+        if (i < flowerbed.length - 1 && flowerbed[i + 1] == 1) {
+            return false;
+        }
+        return true;
+    }
+}
+
 class Solution {
     public boolean canPlaceFlowers(int[] flowerbed, int n) {
         int count = 0;
