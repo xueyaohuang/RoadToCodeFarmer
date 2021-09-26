b34949fc776250cf9721c0c20c322af8f115e626
CodeToFLAG <hxyqbsmile@gmail.com>
2021-09-26 16:19:01 -0700

    Update 173. Binary Search Tree Iterator.java

--- 173. Binary Search Tree Iterator.java
+++ 173. Binary Search Tree Iterator.java
@@ -55,3 +55,31 @@ That's why it's "average" O(1) time.
  * BSTIterator i = new BSTIterator(root);
  * while (i.hasNext()) v[f()] = i.next();
  */
+
+class BSTIterator {
+    
+    List<Integer> list;
+    int idx;
+
+    public BSTIterator(TreeNode root) {
+        list = new ArrayList<>();
+        dfs(root);
+    }
+    
+    public int next() {
+        return list.get(idx++);
+    }
+    
+    public boolean hasNext() {
+        return idx < list.size();
+    }
+    
+    private void dfs(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        dfs(root.left);
+        list.add(root.val);
+        dfs(root.right);
+    }
+}
