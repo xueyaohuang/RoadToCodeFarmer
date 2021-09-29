1b5ae2b62dbe571895862a4810edcac0fcb202f9
CodeToFLAG <hxyqbsmile@gmail.com>
2021-09-28 23:15:46 -0700

    Update 416. Partition Equal Subset Sum.java

--- 416. Partition Equal Subset Sum.java
+++ 416. Partition Equal Subset Sum.java
@@ -1,33 +1,38 @@
 // 背包问题的变化
 class Solution {
     public boolean canPartition(int[] nums) {
-        if (nums == null || nums.length == 0) {
-            return true;
-        }
-        int sum = 0;
-        for (int num : nums) {
-            sum += num;
-        }
-        if (sum % 2 == 1) {
+        int sum = getSum(nums);
+        if (sum % 2 != 0) {
             return false;
         }
         sum /= 2;
         int len = nums.length;
-        boolean[][] dp = new boolean[len + 1][sum + 1];
-        for (int i = 0; i<= len; i++) {
-            dp[i][0] = true;
+        boolean[][] canPartition = new boolean[len + 1][sum + 1];
+        for (int i = 0; i <= len; i++) {
+            canPartition[i][0] = true;
         }
-        
         for (int i = 1; i <= len; i++) {
             for (int j = 1; j <= sum; j++) {
-                dp[i][j] = dp[i - 1][j];
+                canPartition[i][j] = canPartition[i - 1][j]; // 注意这一步不能少
                 if (j >= nums[i - 1]) {
-                    dp[i][j] = dp[i - 1][j] || dp[i - 1][j - nums[i - 1]];
+                    // 不用nums[i - 1]和用nums[i - 1]
+                    canPartition[i][j] = canPartition[i - 1][j] || canPartition[i - 1][j - nums[i - 1]];
                 }
             }
+            // 检查提前退出
+            if (canPartition[i][sum]) {
+                return true;
+            }
         }
-        
-        return dp[len][sum];
+        return canPartition[len][sum];
+    }
+    
+    private int getSum(int[] nums) {
+        int sum = 0;
+        for (int n : nums) {
+            sum += n;
+        }
+        return sum;
     }
 }
 
