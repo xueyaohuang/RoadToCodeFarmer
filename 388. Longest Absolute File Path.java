5e36e39a0bb4d52f33b72f9df41779fb98797863
CodeToFLAG <hxyqbsmile@gmail.com>
2021-10-02 18:46:39 -0700

    Update 388. Longest Absolute File Path.java

--- 388. Longest Absolute File Path.java
+++ 388. Longest Absolute File Path.java
@@ -1,18 +1,20 @@
+// 用stack存上一层文件夹的长度
 class Solution {
     public int lengthLongestPath(String input) {
-        String[] strs = input.split("\n");
         Deque<Integer> stack = new ArrayDeque<>();
         int maxLen = 0;
-        stack.push(0); // "dummy" length
-        for (String s : strs) {
-            int numTab = s.lastIndexOf("\t") + 1; // number of "\t"
-            while (numTab + 1 < stack.size()) {  // find parent
-                stack.pop();  // 把与当前文件（夹）平级的文件（夹）的长度pop出去
+        String[] strs = input.split("\n");
+        for (String str : strs) {
+            int numOfTab = str.lastIndexOf("\t") + 1; // number of "\t"
+            while (stack.size() > numOfTab) { // find parent
+                stack.pop(); // 把与当前文件（夹）平级的文件（夹）的长度pop出去
             }
-            int curLen = stack.peek() + s.length() - numTab + 1;  // remove "/t", add"/", "/"是最终结果里面要加的，不是s里面的
-            stack.push(curLen);
-            if (s.contains(".")) {
-                maxLen = Math.max(maxLen, curLen - 1); // if s contains ".", it has no "/", so curLen - 1.
+            int previousPathLen = stack.isEmpty() ? 0 : stack.peek();
+            int curLen = previousPathLen + str.length() - numOfTab; // remove length of "/t"
+            if (str.contains(".")) {
+                maxLen = Math.max(maxLen, curLen); // if it's a file, don't add to stack
+            } else {
+                stack.push(curLen + 1); // if it's a dir, add to stack, also add length of "/"
             }
         }
         return maxLen;
