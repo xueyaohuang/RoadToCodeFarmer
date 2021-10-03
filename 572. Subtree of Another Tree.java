f875f3640c641c0664dcd996b10c394669f75219
CodeToFLAG <hxyqbsmile@gmail.com>
2021-10-02 17:18:12 -0700

    Update 572. Subtree of Another Tree.java

--- 572. Subtree of Another Tree.java
+++ 572. Subtree of Another Tree.java
@@ -6,6 +6,10 @@
  *     TreeNode right;
  *     TreeNode(int x) { val = x; }
  * }
+ 
+If assum m is the number of nodes in the 1st tree and n is the number of nodes in the 2nd tree, then
+Time complexity: O(m*n), worst case: for each node in the 1st tree, we need to check if isSame(Node s, Node t). Total m nodes, isSame(...) takes O(n) worst case
+Space complexity: O(height of 1str tree)(Or you can say: O(m) for worst case, O(logm) for average case)
  */
 class Solution {
     public boolean isSubtree(TreeNode s, TreeNode t) {
