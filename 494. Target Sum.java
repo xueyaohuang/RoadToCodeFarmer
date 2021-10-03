5d29b2c782db3471a67ba8ed1483ee9bb55a3f4b
CodeToFLAG <hxyqbsmile@gmail.com>
2021-10-02 17:44:59 -0700

    Update 494. Target Sum.java

--- 494. Target Sum.java
+++ 494. Target Sum.java
@@ -67,3 +67,40 @@ class Solution {
         return dp[target];
     }
 }
+
+/*
+this is a classic knapsack problem
+in knapsack, we decide whether we choose this element or not
+in this question, we decide whether we add this element or minus it
+
+So start with a two dimensional array dp[i][j] which means the number of ways for first i-th element to reach a sum j
+
+we can easily observe that dp[i][j] = dp[i-1][j+nums[i]] + dp[i-1][j-nums[i],
+
+Another part which is quite confusing is return value, here we return dp[sum+S], why is that?
+because dp's range starts from -sum --> 0 --> +sum
+so we need to add sum first, then the total starts from 0, then we add S
+
+Actually most of Sum problems can be treated as knapsack problem, hope it helps
+*/
+
+public int findTargetSumWays(int[] nums, int S) {
+      
+      int sum = 0;
+      for(int n: nums){
+        sum += n;
+      }
+      if (S < -sum || S > sum) { return 0;}
+        
+      int[][] dp = new int[nums.length + 1][ 2 * sum + 1];
+      dp[0][0 + sum] = 1; // 0 + sum means 0, 0 means -sum,  check below graph
+      for(int i = 1; i <= nums.length; i++){
+        for(int j = 0; j < 2 * sum + 1; j++){
+          
+          if(j + nums[i - 1] < 2  * sum + 1) dp[i][j] += dp[i - 1][j + nums[i - 1]];
+          if(j - nums[i - 1] >= 0) dp[i][j] += dp[i - 1][j - nums[i - 1]];
+        }
+      }
+      return dp[nums.length][sum + S];
+    }
+
