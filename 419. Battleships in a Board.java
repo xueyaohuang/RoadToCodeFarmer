814f63934ed08a04d95e800b2370789d190667fb
CodeToFLAG <hxyqbsmile@gmail.com>
2021-10-04 19:25:21 -0700

    Update 419. Battleships in a Board.java

--- 419. Battleships in a Board.java
+++ 419. Battleships in a Board.java
@@ -24,33 +24,50 @@ class Solution {
     }
 }
 
-// 数哪些能加，麻烦一点
 class Solution {
     public int countBattleships(char[][] board) {
-        if (board == null || board.length == 0 || board[0].length == 0) {
-            return 0;
-        }
+        int m = board.length, n = board[0].length;
         int count = 0;
-        for (int i = 0; i < board.length; i++) {
-            if (i == 0 && board[i][0] == 'X') {
-                count++;
-            }
-            if (i > 0 && board[i][0] == 'X' && board[i - 1][0] == '.') {
-                count++;
-            }
-        }
-        for (int j = 1; j < board[0].length; j++) {
-            if (board[0][j] == 'X' && board[0][j - 1] == '.') {
-                count++;
-            }
-        }
-        for (int i = 1; i < board.length; i++) {
-            for (int j = 1; j < board[0].length; j++) {
-                if (board[i][j] == 'X' && board[i][j - 1] == '.' && board[i - 1][j] == '.') {
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (board[i][j] == 'X' && isUpperLeft(board, i, j)) {
                     count++;
                 }
             }
         }
         return count;
+    }
+    
+    private boolean isUpperLeft(char[][] board, int i, int j) {
+        return (i == 0 || board[i - 1][j] == '.') && (j == 0 || board[i][j - 1] == '.');
+    }
+}
+
+// dfs, 没有用到形状是1*k这个info
+class Solution {
+    public int countBattleships(char[][] board) {
+        int m = board.length, n = board[0].length;
+        boolean[][] visited = new boolean[m][n];
+        int count = 0;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (board[i][j] == 'X' && !visited[i][j]) {
+                    count++;
+                    dfs(board, i, j, visited);
+                }
+            }
+        }
+        return count;
+    }
+    
+    private void dfs(char[][] board, int i, int j, boolean[][] visited) {
+        if (i < 0 || i >= board.length || j < 0 || j >= board[0].length || board[i][j] == '.' || visited[i][j]) {
+            return;
+        }
+        visited[i][j] = true;
+        dfs(board, i + 1, j, visited);
+        dfs(board, i - 1, j, visited);
+        dfs(board, i, j + 1, visited);
+        dfs(board, i, j - 1, visited);
     }
 }
