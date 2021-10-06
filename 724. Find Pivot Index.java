9399cef9c9326c3aed8effba768c9d82a166a156
CodeToFLAG <hxyqbsmile@gmail.com>
2021-10-05 20:43:34 -0700

    Update 724. Find Pivot Index.java

--- 724. Find Pivot Index.java
+++ 724. Find Pivot Index.java
@@ -1,5 +1,23 @@
 class Solution {
     public int pivotIndex(int[] nums) {
+        int sum = 0;
+        for (int i : nums) {
+            sum += i;
+        }
+        int cur = 0;
+        for (int i = 0; i < nums.length; i++) {
+            sum -= nums[i];
+            if (cur == sum) {
+                return i;
+            }
+            cur += nums[i];
+        }
+        return -1;
+    }
+}
+
+class Solution {
+    public int pivotIndex(int[] nums) {
         if (nums == null || nums.length == 0) {
             return -1;
         }
