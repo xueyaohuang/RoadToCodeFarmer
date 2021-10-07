22e668256862d7229f0d00384b2f700e3d23e54d
CodeToFLAG <hxyqbsmile@gmail.com>
2021-10-06 19:58:00 -0700

    Create 1382. Balance a Binary Search Tree.java

---
+++ 1382. Balance a Binary Search Tree.java
@@ -0,0 +1,42 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode balanceBST(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        inorderTraversal(root, list);
+        return buildBST(list, 0, list.size() - 1);
+    }
+    
+    private void inorderTraversal(TreeNode root, List<Integer> list) {
+        if (root == null) {
+            return;
+        }
+        inorderTraversal(root.left, list);
+        list.add(root.val);
+        inorderTraversal(root.right, list);
+    }
+    
+    private TreeNode buildBST(List<Integer> list, int start, int end) {
+        if (start > end) {
+            return null;
+        }
+        int mid = (start + end) / 2;
+        TreeNode root = new TreeNode(list.get(mid));
+        root.left = buildBST(list, start, mid - 1);
+        root.right = buildBST(list, mid + 1, end);
+        return root;
+    }
+}
