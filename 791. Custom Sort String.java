6e134508b6faa19c5e14dc584958cf825fa0d8f7
CodeToFLAG <hxyqbsmile@gmail.com>
2021-10-06 20:10:03 -0700

    Update 791. Custom Sort String.java

--- 791. Custom Sort String.java
+++ 791. Custom Sort String.java
@@ -23,3 +23,29 @@ class Solution {
         return sb.toString();
     }
 }
+
+class Solution {
+    public String customSortString(String order, String s) {
+        int[] freq = new int[26];
+        for (char c : s.toCharArray()) {
+            freq[c -  'a']++;
+        }
+        StringBuilder sb = new StringBuilder();
+        for (char c : order.toCharArray()) {
+            if (freq[c - 'a'] != 0) {
+                for (int i = 0; i < freq[c - 'a']; i++) {
+                    sb.append(c);
+                }
+            }
+            freq[c - 'a'] = 0;
+        }
+        for (int i = 0; i < 26; i++) {
+            if (freq[i] != 0) {
+                for (int j = 0; j < freq[i]; j++) {
+                    sb.append((char)(i + 'a'));
+                }
+            }
+        }
+        return sb.toString();
+    }
+}
