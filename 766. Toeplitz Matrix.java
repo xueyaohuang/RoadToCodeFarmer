800e6970e7929b3af9952ad5d50d66b75b837e95
CodeToFLAG <hxyqbsmile@gmail.com>
2021-10-09 15:26:25 -0700

    Update 766. Toeplitz Matrix.java

--- 766. Toeplitz Matrix.java
+++ 766. Toeplitz Matrix.java
@@ -1,5 +1,18 @@
 class Solution {
     public boolean isToeplitzMatrix(int[][] matrix) {
+        for (int i = 0; i < matrix.length - 1; i++) {
+            for (int j = 0; j < matrix[0].length - 1; j++) {
+                if (matrix[i][j] != matrix[i + 1][j + 1]) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
+
+class Solution {
+    public boolean isToeplitzMatrix(int[][] matrix) {
         int m = matrix.length;
         int n = matrix[0].length;
         for (int i = 0; i < m - 1; i++) {
@@ -14,19 +27,6 @@ class Solution {
             int temp = matrix[0][j];
             for (int i = 1; i < m && i + j < n; i++) {
                 if (matrix[i][i + j] != temp) {
-                    return false;
-                }
-            }
-        }
-        return true;
-    }
-}
-
-class Solution {
-    public boolean isToeplitzMatrix(int[][] matrix) {
-        for (int i = 0; i < matrix.length - 1; i++) {
-            for (int j = 0; j < matrix[0].length - 1; j++) {
-                if (matrix[i][j] != matrix[i + 1][j + 1]) {
                     return false;
                 }
             }
