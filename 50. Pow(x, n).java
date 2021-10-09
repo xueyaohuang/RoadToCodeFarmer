f75653e418562e0d391617c1bd8a62617ae98a52
CodeToFLAG <hxyqbsmile@gmail.com>
2021-10-08 20:05:05 -0700

    Update 50. Pow(x, n).java

--- 50. Pow(x, n).java
+++ 50. Pow(x, n).java
@@ -1,5 +1,21 @@
 class Solution {
     public double myPow(double x, int n) {
+        if (n == 0 || x == 1) {
+            return 1;
+        }
+        // n%2==0在n < 0之前检查可以避免n是-2147483648的时候-n溢出
+        if (n % 2 == 0) {
+            return myPow(x * x, n / 2);
+        }
+        if (n < 0) {
+            return 1 / myPow(x, -n);
+        }
+        return x * myPow(x * x, n / 2);
+    }
+}
+
+class Solution {
+    public double myPow(double x, int n) {
         if (n == 0) {
             return 1;
         }
