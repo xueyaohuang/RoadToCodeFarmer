d859b1a59c78cf598b3cf028ab394d28d3b4e395
CodeToFLAG <hxyqbsmile@gmail.com>
2021-10-11 23:17:30 -0700

    Update 496. Next Greater Element I.java

--- 496. Next Greater Element I.java
+++ 496. Next Greater Element I.java
@@ -3,9 +3,11 @@
 // Suppose we have a decreasing sequence followed by a greater number
 // For example [5, 4, 3, 2, 1, 6] then the greater number 6 is the next greater element for all previous numbers in the sequence
 
-// We use a stack to keep a decreasing sub-sequence, whenever we see a number x greater than stack.peek() we pop all elements less than x and for all the popped ones, their next greater element is x
+// We use a stack to keep a decreasing sub-sequence, whenever we see a number x greater than stack.peek() we pop all elements less
+// than x and for all the popped ones, their next greater element is x
 // For example [9, 8, 7, 3, 2, 1, 6]
 // The stack will first contain [9, 8, 7, 3, 2, 1] and then we see 6 which is greater than 1 so we pop 1 2 3 whose next greater element should be 6
+// stack里是还没有找到下一个greater的数
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
         Map<Integer, Integer> map = new HashMap<>();
