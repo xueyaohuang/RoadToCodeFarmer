d46b91d77f0fa17d43a798251adac7d82b771712
CodeToFLAG <hxyqbsmile@gmail.com>
2021-10-11 22:52:47 -0700

    Update 424. Longest Repeating Character Replacement.java

--- 424. Longest Repeating Character Replacement.java
+++ 424. Longest Repeating Character Replacement.java
@@ -1,3 +1,11 @@
+/*
+There's no edge case for this question. The initial step is to extend the window to its limit, that is,
+the longest we can get to with maximum number of modifications. Until then the variable start will remain at 0.
+
+Then as end increase, the whole substring from 0 to end will violate the rule, so we need to update start accordingly (slide the window).
+We move start to the right until the whole string satisfy the constraint again. Then each time we reach such situation, we update our max length.
+*/
+
 class Solution {
     public int characterReplacement(String s, int k) {
         int len = s.length();
