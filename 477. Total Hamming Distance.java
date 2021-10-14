76e345fa07c6f6a48a5115a32f48f6fea00795bd
CodeToFLAG <hxyqbsmile@gmail.com>
2021-10-13 20:47:58 -0700

    Update 477. Total Hamming Distance.java

--- 477. Total Hamming Distance.java
+++ 477. Total Hamming Distance.java
@@ -1,3 +1,7 @@
+// O(n)
+// 转换思路，数每一位上面的Hamming Distance
+// 一共32bits，对每一个bit，数有多少个1（x），0的个数就是len-x
+// 这一位上的Hamming Distance只有在不同时才不是0，需要从1中选一个，0中选一个，所以就是x*(len-x)
 class Solution {
     public int totalHammingDistance(int[] nums) {
         int count = 0;
