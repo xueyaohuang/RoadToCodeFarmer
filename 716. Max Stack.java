e8b48c53282721271a52285ba35a560f91234ca0
CodeToFLAG <hxyqbsmile@gmail.com>
2021-10-24 22:28:14 -0700

    Update 716. Max Stack.java

--- 716. Max Stack.java
+++ 716. Max Stack.java
@@ -64,6 +64,81 @@ class MaxStack {
  * int param_5 = obj.popMax();
  */
 
+/*
+Follow up: Could you come up with a solution that supports O(1) for each top call and O(logn) for each other call?
+Using doubly linked list and treemap
+TreeMap is: A Red-Black tree based NavigableMap implementation.
+This implementation provides guaranteed log(n) time cost for the containsKey, get, put, lastKey and remove operations.
+*/
+class MaxStack {
+
+    Node head;
+    Node tail;
+    TreeMap<Integer, List<Node>> map;
+    
+    public MaxStack() {
+        head = new Node(0);
+        tail = new Node(0);
+        head.next = tail;
+        tail.pre = head;
+        map = new TreeMap<>();
+    }
+    
+    public void push(int x) {
+        Node newNode = new Node(x);
+        newNode.pre = tail.pre;
+        newNode.next = tail;
+        tail.pre.next = newNode;
+        tail.pre = newNode;
+        if(!map.containsKey(x))    map.put(x, new ArrayList<Node>());
+        map.get(x).add(newNode);
+    }
+    
+    public int pop() {
+        int value = tail.pre.val;
+        removeNode(tail.pre);
+        int listSize = map.get(value).size();
+        map.get(value).remove(listSize - 1);
+        if(listSize == 1)    map.remove(value);
+        return value;
+    }
+    
+    public int top() {
+        return tail.pre.val;
+    }
+    
+    public int peekMax() {
+        return map.lastKey();
+    }
+    
+    public int popMax() {
+        int maxVal = map.lastKey();
+        int listSize = map.get(maxVal).size();
+        Node node = map.get(maxVal).remove(listSize - 1);
+        removeNode(node);
+        if(listSize == 1)    map.remove(maxVal);
+        return maxVal;
+    }
+    
+    private void removeNode(Node n){
+        Node preNode = n.pre;
+        Node nextNode = n.next;
+        preNode.next = nextNode;
+        nextNode.pre = preNode;
+    }
+    
+    class Node{
+        Node pre;
+        Node next;
+        int val;
+        public Node(int x){
+            this.val = x;
+            this.pre = null;
+            this.next = null;
+        }
+    }
+}
+
 class MaxStack {
 
     Deque<Integer> stack;
