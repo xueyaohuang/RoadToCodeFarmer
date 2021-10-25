59eb856fc75bf36fcc72c3dc74ac2cdf8c1bb05f
CodeToFLAG <hxyqbsmile@gmail.com>
2021-10-24 20:47:09 -0700

    Create 1547. Minimum Cost to Cut a Stick.java

---
+++ 1547. Minimum Cost to Cut a Stick.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int minCost(int n, int[] cuts) {
+        int[] points = new int[cuts.length + 2];
+        points[0] = 0;
+        points[1] = n;
+        for (int i = 2; i < points.length; i++) {
+            points[i] = cuts[i - 2];
+        }
+        Arrays.sort(points);
+        int[][] dp = new int[points.length][points.length];
+        // i is start
+        // j is end
+        // k is the point in between
+        // if i == j, there is no cost, so j can start from i + 1
+        for (int i = points.length - 2; i >= 0; i--) {
+            for (int j = i + 1; j < points.length; j++) {
+                for (int k = i + 1; k < j; k++) {
+                    int cur = dp[i][k] + dp[k][j] + points[j] - points[i];
+                    dp[i][j] = Math.min(dp[i][j] == 0 ? Integer.MAX_VALUE : dp[i][j], cur);
+                }
+            }
+        }
+        return dp[0][dp.length - 1];
+    }
+}
