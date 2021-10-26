f14667b59e2eb345d668940334b85a767c34545b
CodeToFLAG <hxyqbsmile@gmail.com>
2021-10-25 23:57:33 -0700

    Update 545. Boundary of Binary Tree.java

--- 545. Boundary of Binary Tree.java
+++ 545. Boundary of Binary Tree.java
@@ -75,6 +75,82 @@ class Solution {
     }
 }
 
+class Solution {
+    List<Integer> res;
+    List<Integer> rightBoundary;
+    public List<Integer> boundaryOfBinaryTree(TreeNode root) {
+        res = new ArrayList<>();
+        if (root == null) {
+            return res;
+        }
+        res.add(root.val);
+        if (root.left == null &&  root.right == null) {
+            return res;
+        }
+        getLeftBoundary(root);
+        getLeavesBoundary(root);
+        rightBoundary = new ArrayList<>();
+        getRightBoundary(root);
+        for (int i = rightBoundary.size() - 1; i >= 0; i--) {
+            res.add(rightBoundary.get(i));
+        }
+        return res;
+    }
+    
+    private void getLeftBoundary(TreeNode root) {
+        TreeNode node = root.left;
+        if (node == null) {
+            return;
+        }
+        while (node != null) {
+            if (node.left == null && node.right == null) {
+                return;
+            }
+            res.add(node.val);
+            if (node.left != null) {
+                node = node.left;
+            } else if (node.right != null) {
+                node = node.right;
+            } else {
+                return;
+            }
+        }
+    }
+    
+    private void getLeavesBoundary(TreeNode root) {
+        TreeNode node = root;
+        if (node == null) {
+            return;
+        }
+        if (node.left == null && node.right == null) {
+            res.add(node.val);
+            return;
+        }
+        getLeavesBoundary(node.left);
+        getLeavesBoundary(node.right);
+    }
+    
+    private void getRightBoundary(TreeNode root) {
+        TreeNode node = root.right;
+        if (node == null) {
+            return;
+        }
+        while (node != null) {
+            if (node.left == null && node.right == null) {
+                return;
+            }
+            rightBoundary.add(node.val);
+            if (node.right != null) {
+                node = node.right;
+            } else if (node.left != null) {
+                node = node.left;
+            } else {
+                return;
+            }
+        }
+    }
+}
+
 // one pass
 class Solution {
     public List<Integer> boundaryOfBinaryTree(TreeNode root) {
