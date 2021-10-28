0e0e232731b8c93bb6633a0ac1cf2c6b78f62af9
CodeToFLAG <hxyqbsmile@gmail.com>
2021-10-28 00:42:58 -0700

    Update 670. Maximum Swap.java

--- 670. Maximum Swap.java
+++ 670. Maximum Swap.java
@@ -1,5 +1,7 @@
 /*
 Use buckets to record the last position of digit 0 ~ 9 in this num.
+为什么要找last position，因为把较小的数越往后换新的数越大，比如
+39059，可以换成93059，也可以换成99053，显然3和最后那个9换了之后得到的数最大。
 
 Loop through the num array from left to right.
 For each position, we check whether there exists a larger digit in this num (start from 9 to current digit).
@@ -18,7 +20,7 @@ class Solution {
             for (int j = 9; j > n[i] - '0'; j--) {
                 if (lastIdx[j] > i) { // 不能只check j是否存在（lastIdx[j] ！= 0）， 否则input是9974会return9794
                     char c = n[i];
-                    n[i] = n[lastIdx[j]];
+                    n[i] = n[lastIdx[j]]; // or n[i] = (char)(j + '0');
                     n[lastIdx[j]] = c;
                     return Integer.parseInt(String.valueOf(n));
                 }
