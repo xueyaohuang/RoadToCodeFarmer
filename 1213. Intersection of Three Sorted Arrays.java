d4846202947b46d4f4bbf538cc2fdcd7ab5e50c4
CodeToFLAG <hxyqbsmile@gmail.com>
2021-10-30 22:10:20 -0700

    Update 1213. Intersection of Three Sorted Arrays.java

--- 1213. Intersection of Three Sorted Arrays.java
+++ 1213. Intersection of Three Sorted Arrays.java
@@ -3,12 +3,35 @@ class Solution {
         List<Integer> res = new ArrayList<>();
         int i = 0, j = 0, k = 0;
         while (i < arr1.length && j < arr2.length && k < arr3.length) {
+            int min = Math.min(arr1[i], Math.min(arr2[j], arr3[k]));
+            if (arr1[i] == arr2[j] && arr2[j] == arr3[k]) {
+                res.add(arr1[i]);
+            }
+            if (arr1[i] == min) {
+                i++;
+            }
+            if (arr2[j] == min) {
+                j++;
+            }
+            if (arr3[k] == min) {
+                k++;
+            }
+        }
+        return res;
+    }
+}
+
+class Solution {
+    public List<Integer> arraysIntersection(int[] arr1, int[] arr2, int[] arr3) {
+        List<Integer> res = new ArrayList<>();
+        int i = 0, j = 0, k = 0;
+        while (i < arr1.length && j < arr2.length && k < arr3.length) {
             if (arr1[i] == arr2[j] && arr2[j] == arr3[k]) {
                 res.add(arr1[i]);
                 i++;
                 j++;
                 k++;
-            } else if (arr1[i] < arr2[j]) {
+            } else if (arr1[i] < arr2[j]) { // we don't have to always advance the smallest element's index..
                 i++;
             } else if (arr2[j] < arr3[k]) {
                 j++;
