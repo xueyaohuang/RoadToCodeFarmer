c7fc12a3eef5468dd6c1b226ed704da37585c72a
CodeToFLAG <hxyqbsmile@gmail.com>
2021-10-31 18:49:46 -0700

    Update 157. Read N Characters Given Read4.java

--- 157. Read N Characters Given Read4.java
+++ 157. Read N Characters Given Read4.java
@@ -4,6 +4,29 @@
 public class Solution extends Reader4 {
     /**
      * @param buf Destination buffer
+     * @param n   Number of characters to read
+     * @return    The number of actual characters read
+     */
+    public int read(char[] buf, int n) {
+        int idx = 0;
+        while (idx < n) {
+            char[] temp = new char[4];
+            int cur = read4(temp);
+            if (cur == 0) {
+                break;
+            }
+            int i = 0;
+            while (i < cur && idx < n) {
+                buf[idx++] = temp[i++];
+            }
+        }
+        return idx;
+    }
+}
+
+public class Solution extends Reader4 {
+    /**
+     * @param buf Destination buffer
      * @param n   Maximum number of characters to read
      * @return    The number of characters read
      */
