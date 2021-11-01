846a2a5744691f0828b1cc2a20130b96089e06f8
CodeToFLAG <hxyqbsmile@gmail.com>
2021-10-31 22:10:56 -0700

    Update 543. Diameter of Binary Tree.java

--- 543. Diameter of Binary Tree.java
+++ 543. Diameter of Binary Tree.java
@@ -7,27 +7,6 @@
  *     TreeNode(int x) { val = x; }
  * }
  */
-// https://www.geeksforgeeks.org/diameter-n-ary-tree/
-// https://www.geeksforgeeks.org/longest-path-undirected-tree/
-class Solution {
-    public int diameterOfBinaryTree(TreeNode root) {
-        int[] len = new int[1];
-        if (root != null) {
-            maxAtThisNode(root, len);
-        }
-        return len[0];
-    }
-    private int maxAtThisNode(TreeNode node, int[] len) {
-        if (node == null) {
-            return 0;
-        }
-        int left = node.left == null ? 0 : 1 + maxAtThisNode(node.left, len);
-        int right = node.right == null ? 0 : 1 + maxAtThisNode(node.right, len);
-        len[0] = Math.max(len[0], left + right);
-        return Math.max(left, right);
-    }
-}
-
 // Time complexity is O(n) because the solution post-order traverses across each tree node once.
 // Space complexity is O(h), where h is the height of the tree, or O(lgn).
 class Solution {
@@ -47,6 +26,27 @@ class Solution {
         int right = longestFromHere(node.right);
         diameter = Math.max(diameter, left + right);
         return 1 + Math.max(left, right);
+    }
+}
+
+// https://www.geeksforgeeks.org/diameter-n-ary-tree/
+// https://www.geeksforgeeks.org/longest-path-undirected-tree/
+class Solution {
+    public int diameterOfBinaryTree(TreeNode root) {
+        int[] len = new int[1];
+        if (root != null) {
+            maxAtThisNode(root, len);
+        }
+        return len[0];
+    }
+    private int maxAtThisNode(TreeNode node, int[] len) {
+        if (node == null) {
+            return 0;
+        }
+        int left = node.left == null ? 0 : 1 + maxAtThisNode(node.left, len);
+        int right = node.right == null ? 0 : 1 + maxAtThisNode(node.right, len);
+        len[0] = Math.max(len[0], left + right);
+        return Math.max(left, right);
     }
 }
 
