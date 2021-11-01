0f63e1728102d9bbad3a7fe93c8a3b6f3ebe90ad
CodeToFLAG <hxyqbsmile@gmail.com>
2021-11-01 00:46:38 -0700

    Update 249. Group Shifted Strings.java

--- 249. Group Shifted Strings.java
+++ 249. Group Shifted Strings.java
@@ -1,5 +1,38 @@
 class Solution {
     public List<List<String>> groupStrings(String[] strings) {
+        Map<String, List<String>> map = new HashMap<>();
+        List<List<String>> res = new ArrayList<>();
+        for (String s : strings) {
+            String ShiftedStr = makeShiftedString(s);
+            map.putIfAbsent(ShiftedStr, new ArrayList<>());
+            map.get(ShiftedStr).add(s);
+        }
+        for (List<String> l : map.values()) {
+            res.add(l);
+        }
+        return res;
+    }
+    
+    private String makeShiftedString(String s) {
+        int diff = s.charAt(0) - 'a';
+        // if starts with 'a', doesn't need to shift
+        if (diff == 0) {
+            return s;
+        }
+        StringBuilder sb = new StringBuilder();
+        for (char c : s.toCharArray()) {
+            char shiftedChar = (char)(c - diff);
+            if (shiftedChar < 'a') {
+                shiftedChar = (char)(shiftedChar + 26);
+            }
+            sb.append(shiftedChar);
+        }
+        return sb.toString();
+    }
+}
+
+class Solution {
+    public List<List<String>> groupStrings(String[] strings) {
         if (strings == null || strings.length == 0) {
             return new ArrayList<>();
         }
