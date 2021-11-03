5682abd2ed1a497a7d9bb7b92b86f51c5ec78eb4
CodeToFLAG <hxyqbsmile@gmail.com>
2021-11-03 01:03:03 -0700

    Update 23. Merge k Sorted Lists.java

--- 23. Merge k Sorted Lists.java
+++ 23. Merge k Sorted Lists.java
@@ -45,13 +45,11 @@ class Solution {
             return l1 == null ? l2 : l1;
         }
         if (l1.val < l2.val) {
-            ListNode l = new ListNode(l1.val);
-            l.next = mergeTwoLists(l1.next, l2);
-            return l;
+            l1.next = mergeTwoLists(l1.next, l2);
+            return l1;
         }
-        ListNode l = new ListNode(l2.val);
-        l.next = mergeTwoLists(l1, l2.next);
-        return l;
+        l2.next = mergeTwoLists(l1, l2.next);
+        return l2;
     }
     
     private ListNode mergeKListsHelper(ListNode[] lists, int start, int end) {
