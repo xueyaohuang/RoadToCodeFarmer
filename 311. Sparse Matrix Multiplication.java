24e8e6d457c8ce63e7e320e511159cb0fa0229c8
CodeToFLAG <hxyqbsmile@gmail.com>
2021-11-07 19:18:04 -0800

    Update 311. Sparse Matrix Multiplication.java

--- 311. Sparse Matrix Multiplication.java
+++ 311. Sparse Matrix Multiplication.java
@@ -1,3 +1,4 @@
+// 调换循环顺序，可以减少内层循环次数
 class Solution {
     public int[][] multiply(int[][] A, int[][] B) {
         int row = A.length;
@@ -5,10 +6,12 @@ class Solution {
         int n = A[0].length;
         int[][] res = new int[row][col];
         for (int i = 0; i < row; i++) {
-            for (int j = 0; j < col; j++) {
-                for (int k = 0; k < n; k++) {
-                    if (A[i][k] != 0 && B[k][j] != 0) {
-                        res[i][j] += A[i][k] * B[k][j];
+            for (int k = 0; k < n; k++) {
+                if (A[i][k] != 0) {
+                    for (int j = 0; j < col; j++) {
+                        if (B[k][j] != 0) {
+                            res[i][j] += A[i][k] * B[k][j];
+                        }
                     }
                 }
             }
@@ -24,12 +27,10 @@ class Solution {
         int n = A[0].length;
         int[][] res = new int[row][col];
         for (int i = 0; i < row; i++) {
-            for (int k = 0; k < n; k++) {
-                if (A[i][k] != 0) {
-                    for (int j = 0; j < col; j++) {
-                        if (B[k][j] != 0) {
-                            res[i][j] += A[i][k] * B[k][j];
-                        }
+            for (int j = 0; j < col; j++) {
+                for (int k = 0; k < n; k++) {
+                    if (A[i][k] != 0 && B[k][j] != 0) {
+                        res[i][j] += A[i][k] * B[k][j];
                     }
                 }
             }
