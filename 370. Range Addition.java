92853532d45e4cf6ab3d037d9bc600a80e292473
CodeToFLAG <hxyqbsmile@gmail.com>
2021-12-17 17:49:50 -0800

    Update 370. Range Addition.java

--- 370. Range Addition.java
+++ 370. Range Addition.java
@@ -1,10 +1,38 @@
+/*
+why put inc at startIndex and -inc at endIndex + 1?
+we want to achieve the final result array in two passes:
+Iterate through the k update operations and "somehow" mark them in the [0, 0, 0, 0, 0] array (using length 5 for example),
+for each operation, only update startIndex and endIndex + 1. this is O(k) in total.
+iterate through the marked array and "somehow" transforms it to the final result array. this is O(n) in total (n = length).
+All in all it is O(n + k).
+Now think in a simpler way first, if you have only one update operation, suppose input is (n = 5, updates = { {1, 3, 2} }), what does the O(n + k) solution do?
+
+Initialize the result array as length of n + 1, because we will operate on endIndex + 1:
+result = [0, 0, 0, 0, 0, 0]
+Then marks index 1 as 2 and marks index 3+1 as -2:
+result = [0, 2, 0, 0, -2, 0]
+Next, iterate through result, and accumulates previous sum to current position, just like 303. Range Sum Query - Immutable:
+result = [0, 0 + 2, 0 + 2, 0 + 2, 2 + (-2), 0] = [0, 2, 2, 2, 0, 0]
+Finally, trivial work to discard the last element because we don't need it:
+result = [0, 2, 2, 2, 0], which is the final result.
+Now you might see why we do "puts inc at startIndex and -inc at endIndex + 1":
+
+Put inc at startIndex allows the inc to be carried to the next index starting from startIndex when we do the sum accumulation.
+Put -inc at endIndex + 1 simply means cancel out the previous carry from the next index of the endIndex, because the previous carry 
+should not be counted beyond endIndex.
+And finally, because each of the update operation is independent and the list operation is just an accumulation of the "marks" we do,
+so it can be "makred" all at once first and do the range sum at one time at last step.
+*/
 class Solution {
     public int[] getModifiedArray(int length, int[][] updates) {
         int[] res = new int[length];
         for (int[] update : updates) {
-            res[update[0]] += update[2];
-            if (update[1] < length - 1) {
-                res[update[1] + 1] -= update[2];
+            int start = update[0];
+            int end = update[1];
+            int inc = update[2];
+            res[start] += inc;
+            if (end + 1 < length) {
+                res[end + 1] -= inc;
             }
         }
         int sum = 0;
