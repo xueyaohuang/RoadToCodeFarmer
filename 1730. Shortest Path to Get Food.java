b176213a1e992a4f5660fb81431d0848d001a19d
CodeToFLAG <hxyqbsmile@gmail.com>
2021-12-18 19:44:02 -0800

    Update 1730. Shortest Path to Get Food.java

--- 1730. Shortest Path to Get Food.java
+++ 1730. Shortest Path to Get Food.java
@@ -1,3 +1,51 @@
+// BFS
+class Solution {
+    public int getFood(char[][] grid) {
+        int m = grid.length, n = grid[0].length;
+        Queue<Integer> queue = new LinkedList<>();
+        boolean[][] visited = new boolean[m][n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == '*') {
+                    queue.offer(i * n + j);
+                    visited[i][j] = true;
+                    break;
+                }
+            }
+            if (!queue.isEmpty()) {
+                break;
+            }
+        }
+        int[][] dirs = new int[][]{{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+        int path = 0;
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            path++;
+            for (int k = 0; k < size; k++) {
+                int position = queue.poll();
+                int i = position / n;
+                int j = position % n;
+                // System.out.println(i + " " + j);
+                for (int[] dir : dirs) {
+                    int x = i + dir[0];
+                    int y = j + dir[1];
+                    if (x < 0 || x >= m || y < 0 || y >= n || grid[x][y] == 'X' || visited[x][y]) {
+                        continue;
+                    }
+                    if (grid[x][y] == '#') {
+                        return path;
+                    }
+                    if (grid[x][y] == 'O') {
+                        queue.offer(x * n + y);
+                        visited[x][y] = true;
+                    }
+                }
+            }
+        }
+        return -1;
+    }
+}
+
 class Solution {
     public int getFood(char[][] grid) {
         int m = grid.length, n = grid[0].length;
