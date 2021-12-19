95e28523b703c8dd0fedb550f0ec8afb04b30e92
CodeToFLAG <hxyqbsmile@gmail.com>
2021-12-18 21:16:40 -0800

    Update 239. Sliding Window Maximum.java

--- 239. Sliding Window Maximum.java
+++ 239. Sliding Window Maximum.java
@@ -3,6 +3,17 @@
 // 删除超出window范围的index从head出去，因为先进先出
 // 比nums[i]小的index从tail出去，monotonic queue的出法，都是从tail出
 // 因为有两个方向的dequeue操作，所以需要deque
+/*
+We scan the array from 0 to n-1, keep "promising" elements in the deque. The algorithm is amortized O(n) as each element is put and polled once.
+
+At each i, we keep "promising" elements, which are potentially max number in window [i-(k-1),i] or any subsequent window. This means
+
+    If an element in the deque and it is out of i-(k-1), we discard them. We just need to poll from the head, as we are using a deque and elements are ordered as the sequence in the array
+
+    Now only those elements within [i-(k-1),i] are in the deque. We then discard elements smaller than a[i] from the tail. This is because if a[x] <a[i] and x<i, then a[x] has no chance to be the "max" in [i-(k-1),i], or any other subsequent window: a[i] would always be a better candidate.
+
+    As a result elements in the deque are ordered in both sequence in array and their value. At each step the head of the deque is the max element in [i-(k-1),i]
+*/
 class Solution {
     public int[] maxSlidingWindow(int[] nums, int k) {
         int[] res = new int[nums.length - k + 1];
