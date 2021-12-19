685ce4002606b6bd05ed25fdff7d638f20191fc2
CodeToFLAG <hxyqbsmile@gmail.com>
2021-12-18 16:43:25 -0800

    Update 907. Sum of Subarray Minimums.java

--- 907. Sum of Subarray Minimums.java
+++ 907. Sum of Subarray Minimums.java
@@ -1,19 +1,17 @@
 // naive O(n^2)
 class Solution {
     public int sumSubarrayMins(int[] arr) {
-        int res = 0;
+        int sum = 0;
         int mod = (int)Math.pow(10, 9) + 7;
         for (int i = 0; i < arr.length; i++) {
             int min = arr[i];
-            res += min;
-            res %= mod;
-            for (int j = i + 1; j < arr.length; j++) {
+            for (int j = i; j < arr.length; j++) {
                 min = Math.min(min, arr[j]);
-                res += min;
-                res %= mod;
+                sum += min;
+                sum %= mod;
             }
         }
-        return res;
+        return sum;
     }
 }
 
