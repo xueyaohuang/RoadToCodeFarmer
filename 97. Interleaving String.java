9e8b17ebb7ed7251c3a7da938c6fe2f3a17940e4
CodeToFLAG <hxyqbsmile@gmail.com>
2021-12-26 22:31:30 -0800

    Update 97. Interleaving String.java

--- 97. Interleaving String.java
+++ 97. Interleaving String.java
@@ -7,7 +7,7 @@ class Solution {
         if (len1 + len2 != len3) {
             return false;
         }
-        // dp[i][j]表示s3[:i+j-1]是否是s1[:i],s2[:j]的interleave
+        // dp[i][j]表示s3[:i+j]是否是s1[:i],s2[:j]的interleave(注意s[:i]这个substring不包含第i个char)
         boolean[][] dp = new boolean[len1 + 1][len2 + 1];
         
         // 像这样dp table多出一维的情况，初始化时不仅要考虑dp[0][0],还要考虑dp[i][0],dp[0][i]
@@ -32,5 +32,30 @@ class Solution {
         }
 
         return dp[len1][len2];
+    }
+}
+
+class Solution {
+    public boolean isInterleave(String s1, String s2, String s3) {
+        int m = s1.length(), n = s2.length(), p = s3.length();
+        if (m + n != p) {
+            return false;
+        }
+        boolean[][] dp = new boolean[m + 1][n + 1];
+        for (int i = 0; i <= m; i++) {
+            for (int j = 0; j <= n; j++) {
+                if (i == 0 && j == 0) {
+                    dp[i][j] = true;
+                } else if (i == 0) {
+                    dp[i][j] = dp[i][j - 1] && s2.charAt(j - 1) == s3.charAt(j - 1);
+                } else if (j == 0) {
+                    dp[i][j] = dp[i - 1][j] && s1.charAt(i - 1) == s3.charAt(i - 1);
+                } else {
+                    dp[i][j] = (dp[i - 1][j] && s1.charAt(i - 1) == s3.charAt(i + j - 1)) ||
+                        (dp[i][j - 1] && s2.charAt(j - 1) == s3.charAt(i + j - 1));
+                }
+            }
+        }
+        return dp[m][n];
     }
 }
