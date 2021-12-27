81ed214dd017dd9ae96df72576277fe397438d62
CodeToFLAG <hxyqbsmile@gmail.com>
2021-12-26 22:48:15 -0800

    Update 55. Jump Game.java

--- 55. Jump Game.java
+++ 55. Jump Game.java
@@ -1,5 +1,21 @@
 class Solution {
     public boolean canJump(int[] nums) {
+        int maxReach = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (i > maxReach) {
+                return false;
+            }
+            maxReach = Math.max(maxReach, i + nums[i]);
+            if (maxReach >= nums.length - 1) {
+                return true;
+            }
+        }
+        return maxReach >= nums.length - 1;
+    }
+}
+
+class Solution {
+    public boolean canJump(int[] nums) {
         if (nums == null || nums.length == 0) {
             return true;
         }
