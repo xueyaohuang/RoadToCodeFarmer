86ea6d5be0624a90979c61cee2d770a22ae4a1ca
CodeToFLAG <hxyqbsmile@gmail.com>
2021-12-29 17:56:51 -0800

    Create 1429. First Unique Number.java

---
+++ 1429. First Unique Number.java
@@ -0,0 +1,37 @@
+class FirstUnique {
+    
+    Map<Integer, Integer> map;
+    Queue<Integer> queue;
+
+    public FirstUnique(int[] nums) {
+        map = new HashMap<>();
+        queue = new LinkedList<>();
+        for (int n : nums) {
+            if (!map.containsKey(n)) {
+                queue.offer(n);
+            }
+            map.put(n, map.getOrDefault(n, 0) + 1);
+        }
+    }
+    
+    public int showFirstUnique() {
+        while (!queue.isEmpty() && map.getOrDefault(queue.peek(), 0) > 1) {
+            queue.poll();
+        }
+        return queue.isEmpty() ? -1 : queue.peek();
+    }
+    
+    public void add(int value) {
+        if (!map.containsKey(value)) {
+                queue.offer(value);
+        }
+        map.put(value, map.getOrDefault(value, 0) + 1);
+    }
+}
+
+/**
+ * Your FirstUnique object will be instantiated and called as such:
+ * FirstUnique obj = new FirstUnique(nums);
+ * int param_1 = obj.showFirstUnique();
+ * obj.add(value);
+ */
