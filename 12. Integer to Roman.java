82c6daebeea6b82fd975048c7d2272d97033e3df
CodeToFLAG <hxyqbsmile@gmail.com>
2021-12-30 18:08:23 -0800

    Update 12. Integer to Roman.java

--- 12. Integer to Roman.java
+++ 12. Integer to Roman.java
@@ -1,9 +1,26 @@
 class Solution {
     public String intToRoman(int num) {
+        String[] roman = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+	    int[] values = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+        StringBuilder sb = new StringBuilder();
+        int idx = 0;
+        while (num > 0) {
+            while (num >= values[idx]) {
+                num -= values[idx];
+                sb.append(roman[idx]);
+            }
+            idx++;
+        }
+        return sb.toString();
+    }
+}
+
+class Solution {
+    public String intToRoman(int num) {
         String[] M = {"", "M", "MM", "MMM"};
-        String C[] = {"", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"};
-        String X[] = {"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"};
-        String I[] = {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"};
+        String[] C = {"", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"};
+        String[] X = {"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"};
+        String[] I = {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"};
         return M[num / 1000] + C[(num % 1000) / 100] + X[(num % 100) / 10] + I[num % 10];
     }
 }
