184288a269f20d7d19a83badaa420693e98a7bbf
CodeToFLAG <hxyqbsmile@gmail.com>
2022-01-03 22:13:05 -0800

    Update 8. String to Integer (atoi).java

--- 8. String to Integer (atoi).java
+++ 8. String to Integer (atoi).java
@@ -1,32 +1,29 @@
 class Solution {
-    public int myAtoi(String str) {
-        str = str.trim();
-        if (str == null || str.length() == 0) {
+    public int myAtoi(String s) {
+        s = s.trim();
+        if (s.length() == 0) {
             return 0;
-        }  
+        }
         long sum = 0;
         int sign = 1;
-        int start = 0;
-        if (str.charAt(0) == '+') {
-            sign = 1;
-            start = 1;
-        }
-        if (str.charAt(0) == '-') {
+        if (s.charAt(0) == '-') {
             sign = -1;
-            start = 1;
+            s = s.substring(1);
+        } else if (s.charAt(0) == '+') {
+            s = s.substring(1);
         }
-        for (int i = start; i < str.length(); i++) {
-            if (! Character.isDigit(str.charAt(i))) {
-                return (int) sum * sign;
+        for (int i = 0; i < s.length(); i++) {
+            if (!Character.isDigit(s.charAt(i))) {
+                return sign * (int)sum;
             }
-            sum = sum * 10 + str.charAt(i) - '0';
-            if (sign == 1 && sum > Integer.MAX_VALUE) {
+            sum = sum * 10 + s.charAt(i) - '0';
+            if (sign * sum >= Integer.MAX_VALUE) {
                 return Integer.MAX_VALUE;
             }
-            if (sign == -1 && sum > Integer.MAX_VALUE) {
+            if (sign * sum <= Integer.MIN_VALUE) {
                 return Integer.MIN_VALUE;
             }
         }
-        return (int) sum * sign;
+        return sign * (int)sum;
     }
 }
