3a135ea9d39d68ce4190cf9f182ab98d26b93d72
CodeToFLAG <hxyqbsmile@gmail.com>
2022-01-08 20:46:05 -0800

    Update 380. Insert Delete GetRandom O-1.java

--- 380. Insert Delete GetRandom O-1.java
+++ 380. Insert Delete GetRandom O-1.java
@@ -1,12 +1,14 @@
 class RandomizedSet {
     
-    private ArrayList<Integer> nums;
-    private HashMap<Integer, Integer> map;    
+    Map<Integer, Integer> map;
+    List<Integer> list;
+    Random rand;
 
     /** Initialize your data structure here. */
     public RandomizedSet() {
-        nums = new ArrayList<Integer>();
-        map = new HashMap<Integer, Integer>();
+        map = new HashMap<>();
+        list = new ArrayList<>();
+        rand = new Random();
     }
     
     /** Inserts a value to the set. Returns true if the set did not already contain the specified element. */
@@ -14,32 +16,31 @@ class RandomizedSet {
         if (map.containsKey(val)) {
             return false;
         }
-        nums.add(val);
-        map.put(val, nums.size() - 1);
+        list.add(val);
+        map.put(val, list.size() - 1);
         return true;
     }
     
     /** Removes a value from the set. Returns true if the set contained the specified element. */
     public boolean remove(int val) {
-        if (! map.containsKey(val)) {
+        if (!map.containsKey(val)) {
             return false;
         }
-        int index = map.get(val);
-        if (index != nums.size() - 1) {
-            int last = nums.get(nums.size() - 1);
-            nums.set(index, last);
-            map.put(last, index);
+        int last = list.get(list.size() - 1);
+        if (last != val) {
+            int idx = map.get(val);
+            map.put(last, idx);
+            list.set(idx, last);
         }
-        nums.remove(nums.size() - 1);
         map.remove(val);
+        list.remove(list.size() - 1);
         return true;
     }
     
     /** Get a random element from the set. */
     public int getRandom() {
-        Random random = new Random();
-        int rand = random.nextInt(nums.size());
-        return nums.get(rand);
+        int idx = rand.nextInt(list.size());
+        return list.get(idx);
     }
 }
 
