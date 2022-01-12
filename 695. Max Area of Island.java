6922512c7a947523c9decf29484e8e9fd824d140
CodeToFLAG <hxyqbsmile@gmail.com>
2022-01-11 23:04:33 -0800

    Update 695. Max Area of Island.java

--- 695. Max Area of Island.java
+++ 695. Max Area of Island.java
@@ -1,4 +1,35 @@
 class Solution {
+    int curArea = 0;
+    public int maxAreaOfIsland(int[][] grid) {
+        int m = grid.length, n = grid[0].length;
+        boolean[][] visited = new boolean[m][n];
+        int res = 0;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == 1 && !visited[i][j]) {
+                    curArea = 0;
+                    dfs(grid, i, j, visited);
+                    res = Math.max(res, curArea);
+                }
+            }
+        }
+        return res;
+    }
+    
+    private void dfs(int[][] grid, int i, int j, boolean[][] visited) {
+        if (i < 0 || i >= grid.length || j < 0 || j >= grid[0].length || grid[i][j] == 0 || visited[i][j]) {
+            return;
+        }
+        visited[i][j] = true;
+        curArea++;
+        dfs(grid, i + 1, j, visited);
+        dfs(grid, i - 1, j, visited);
+        dfs(grid, i, j + 1, visited);
+        dfs(grid, i, j - 1, visited);
+    }
+}
+
+class Solution {
     public int maxAreaOfIsland(int[][] grid) {
         if (grid == null || grid.length == 0) {
             return 0;
