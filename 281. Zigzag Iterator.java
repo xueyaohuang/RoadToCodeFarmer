6c53fbba4237fd2b493ced0674cf3ac4f2bceb57
CodeToFLAG <hxyqbsmile@gmail.com>
2022-01-29 16:02:28 -0800

    Update 281. Zigzag Iterator.java

--- 281. Zigzag Iterator.java
+++ 281. Zigzag Iterator.java
@@ -1,36 +1,57 @@
+// Follow up: What if you are given k vectors? How well can your code be extended to such cases?
 public class ZigzagIterator {
     
-    List<Integer> list;
-    Iterator<Integer> ltr;
+    Queue<Iterator> its;
 
     public ZigzagIterator(List<Integer> v1, List<Integer> v2) {
-        list = new ArrayList<>();
-        initializeIterator(v1, v2);
-        ltr = list.iterator();
-    }
-    
-    private void initializeIterator(List<Integer> v1, List<Integer> v2) {
-        int len1 = v1.size();
-        int len2 = v2.size();
-        int i = 0, j = 0;
-        while (i < len1 || j < len2) {
-            if (i < len1) {
-                list.add(v1.get(i));
-                i++;
-            }
-            if (j < len2) {
-                list.add(v2.get(j));
-                j++;
-            }
+        its = new LinkedList<>();
+        if (!v1.isEmpty()) {
+            its.offer(v1.iterator());
+        }
+        if (!v2.isEmpty()) {
+            its.offer(v2.iterator());
         }
     }
 
     public int next() {
-        return ltr.next();
+        Iterator cur = its.poll();
+        int res = (Integer)cur.next();
+        if (cur.hasNext()) {
+            its.offer(cur);
+        }
+        return res;
     }
 
     public boolean hasNext() {
-        return ltr.hasNext();
+        return !its.isEmpty();
+    }
+}
+
+public class ZigzagIterator {
+    
+    List<Integer> list;
+    Iterator<Integer> it;
+
+    public ZigzagIterator(List<Integer> v1, List<Integer> v2) {
+        list = new ArrayList<>();
+        int i = 0, j = 0;
+        while (i < v1.size() || j < v2.size()) {
+            if (i < v1.size()) {
+                list.add(v1.get(i++));
+            }
+            if (j < v2.size()) {
+                list.add(v2.get(j++));
+            }
+        }
+        it = list.iterator();
+    }
+
+    public int next() {
+        return it.next();
+    }
+
+    public boolean hasNext() {
+        return it.hasNext();
     }
 }
 
