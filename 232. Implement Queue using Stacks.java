1f49591f0d4d5af2cbbb732e6257900904408f41
CodeToFLAG <hxyqbsmile@gmail.com>
2022-01-30 19:58:12 -0800

    Update 232. Implement Queue using Stacks.java

--- 232. Implement Queue using Stacks.java
+++ 232. Implement Queue using Stacks.java
@@ -7,47 +7,42 @@ there will always one stack empty. However, in a multi-thread application, if we
 for thread-safety, either read or write will lock the whole queue. In the two stack implementation, as
 long as the second stack is not empty, push operation will not lock the stack for pop.
 */
-
 class MyQueue {
     
-    Stack<Integer> sIn;
-    Stack<Integer> sOut;
-    // 一个用来接收push的新元素，一个用来pop和peek，只有当sOut为空时，才从in把元素都转到out
+    // 一个用来接收push的新元素，一个用来pop和peek，只有当out为空时，才从in把元素都转到out
+    Deque<Integer> in;
+    Deque<Integer> out;
 
-    /** Initialize your data structure here. */
     public MyQueue() {
-        sIn = new Stack<Integer>();
-        sOut = new Stack<Integer>();
+        in = new ArrayDeque<>();
+        out = new ArrayDeque<>();
     }
     
-    /** Push element x to the back of queue. */
     public void push(int x) {
-        sIn.push(x);
+        in.push(x);
     }
     
-    /** Removes the element from in front of queue and returns that element. */
     public int pop() {
-        if (sOut.isEmpty()) { //往sOut里面push要注意，为了保持顺序，只有sOut是空的时候才能push新的元素。
-            while (!sIn.isEmpty()) {
-                sOut.push(sIn.pop());
-            }
-        }      
-        return sOut.pop();
-    }
-    
-    /** Get the front element. */
-    public int peek() {
-        if (sOut.isEmpty()) { //往sOut里面push要注意，为了保持顺序，只有sOut是空的时候才能push新的元素。
-            while (!sIn.isEmpty()) {
-                sOut.push(sIn.pop());
+        //往sOut里面push要注意，为了保持顺序，只有sOut是空的时候才能push新的元素
+        if (out.isEmpty()) {
+            while (!in.isEmpty()) {
+                out.push(in.pop());
             }
         }
-        return sOut.peek();
+        return out.pop();
     }
     
-    /** Returns whether the queue is empty. */
+    public int peek() {
+        if (out.isEmpty()) {
+            while (!in.isEmpty()) {
+                out.push(in.pop());
+            }
+        }
+        return out.peek();
+    }
+    
     public boolean empty() {
-        return sIn.isEmpty() && sOut.isEmpty();
+        return in.isEmpty() &&  out.isEmpty();
     }
 }
 
