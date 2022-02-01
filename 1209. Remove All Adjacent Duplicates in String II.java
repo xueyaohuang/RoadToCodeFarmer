aaff6c13f1c8d2cb246dd0ccc298339874a1f869
CodeToFLAG <hxyqbsmile@gmail.com>
2022-01-31 21:38:30 -0800

    Update 1209. Remove All Adjacent Duplicates in String II.java

--- 1209. Remove All Adjacent Duplicates in String II.java
+++ 1209. Remove All Adjacent Duplicates in String II.java
@@ -1,3 +1,23 @@
+class Solution {
+    public String removeDuplicates(String s, int k) {
+        int[] count = new int[s.length()];
+        StringBuilder sb = new StringBuilder();
+        for (char c : s.toCharArray()) {
+            sb.append(c);
+            int idx = sb.length() - 1;
+            count[idx] = 1 + (idx > 0 && sb.charAt(idx - 1) == c ? count[idx - 1] : 0);
+            if (count[idx] == k) {
+                sb.delete(sb.length() - k, sb.length());
+                // for (int i = 0; i < k; i++) {
+                //     sb.setLength(sb.length() - 1);
+                //     count[idx - i] = 0; // this is not needed
+                // }
+            }
+        }
+        return sb.toString();
+    }
+}
+
 // time complexity of Java Stringbuilder setLength (用来删除sb里最后一个char)
 // https://stackoverflow.com/questions/35110696/java-stringbuilder-setlength-is-time-complexity-o1
 // Time complexity O(1)
