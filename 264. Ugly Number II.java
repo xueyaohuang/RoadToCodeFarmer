69cc1d077555ae29eaaf678bc3fca1aa82ddbd67
CodeToFLAG <hxyqbsmile@gmail.com>
2022-02-02 19:34:17 -0800

    Update 264. Ugly Number II.java

--- 264. Ugly Number II.java
+++ 264. Ugly Number II.java
@@ -1,6 +1,7 @@
 
 /*
-We have an array k of first n ugly number. We only know, at the beginning, the first one, which is 1. Then
+动态生成Ugly Number序列，一开始只有1，下一个数字是用已有的数字分别乘以2，3，5，然后取最小的那个
+idx2表示下一个可以和2相乘的数字的idx，同理idx3，idx5
 
 k[1] = min( k[0]x2, k[0]x3, k[0]x5). The answer is k[0]x2. So we move 2's pointer to 1. Then we test:
 
@@ -8,21 +9,21 @@ k[2] = min( k[1]x2, k[0]x3, k[0]x5). And so on. Be careful about the cases such 
 */
 class Solution {
     public int nthUglyNumber(int n) {
-        int[] ugly = new int[n];
-        ugly[0] = 1;
-        int p2 = 0, p3 = 0, p5 = 0;
+        int[] dp = new int[n];
+        dp[0] = 1;
+        int idx2 = 0, idx3 = 0, idx5 = 0;
         for (int i = 1; i < n; i++) {
-            ugly[i] = Math.min(ugly[p2] * 2, Math.min(ugly[p3] * 3, ugly[p5] * 5));
-            if (ugly[i] == ugly[p2] * 2) {
-                p2++;
+            dp[i] = Math.min(dp[idx2] * 2, Math.min(dp[idx3] * 3, dp[idx5] * 5));
+            if (dp[i] == dp[idx2] * 2) {
+                idx2++;
             }
-            if (ugly[i] == ugly[p3] * 3) {
-                p3++;
+            if (dp[i] == dp[idx3] * 3) {
+                idx3++;
             }
-            if (ugly[i] == ugly[p5] * 5) {
-                p5++;
+            if (dp[i] == dp[idx5] * 5) {
+                idx5++;
             }
         }
-        return ugly[n - 1];
+        return dp[n - 1];
     }
 }
