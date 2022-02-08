0f0ec9fd70a0ad10b9e1bce08fc054d1ae708069
CodeToFLAG <hxyqbsmile@gmail.com>
2022-02-07 21:15:36 -0800

    Update and rename 283. Move Zeroes to 283. Move Zeroes.java

--- 283. Move Zeroes.java
+++ 283. Move Zeroes.java
@@ -1,3 +1,21 @@
+
+class Solution {
+    public void moveZeroes(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return;
+        }
+        int j = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                int temp = nums[j];
+                nums[j] = nums[i];
+                nums[i] = temp;
+                j++;
+            }
+        }
+    }
+}
+
 class Solution {
     public void moveZeroes(int[] nums) {
         if (nums == null || nums.length == 0) {
@@ -33,23 +51,6 @@ class Solution {
             }
             i++;
             j++;
-        }
-    }
-}
-
-class Solution {
-    public void moveZeroes(int[] nums) {
-        if (nums == null || nums.length == 0) {
-            return;
-        }
-        int j = 0;
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] != 0) {
-                int temp = nums[j];
-                nums[j] = nums[i];
-                nums[i] = temp;
-                j++;
-            }
         }
     }
 }
