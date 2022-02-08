315afc7505778788dcf4190650394824cae8ad40
CodeToFLAG <hxyqbsmile@gmail.com>
2022-02-07 21:19:48 -0800

    Update 26. Remove Duplicates from Sorted Array.java

--- 26. Remove Duplicates from Sorted Array.java
+++ 26. Remove Duplicates from Sorted Array.java
@@ -1,15 +1,11 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
-        if (nums == null || nums.length == 0) {
-            return 0;
-        }
-        int j = 1;
+        int k = 1;
         for (int i = 1; i < nums.length; i++) {
             if (nums[i] != nums[i - 1]) {
-                nums[j] = nums[i];
-                j++;
+                nums[k++] = nums[i];
             }
         }
-        return j;
+        return k;
     }
 }
