cc6de75e770bf3b320ca41d46ccd7c9274a7e058
CodeToFLAG <hxyqbsmile@gmail.com>
2022-02-11 01:37:27 -0800

    Update 340. Longest Substring with At Most K Distinct Characters.java

--- 340. Longest Substring with At Most K Distinct Characters.java
+++ 340. Longest Substring with At Most K Distinct Characters.java
@@ -1,23 +1,21 @@
 class Solution {
     public int lengthOfLongestSubstringKDistinct(String s, int k) {
-        if (s == null || s.length() == 0) {
-            return 0;
-        }
-        int[] map = new int[256];
-        int len = s.length();
-        int maxLen = 0;
-        int count = 0;
+        int[] map = new int[128];
         int start = 0;
-        for (int end = 0; end < len; end++) {
+        int count = 0;
+        int maxLen = 0;
+        for (int end = 0; end < s.length(); end++) {
             char cEnd = s.charAt(end);
-            if (map[cEnd]++ == 0) { // ++之前等于0，说明加完后多出一个distinct的char 
+            if (map[cEnd] == 0) { // ++之前等于0，说明加完后多出一个distinct的char 
                 count++;
             }
+            map[cEnd]++;
             while (count > k) {
                 char cStart = s.charAt(start);
-                if (map[cStart]-- == 1) { // -- 之前 等于1，说明减完后会消除一个distinct的char
+                if (map[cStart] == 1) { // -- 之前 等于1，说明减完后会消除一个distinct的char
                     count--;
                 }
+                map[cStart]--;
                 start++;
             }
             maxLen = Math.max(maxLen, end - start + 1);
