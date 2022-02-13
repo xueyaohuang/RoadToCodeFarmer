cac6adc33ada3d9fa72d245337c80b68ea84e4f2
CodeToFLAG <hxyqbsmile@gmail.com>
2022-02-12 17:05:06 -0800

    Update 133. Clone Graph.java

--- 133. Clone Graph.java
+++ 133. Clone Graph.java
@@ -20,6 +20,31 @@ class Node {
 class Solution {
     public Node cloneGraph(Node node) {
         if (node == null) {
+            return null;
+        }
+        Map<Node, Node> map = new HashMap<>();
+        return cloneNode(node, map);
+    }
+    
+    // 想清楚helper method到底要干啥：1.clone一个node，2.clone node的neighbor
+    private Node cloneNode(Node node, Map<Node, Node> map) {
+        if (map.containsKey(node)) {
+            return map.get(node);
+        }
+        // 1.clone一个node
+        Node cloned = new Node(node.val);
+        map.put(node, cloned);
+        // 2.clone node的neighbor
+        for (Node n : node.neighbors) {
+            cloned.neighbors.add(cloneNode(n, map));
+        }
+        return cloned;
+    }
+}
+
+class Solution {
+    public Node cloneGraph(Node node) {
+        if (node == null) {
             return node;
         }
         Map<Node, Node> map = new HashMap<>();
@@ -37,27 +62,5 @@ class Solution {
             dfs(n, map);
             cloned.neighbors.add(map.get(n));
         }
-    }
-}
-
-class Solution {
-    public Node cloneGraph(Node node) {
-        if (node == null) {
-            return node;
-        }
-        Map<Node, Node> map = new HashMap<>();
-        return dfs(node, map);
-    }
-    
-    private Node dfs(Node node, Map<Node, Node> map) {
-        if (map.containsKey(node)) {
-            return map.get(node);
-        }
-        Node cloned = new Node(node.val);
-        map.put(node, cloned);
-        for (Node n : node.neighbors) {
-            cloned.neighbors.add(dfs(n, map));
-        }
-        return cloned;
     }
 }
