3c30ccf08f516b1391e0f17f7331a9fddaabe530
CodeToFLAG <hxyqbsmile@gmail.com>
2022-02-20 17:05:36 -0800

    Update 987. Vertical Order Traversal of a Binary Tree.java

--- 987. Vertical Order Traversal of a Binary Tree.java
+++ 987. Vertical Order Traversal of a Binary Tree.java
@@ -7,6 +7,42 @@
  *     TreeNode(int x) { val = x; }
  * }
  */
+class Solution {
+    int leftMost = 0;
+    int rightMost = 0;
+    public List<List<Integer>> verticalTraversal(TreeNode root) {
+        if (root == null) {
+            return new ArrayList<>();
+        }
+        List<List<Integer>> res = new ArrayList<>();
+        Map<Integer, List<int[]>> map = new HashMap<>();
+        dfs(root, map, 0, 0);
+        for (int i = leftMost; i <= rightMost; i++) {
+            Collections.sort(map.get(i), (a, b) -> a[1] == b[1] ? a[0] - b[0] : a[1] - b[1]);
+            List<Integer> temp = new ArrayList<>();
+            for (int[] arr : map.get(i)) {
+                temp.add(arr[0]);
+            }
+            res.add(temp);
+        }
+        return res;
+    }
+    
+    private void dfs(TreeNode root, Map<Integer, List<int[]>> map, int level, int col) {
+        if (root == null) {
+            return;
+        }
+        map.putIfAbsent(col, new ArrayList<>());
+        leftMost = Math.min(leftMost, col);
+        rightMost = Math.max(rightMost, col);
+        map.get(col).add(new int[]{root.val, level});
+        dfs(root.left, map, level + 1, col - 1);
+        dfs(root.right, map, level + 1, col + 1);
+    }
+}
+
+
+// sol 2
 class Tuple {
     int y;
     int val;
