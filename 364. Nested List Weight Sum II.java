0fee2c9130862cca47c04d994e439bbe710c04a1
CodeToFLAG <hxyqbsmile@gmail.com>
2022-02-22 22:05:31 -0800

    Update 364. Nested List Weight Sum II.java

--- 364. Nested List Weight Sum II.java
+++ 364. Nested List Weight Sum II.java
@@ -26,26 +26,24 @@ For example, 1x + 2y + 3z = (3 + 1) * (x + y + z) - (3x + 2y + z);
 So we can convert this problem to Nested List Weight Sum I and just record max depth and flat sum at the same time.
 */
 class Solution {
+    int maxDepth;
+    int flatSum;
+    int weightedSum; // weightedSum in LC339
     public int depthSumInverse(List<NestedInteger> nestedList) {
-        int depthSum = dfs(nestedList, 1);
-        return flatSum * (max + 1) - depthSum;
+        getSum(nestedList, 1);
+        return flatSum * (maxDepth + 1) - weightedSum;
     }
-
-    int flatSum = 0;
-    int max = 1;
-    private int dfs(List<NestedInteger> input, int depth) {
-        if (input == null || input.size() == 0) return 0;
-        int sum = 0;
-        for (NestedInteger i : input) {
-            if (i.isInteger()) {
-                max = Math.max(depth, max);
-                sum += i.getInteger() * depth;
-                flatSum += i.getInteger();
+    
+    private void getSum(List<NestedInteger> nestedList, int depth) {
+        for (NestedInteger ni : nestedList) {
+            if (ni.isInteger()) {
+                weightedSum += ni.getInteger() * depth;
+                flatSum += ni.getInteger();
+                maxDepth = Math.max(maxDepth, depth);
             } else {
-                sum += dfs(i.getList(), depth + 1);
+                getSum(ni.getList(), depth + 1);
             }
         }
-        return sum;
     }
 }
 
