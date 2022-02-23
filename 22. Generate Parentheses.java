394b1596ef746a71dd421b32a53458f1602e012f
CodeToFLAG <hxyqbsmile@gmail.com>
2022-02-23 01:24:25 -0800

    Update 22. Generate Parentheses.java

--- 22. Generate Parentheses.java
+++ 22. Generate Parentheses.java
@@ -6,19 +6,19 @@ class Solution {
     }
     
     private void backtracking(List<String> res, StringBuilder sb, int open, int close) {
-        if (open == 0 && close == 0) {
+        if (close == 0) {
             res.add(sb.toString());
             return;
         }
         if (open > 0) {
             sb.append('(');
             backtracking(res, sb, open - 1, close);
-            sb.setLength(sb.length() -  1);
+            sb.setLength(sb.length() - 1);
         }
-        if (close > 0 && open < close) {
+        if (open < close) {
             sb.append(')');
             backtracking(res, sb, open, close - 1);
-            sb.setLength(sb.length() -  1);
+            sb.setLength(sb.length() - 1);
         }
     }
 }
