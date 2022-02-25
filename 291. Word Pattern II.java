d2f1f5c26781e740c40d8c79150483a8ba279cf9
CodeToFLAG <hxyqbsmile@gmail.com>
2022-02-24 21:17:17 -0800

    Create 291. Word Pattern II.java

---
+++ 291. Word Pattern II.java
@@ -0,0 +1,38 @@
+// backtracking
+class Solution {
+    public boolean wordPatternMatch(String pattern, String s) {
+        Map<Character, String> map = new HashMap<>();
+        return backtracking(pattern, s, 0, 0, map);
+    }
+    
+    private boolean backtracking(String pattern, String s, int idxp, int idxs, Map<Character, String> map) {
+        if (idxp == pattern.length() && idxs == s.length()) {
+            return true;
+        }
+        if (idxp >= pattern.length() || idxs >= s.length()) {
+            return false;
+        }
+        char c = pattern.charAt(idxp);
+        if (map.containsKey(c)) {
+            String str = map.get(c);
+            if (!s.substring(idxs).startsWith(str)) {
+                return false;
+            }
+            // 注意这个return不能少
+            return backtracking(pattern, s, idxp + 1, idxs + str.length(), map);
+        } else {
+            for (int i = idxs; i < s.length(); i++) {
+                String cur = s.substring(idxs, i + 1);
+                if (map.values().contains(cur)) {
+                    continue;
+                }
+                map.put(c, cur);
+                if (backtracking(pattern, s, idxp + 1, i + 1, map)) {
+                    return true;
+                }
+                map.remove(c);
+            }
+        }
+        return false;
+    }
+}
