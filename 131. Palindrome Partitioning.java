2c9eee160a5b82c5047bba168e9ec9b81c70b9c0
CodeToFLAG <hxyqbsmile@gmail.com>
2022-02-25 17:32:12 -0800

    Update 131. Palindrome Partitioning.java

--- 131. Palindrome Partitioning.java
+++ 131. Palindrome Partitioning.java
@@ -32,6 +32,44 @@ class Solution {
 
 class Solution {
     public List<List<String>> partition(String s) {
+        int n = s.length();
+        boolean[][] isPalindrome = new boolean[n][n];
+        for (int j = 0; j < n; j++) {
+            for (int i = j; i >= 0; i--) {
+                if (i == j) {
+                    isPalindrome[i][j] = true;
+                } else if (i + 1 == j) {
+                    isPalindrome[i][j] = s.charAt(i) == s.charAt(j);
+                } else {
+                    if (isPalindrome[i + 1][j - 1] && s.charAt(i) == s.charAt(j)) {
+                        isPalindrome[i][j] = true;
+                    }
+                }
+            }
+        }
+        List<List<String>> res = new ArrayList<>();
+        backtracking(s, res, isPalindrome, 0, new ArrayList<>());
+        return res;
+    }
+    
+    private void backtracking(String s, List<List<String>> res, boolean[][] isPalindrome, int start, List<String> temp) {
+        if (start == s.length()) {
+            res.add(new ArrayList<>(temp));
+            return;
+        }
+        for (int i = start; i < s.length(); i++) {
+            if (!isPalindrome[start][i]) {
+                continue;
+            }
+            temp.add(s.substring(start, i + 1));
+            backtracking(s, res, isPalindrome, i + 1, temp);
+            temp.remove(temp.size() - 1);
+        }
+    }
+}
+
+class Solution {
+    public List<List<String>> partition(String s) {
         List<List<String>> res = new ArrayList<>();
         if (s == null || s.length() == 0) {
             return res;
