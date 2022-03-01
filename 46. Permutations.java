91cf724b1d273c227f0e51e714371ea24e71f34d
CodeToFLAG <hxyqbsmile@gmail.com>
2022-03-01 02:01:56 -0800

    Update 46. Permutations.java

--- 46. Permutations.java
+++ 46. Permutations.java
@@ -2,6 +2,32 @@
 class Solution {
     public List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> res = new ArrayList<>();
+        Set<Integer> used = new HashSet<>();
+        backtracking(nums, res, new ArrayList<>(), used);
+        return res;
+    }
+    
+    private void backtracking(int[] nums, List<List<Integer>> res, List<Integer> temp, Set<Integer> used) {
+        if (temp.size() == nums.length) {
+            res.add(new ArrayList<>(temp));
+            return;
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if (used.contains(nums[i])) {
+                continue;
+            }
+            temp.add(nums[i]);
+            used.add(nums[i]);
+            backtracking(nums, res, temp, used);
+            temp.remove(temp.size() - 1);
+            used.remove(nums[i]);
+        }
+    }
+}
+
+class Solution {
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
         backtrack(nums, res, new ArrayList<Integer>());
         return res;
     }
@@ -15,31 +41,6 @@ class Solution {
                 temp.add(nums[i]);
                 backtrack(nums, res, temp);
                 temp.remove(temp.size() - 1); // 以nums[i]开头的都穷举完了，所以删了换下一个开头的。
-            }
-        }
-    }
-}
-
-// DFS 和backtrack一样，只不过用visited代替!temp.contains(nums[i])来检查nums[i]是否用过了
-class Solution {
-    public List<List<Integer>> permute(int[] nums) {
-        List<List<Integer>> res = new ArrayList<>();
-        boolean[] visited = new boolean[nums.length];
-        dfs(nums, res, new ArrayList<Integer>(), visited);
-        return res;
-    }
-    
-    private void dfs(int[] nums, List<List<Integer>> res, List<Integer> temp, boolean[] visited) {
-        if (temp.size() == nums.length) {
-            res.add(new ArrayList<Integer>(temp));
-        }
-        for (int i = 0; i < nums.length; i++) {
-            if (!visited[i]) {
-                temp.add(nums[i]);
-                visited[i] = true;
-                dfs(nums, res, temp, visited);
-                temp.remove(temp.size() - 1);
-                visited[i] = false; // 要返回上一层recursion，在remove nums[i]后，还要把nums[i]的标记设置成false
             }
         }
     }
