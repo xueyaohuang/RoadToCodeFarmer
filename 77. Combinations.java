6b304545ac1c6bd2bca22ec8267d0d962a9cefaa
CodeToFLAG <hxyqbsmile@gmail.com>
2022-03-02 01:42:26 -0800

    Update 77. Combinations.java

--- 77. Combinations.java
+++ 77. Combinations.java
@@ -1,21 +1,18 @@
 class Solution {
     public List<List<Integer>> combine(int n, int k) {
-        List<List<Integer>> list = new ArrayList<>();
-        if (n < k || k <= 0 || n <= 0) {
-            return list;
-        }
-        backtrack(n, k, 1, list, new ArrayList<>());
-        return list;       
+        List<List<Integer>> res = new ArrayList<>();
+        backtracking(n, k, 1, res, new ArrayList<>());
+        return res;
     }
     
-    private void backtrack(int n, int k, int start, List<List<Integer>> list, ArrayList<Integer> temp) {   
-        if (temp.size() == k) { // 与subsets那个题相比，就多了一个退出条件
-            list.add(new ArrayList<>(temp));
+    private void backtracking(int n, int k, int start, List<List<Integer>> res, List<Integer> temp) {
+        if (temp.size() == k)  {
+            res.add(new ArrayList<>(temp));
             return;
         }
         for (int i = start; i <= n; i++) {
             temp.add(i);
-            backtrack(n, k, i + 1, list, temp);
+            backtracking(n, k, i + 1, res, temp);
             temp.remove(temp.size() - 1);
         }
     }
