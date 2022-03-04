f92f0c8b696bb37bdd5e57d9e354ee183c136f6b
CodeToFLAG <hxyqbsmile@gmail.com>
2022-03-04 01:47:36 -0800

    Update 72. Edit Distance.java

--- 72. Edit Distance.java
+++ 72. Edit Distance.java
@@ -1,25 +1,16 @@
 class Solution {
     public int minDistance(String word1, String word2) {
-        if (word1 == null && word2 == null) {
-            return 0;
-        }
-        if (word1 == null || word2 == null) {
-            return word1 == null ? word2.length() : word1.length();
-        }
-        int len1 = word1.length();
-        int len2 = word2.length();
-        int[][] dp = new int[len1 + 1][len2 + 1];
-        
-        for (int i = 0; i <= len1; i++) {
-            for (int j = 0; j <= len2; j++) {
-                if (i == 0) {
-                    dp[i][j] = j;
-                } else if (j == 0) {
-                    dp[i][j] = i;
+        int m = word1.length();
+        int n = word2.length();
+        int[][] dp = new int[m + 1][n + 1];
+        for (int i =  0; i <= m; i++) {
+            for (int j =  0; j <= n; j++) {
+                if (i == 0 || j == 0) {
+                    dp[i][j] = i == 0 ? j : i;
                 } else if (word1.charAt(i - 1) == word2.charAt(j - 1)) {
                     dp[i][j] = dp[i - 1][j - 1];
                 } else {
-                    dp[i][j] = Math.min(dp[i - 1][j - 1], Math.min(dp[i - 1][j], dp[i][j - 1])) + 1;
+                    dp[i][j] = Math.min(dp[i - 1][j - 1], Math.min(dp[i][j - 1], dp[i - 1][j])) + 1;
                     /*
                     dp[i - 1][j - 1] + 1: replace
                     dp[i - 1][j] + 1: delete
@@ -28,6 +19,6 @@ class Solution {
                 }
             }
         }
-        return dp[len1][len2];
+        return dp[m][n];
     }
 }
