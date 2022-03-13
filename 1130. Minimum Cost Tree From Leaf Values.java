0b24b20f67d6b518751752aac84785af424b8059
CodeToFLAG <hxyqbsmile@gmail.com>
2022-03-12 21:30:07 -0800

    Create 1130. Minimum Cost Tree From Leaf Values.java

---
+++ 1130. Minimum Cost Tree From Leaf Values.java
@@ -0,0 +1,22 @@
+// https://leetcode.com/problems/minimum-cost-tree-from-leaf-values/discuss/339959/One-Pass-O(N)-Time-and-Space
+class Solution {
+    public int mctFromLeafValues(int[] arr) {
+        int res = 0;
+        Deque<Integer> stack = new ArrayDeque<>();
+        for (int i : arr) {
+            while (!stack.isEmpty() && i >= stack.peek()) {
+                int mid = stack.pop();
+                if (stack.isEmpty()) {
+                    res += mid * i;
+                } else {
+                    res += mid * Math.min(i, stack.peek());
+                }
+            }
+            stack.push(i);
+        }
+        while (stack.size() > 1) {
+            res += stack.pop() * stack.peek();
+        }
+        return res;
+    }
+}
