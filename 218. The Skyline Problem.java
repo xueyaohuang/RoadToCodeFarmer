bbafda107a4224211843095b318e3fde4635d57e
CodeToFLAG <hxyqbsmile@gmail.com>
2022-03-16 03:13:50 -0700

    Update 218. The Skyline Problem.java

--- 218. The Skyline Problem.java
+++ 218. The Skyline Problem.java
@@ -58,3 +58,35 @@ class Solution {
         return res;
     }
 }
+
+class Solution {
+    public List<List<Integer>> getSkyline(int[][] buildings) {
+        List<List<Integer>> res = new ArrayList<>();
+        List<int[]> heights = new ArrayList<>();
+        for (int[] b : buildings) {
+            heights.add(new int[]{b[0], -b[2]});
+            heights.add(new int[]{b[1], b[2]});
+        }
+        Collections.sort(heights, (a, b) -> a[0] == b[0] ? a[1] - b[1] : a[0] - b[0]);
+        TreeMap<Integer, Integer> map = new TreeMap<>();
+        map.put(0, 1);
+        int maxHeight = 0;
+        for (int[] h : heights) {
+            if (h[1] < 0) {
+                map.put(-h[1], map.getOrDefault(-h[1], 0) + 1);
+            } else {
+                if (map.get(h[1]) > 1) {
+                    map.put(h[1], map.get(h[1]) - 1);
+                } else {
+                    map.remove(h[1]);
+                }
+            }
+            int curMaxHeight = map.lastKey();
+            if (curMaxHeight != maxHeight) {
+                res.add(Arrays.asList(h[0], curMaxHeight));
+                maxHeight = curMaxHeight;
+            }
+        }
+        return res;
+    }
+}
