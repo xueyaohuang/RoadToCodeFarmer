2005631603ea9199cfabb150dc8fd3a3769755b1
CodeToFLAG <hxyqbsmile@gmail.com>
2022-03-22 23:01:47 -0700

    Update 279. Perfect Squares.java

--- 279. Perfect Squares.java
+++ 279. Perfect Squares.java
@@ -4,7 +4,7 @@ class Solution {
         Arrays.fill(dp, Integer.MAX_VALUE);
         dp[0] = 0;
         for (int i = 1; i <= n; i++) {
-            for (int j = (int)Math.sqrt(i); j >= 0; j--) {
+            for (int j = 1; j * j <= i; j++) {
                 dp[i] = Math.min(dp[i], dp[i - j * j] + 1);
             }
         }
