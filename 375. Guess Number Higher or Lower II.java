d371f12c39536f21ac90cc8648f217ee1078de1f
CodeToFLAG <hxyqbsmile@gmail.com>
2022-03-25 21:41:19 -0700

    Update 375. Guess Number Higher or Lower II.java

--- 375. Guess Number Higher or Lower II.java
+++ 375. Guess Number Higher or Lower II.java
@@ -1,6 +1,7 @@
 // time O(n^3), space O(n^2)
 // dp[i][j]: 在i到j之间猜对需要最少的钱。
 // 不知道要guess的数是什么，所以肯定要遍历所有的情况，遍历所有情况的话，要么dp，要么backtracking
+// 每次猜一个数后，自然把范围分成了两部分，所以可以联想到是merge interval型的dp
 class Solution {
     public int getMoneyAmount(int n) {
         int[][] dp = new int[n + 1][n + 1];
