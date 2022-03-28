cd0447c4c36015014c57b2968376edcd6c17b002
CodeToFLAG <hxyqbsmile@gmail.com>
2022-03-27 21:16:26 -0700

    Update 718. Maximum Length of Repeated Subarray.java

--- 718. Maximum Length of Repeated Subarray.java
+++ 718. Maximum Length of Repeated Subarray.java
@@ -1,24 +1,18 @@
 // 注意longest common substring 和 subsequence的区别，也就是连续和不连续的区别。
-
 class Solution {
-    public int findLength(int[] A, int[] B) {
-        if (A == null || B == null) {
-            return 0;
-        }
-        int lena = A.length;
-        int lenb = B.length;
-        int[][] dp = new int[lena + 1][lenb + 1];
-        int maxLen = 0;
-        
-        for (int i = 1; i <= lena; i++) {
-            for (int j = 1; j <= lenb; j++) {
-                if (A[i - 1] == B[j - 1]) {
+    public int findLength(int[] nums1, int[] nums2) {
+        int m = nums1.length, n = nums2.length;
+        int[][] dp = new int[m + 1][n + 1];
+        int max = 0;
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (nums1[i - 1] == nums2[j - 1]) {
                     dp[i][j] = dp[i - 1][j - 1] + 1;
+                    max = Math.max(max, dp[i][j]);
                 }
-                maxLen = Math.max(maxLen, dp[i][j]);
             }
         }
-        return maxLen;
+        return max;
     }
 }
 
